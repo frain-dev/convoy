5362345e6907e137dc3b3814cce8fe8a66ed1d37
Lanre Adelowo <yo@lanre.wtf>
2021-05-15 13:43:46 +0100

    add application test

---
+++ datastore/application_test.go
@@ -0,0 +1,45 @@
+// +build integration
+
+package datastore
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hookcamp/hookcamp"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_CreateApplication(t *testing.T) {
+
+	db, closeFn := getDB(t)
+	defer closeFn()
+
+	orgRepo := NewOrganisationRepo(db)
+	appRepo := NewApplicationRepo(db)
+
+	newOrg := &hookcamp.Organisation{
+		OrgName: "Random new organisation",
+	}
+
+	require.NoError(t, orgRepo.CreateOrganisation(context.Background(), newOrg))
+
+	app := &hookcamp.Application{
+		Title: "Next application name",
+		OrgID: newOrg.ID,
+	}
+
+	require.NoError(t, appRepo.CreateApplication(context.Background(), app))
+}
+
+func Test_LoadApplications(t *testing.T) {
+	db, closeFn := getDB(t)
+	defer closeFn()
+
+	appRepo := NewApplicationRepo(db)
+
+	apps, err := appRepo.LoadApplications(context.Background())
+	require.NoError(t, err)
+
+	require.True(t, len(apps) > 0)
+}
