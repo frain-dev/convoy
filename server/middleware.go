19a90b58c269e3cc0a5c1dee2b283a21d8f295a2
horlah <emmanuel.ainaj@gmail.com>
2021-09-02 19:44:51 +0100

    fix CORS error

--- server/middleware.go
+++ server/middleware.go
@@ -77,6 +77,8 @@ func ensureBasicAuthFromRequest(a *config.AuthConfiguration, r *http.Request) er
 func jsonResponse(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
