399798afce5e9585c38cbd090ae96efbac07b476
Smart Mekiliuwa <st.nonso@gmail.com>
2021-09-03 11:49:24 +0100

    refactored API to match open yaml and added delete functionality (#54)
    * app secrets & custom signature on web hook request header #31
    
    * updated signature header implementation
    
    * updated signature header implementation again
    
    * refactored server response and event payload data
    
    * refactored API to match open yaml and added delete functionality
    
    * updated application.go in data store
    
    * fixed attempt count when event is successfully sent
    
    * updated deleted route handlers
    
    * paginated app events API to fetch all

--- server/models/models.go
+++ server/models/models.go
@@ -9,29 +9,29 @@ type Organisation struct {
 }
 
 type Application struct {
-	OrgID   string `json:"orgId" bson:"orgId"`
+	OrgID   string `json:"org_id" bson:"org_id"`
 	AppName string `json:"name" bson:"name"`
 	Secret  string `json:"secret" bson:"secret"`
 }
 
 type Message struct {
-	MessageID  string          `json:"msgId" bson:"msg_id"`
-	AppID      string          `json:"appId" bson:"app_id"`
-	EventType  string          `json:"eventType" bson:"event_type"`
-	ProviderID string          `json:"providerId" bson:"provider_id"`
+	MessageID  string          `json:"msg_id" bson:"msg_id"`
+	AppID      string          `json:"app_id" bson:"app_id"`
+	EventType  string          `json:"event_type" bson:"event_type"`
+	ProviderID string          `json:"provider_id" bson:"provider_id"`
 	Data       json.RawMessage `json:"data" bson:"data"`
 
 	Status    string `json:"status" bson:"status"`
-	CreatedAt int64  `json:"createdAt" bson:"created_at"`
+	CreatedAt int64  `json:"created_at" bson:"created_at"`
 }
 
 type MessageAttempt struct {
-	MessageID  string `json:"msgId" bson:"msg_id"`
-	APIVersion string `json:"apiVersion" bson:"api_version"`
+	MessageID  string `json:"msg_id" bson:"msg_id"`
+	APIVersion string `json:"api_version" bson:"api_version"`
 	IPAddress  string `json:"ip" bson:"ip"`
 
 	Status    string `json:"status" bson:"status"`
-	CreatedAt int64  `json:"createdAt" bson:"created_at"`
+	CreatedAt int64  `json:"created_at" bson:"created_at"`
 
 	MessageResponse MessageResponse `json:"response" bson:"response"`
 }
@@ -49,20 +49,20 @@ type Endpoint struct {
 
 type Pageable struct {
 	Page    int `json:"page" bson:"page"`
-	PerPage int `json:"perPage" bson:"perPage"`
+	PerPage int `json:"per_page" bson:"per_page"`
 	Sort    int `json:"sort" bson:"sort"`
 }
 
 type SearchParams struct {
-	CreatedAtStart int64 `json:"createdAtStart" bson:"created_at_start"`
-	CreatedAtEnd   int64 `json:"createdAtEnd" bson:"created_at_end"`
+	CreatedAtStart int64 `json:"created_at_start" bson:"created_at_start"`
+	CreatedAtEnd   int64 `json:"created_at_end" bson:"created_at_end"`
 }
 
 type DashboardSummary struct {
-	MessagesSent uint64             `json:"messagesSent" bson:"messages_sent"`
+	MessagesSent uint64             `json:"messages_sent" bson:"messages_sent"`
 	Applications int                `json:"apps" bson:"apps"`
 	Period       string             `json:"period" bson:"period"`
-	PeriodData   *[]MessageInterval `json:"messageData,omitempty" bson:"message_data"`
+	PeriodData   *[]MessageInterval `json:"message_data,omitempty" bson:"message_data"`
 }
 
 type MessageInterval struct {
