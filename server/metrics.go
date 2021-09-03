bdafdc24db712d2016dce9a503492c7fb71fbae5
Subomi Oluwalana <subomioluwalana71@gmail.com>
2021-09-03 10:09:11 +0100

    Fixed changed const to var

--- server/metrics.go
+++ server/metrics.go
@@ -2,7 +2,7 @@ package server
 
 import "github.com/prometheus/client_golang/prometheus"
 
-const requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+var requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "request_duration_seconds",
 	Help:    "Time (in seconds) spent serving HTTP requests.",
 	Buckets: prometheus.DefBuckets,
