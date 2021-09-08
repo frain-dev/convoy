d708c6aff9541ca3cdec5064c2841643c3afb325
horlah <emmanuel.ainaj@gmail.com>
2021-09-08 20:45:19 +0100

    update favicons and meta details update logos

--- web/ui/website/nuxt.config.js
+++ web/ui/website/nuxt.config.js
@@ -1,47 +1,46 @@
 export default {
-	// Target: https://go.nuxtjs.dev/config-target
-	target: "static",
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-	// Global page headers: https://go.nuxtjs.dev/config-head
-	head: {
-		title: "courier-website",
-		htmlAttrs: {
-			lang: "en",
-		},
-		meta: [
-			{ charset: "utf-8" },
-			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{ hid: "description", name: "description", content: "" },
-			{ name: "format-detection", content: "telephone=no" },
-		],
-		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-	},
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'Convoy',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            { hid: 'description', name: 'description', content: 'A Cloud native Webhook Service with out-of-the-box security, reliability and scalability for your webhooks infrastructure.' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
 
-	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: ["@/scss/main.scss"],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['@/scss/main.scss'],
 
-	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [{ src: "~/plugins/prism.js", mode: "client" }],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [{ src: '~/plugins/prism.js', mode: 'client' }],
 
-	// Auto import components: https://go.nuxtjs.dev/config-components
-	components: true,
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: [],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
 
-	// Modules: https://go.nuxtjs.dev/config-modules
-	modules: [
-		// https://go.nuxtjs.dev/content
-		"@nuxt/content",
-	],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/content
+        '@nuxt/content',
+    ],
 
-	// Content module configuration: https://go.nuxtjs.dev/config-content
-	content: {},
+    // Content module configuration: https://go.nuxtjs.dev/config-content
+    content: {},
 
-	// Build Configuration: https://go.nuxtjs.dev/config-build
-	build: {},
-	runtimeCompiler: true,
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+    runtimeCompiler: true,
 };
