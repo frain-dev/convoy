feef14143bc6c7f5527cac6912d4f196a9a0f6f2
horlah <emmanuel.ainaj@gmail.com>
2021-09-08 20:11:18 +0100

    build website [basic]

---
+++ web/ui/website/plugins/prism.js
@@ -0,0 +1,10 @@
+import 'clipboard'; // For the copy to clipboard plugin
+import Prism from 'prismjs';
+
+// Include a theme:
+import './prism.scss';
+
+// Include language support: (optional)
+import 'prismjs/components/prism-json';
+
+export default Prism;
