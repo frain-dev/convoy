a46f5af2d2077373859410463484783e814e915d
krisblarq <naturalicon007@gmail.com>
2021-09-17 20:00:27 +0100

    Version number set to inline

--- web/ui/react-app/src/pages/dashboard/index.js
+++ web/ui/react-app/src/pages/dashboard/index.js
@@ -254,7 +254,7 @@ function DashboardPage() {
 				<div className="dashboard--header--container">
 					<div className="logo">
 						<img src={ConvoyLogo} alt="convoy logo" />
-						<p>v0.1.1</p>
+						<p>v0.1.7</p>
 					</div>
 
 					<button className="user">
