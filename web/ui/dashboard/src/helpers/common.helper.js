283a70f8babf0133f48737065624b4e19695642b
horlah <emmanuel.ainaj@gmail.com>
2021-09-28 00:05:29 +0100

    fix UI issues on safari group events by date on events table show time for date created hide next retry for events past max retry add identifier for retried events

--- web/ui/dashboard/src/helpers/common.helper.js
+++ web/ui/dashboard/src/helpers/common.helper.js
@@ -8,6 +8,49 @@ const getDate = date => {
 	return `${day} ${months[month]}, ${year}`;
 };
 
+const getTime = date => {
+	const _date = new Date(date);
+	const hours = _date.getHours();
+	const minutes = _date.getMinutes();
+	const seconds = _date.getSeconds();
+
+	const hour = hours > 12 ? hours - 12 : hours;
+	return `${hour} : ${minutes} : ${seconds} ${hours > 12 ? 'AM' : 'PM'}`;
+};
+
+// Work in progress here
+
+// const getTimeDifference = date => {
+// 	const startDate = new Date();
+// 	const endDate = new Date(date);
+// 	var diff = endDate.getTime() - startDate.getTime();
+// 	var hours = Math.floor(diff / 1000 / 60 / 60);
+// 	diff -= hours * 1000 * 60 * 60;
+// 	console.log('🚀 ~ file: common.helper.js ~ line 27 ~ hours', hours);
+// 	var minutes = Math.floor(diff / 1000 / 60);
+// 	const seconds = Math.floor((diff / 1000) % 60);
+// 	console.log('🚀 ~ file: common.helper.js ~ line 30 ~ seconds', seconds);
+
+// 	// If using time pickers with 24 hours format, add the below line get exact hours
+// 	if (hours < 0) hours = hours + 24;
+
+// 	console.log((hours <= 9 ? '0' : '') + hours + ':' + (minutes <= 9 ? '0' : '') + minutes);
+// 	return (hours <= 9 ? '0' : '') + hours + 'h:' + (minutes <= 9 ? '0' : '') + minutes + 'm' + seconds + 's';
+// 	// return `${dateDifference}`;
+// };
+
+const getDateDifference = date => {
+	const dayOfYear = date => {
+		const start = new Date(date.getFullYear(), 0, 0);
+		const diff = date - start + (start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000;
+		const oneDay = 1000 * 60 * 60 * 24;
+		const day = Math.floor(diff / oneDay);
+		return day;
+	};
+	const dateDifference = dayOfYear(new Date(date)) - dayOfYear(new Date());
+	return `${dateDifference}`;
+};
+
 const copyText = copyText => {
 	const el = document.createElement('textarea');
 	el.value = copyText;
@@ -22,4 +65,4 @@ const logout = () => {
 	window.location.replace('/login');
 };
 
-export { getDate, copyText, logout };
+export { getDate, getTime, copyText, logout, getDateDifference };
