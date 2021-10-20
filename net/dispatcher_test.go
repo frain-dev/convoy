3cf4e8e023af37956e4a1cf21d9e41029814fcd7
Daniel Oluojomu <danvixent@gmail.com>
2021-10-21 00:58:13 +0100

    fix test

--- net/dispatcher_test.go
+++ net/dispatcher_test.go
@@ -2,122 +2,21 @@ package net
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"testing"
-	"time"
 
 	"github.com/jarcoal/httpmock"
 
 	"github.com/frain-dev/convoy/config"
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	host1 = "127.0.0.1:2091"
-	host2 = "127.0.0.1:2092"
-)
-
-const (
-	successEndpoint = "/success"
-	failEndpoint    = "/failure"
-)
-
 var (
-	server1 = &http.Server{
-		Addr: host1,
-	}
-
-	server2 = &http.Server{
-		Addr: host2,
-	}
-
 	successBody      = []byte("received webhook successfully")
 	failureBody      = []byte("error occurred")
 	pageNotFoundBody = []byte("404 page not found\n")
 )
-
-func TestMain(m *testing.M) {
-	mux := http.DefaultServeMux
-	logger := log.WithFields(map[string]interface{}{})
-	mux.HandleFunc(successEndpoint, func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.WithError(err).Fatal("failed to read body")
-		}
-
-		logger.WithField("request_body", string(body))
-
-		w.Header()[http.CanonicalHeaderKey("Request_ID")] = []string{"abcd"}
-
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(successBody)
-
-	})
-
-	mux.HandleFunc(failEndpoint, func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.WithError(err).Fatal("failed to read body")
-		}
-
-		logger.WithField("request_body", string(body))
-
-		w.Header()[http.CanonicalHeaderKey("Request_ID")] = []string{"abcd"}
-
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write(failureBody)
-	})
-
-	server1.Handler = mux
-	server2.Handler = mux
-
-	go func() {
-		if err := server1.ListenAndServe(); err != http.ErrServerClosed {
-			log.WithError(err).Fatal("server1 exited")
-		}
-	}()
-
-	go func() {
-		if err := server2.ListenAndServe(); err != http.ErrServerClosed {
-			log.WithError(err).Fatal("server2 exited")
-		}
-	}()
-
-	// allow the servers start
-	time.Sleep(2 * time.Second)
-	code := m.Run()
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	_ = server1.Shutdown(ctx)
-	_ = server2.Shutdown(ctx)
-
-	os.Exit(code)
-}
-
-// serialize obj into json bytes
-func serialize(obj interface{}) *bytes.Buffer {
-	buf := &bytes.Buffer{}
-	if err := json.NewEncoder(buf).Encode(obj); err != nil {
-		log.WithError(err).Fatalf("unable to serialize obj")
-	}
-	return buf
-}
-
-func rawMessage(obj interface{}) json.RawMessage {
-	buf := serialize(obj)
-	return buf.Bytes()
-}
-
-func formatEndpoint(host, endpoint string) string {
-	return fmt.Sprintf("http://%s%s", host, endpoint)
-}
 
 func TestDispatcher_SendRequest(t *testing.T) {
 	client := http.DefaultClient
