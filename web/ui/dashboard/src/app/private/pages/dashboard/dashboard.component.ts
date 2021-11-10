bcceda14d26a3e9b50ac65dc6760190b6f6ff207
horlah <emmanuel.ainaj@gmail.com>
2021-11-11 00:37:24 +0100

    undo push

--- web/ui/dashboard/src/app/private/pages/dashboard/dashboard.component.ts
+++ web/ui/dashboard/src/app/private/pages/dashboard/dashboard.component.ts
@@ -112,7 +112,6 @@ export class DashboardComponent implements OnInit {
 				method: 'get'
 			});
 			this.dashboardData = dashboardResponse.data;
-			console.log('🚀 ~ file: dashboard.component.ts ~ line 115 ~ DashboardComponent ~ fetchDashboardData ~ this.dashboardData', this.dashboardData);
 
 			let labelsDateFormat = '';
 			if (this.dashboardFrequency === 'daily') labelsDateFormat = 'DD[, ]MMM';
@@ -222,7 +221,6 @@ export class DashboardComponent implements OnInit {
 			}
 
 			this.events = eventsResponse.data;
-			console.log('🚀 ~ file: dashboard.component.ts ~ line 225 ~ DashboardComponent ~ getEvents ~ this.events', this.events);
 			this.setEventsDisplayed(eventsResponse.data.content);
 		} catch (error) {
 			return error;
