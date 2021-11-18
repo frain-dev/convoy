31ded07002c7e7a5bb30e6e49cad1d6a28370b9f
Emmanuel AIna <emmanuel.ainaj@gmail.com>
2021-11-18 15:57:51 +0100

    Update UI with group selection (#222)
    * Update UI with group selection
    Add `groupID` query parameters to all requests
    
    * fix event deliveries request method on init

--- web/ui/dashboard/src/app/public/login/login.component.ts
+++ web/ui/dashboard/src/app/public/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpService } from 'src/app/services/http/http.service';
 
 @Component({
 	selector: 'app-login',
@@ -16,15 +15,12 @@ export class LoginComponent implements OnInit {
 		password: ['', Validators.required]
 	});
 
-	constructor(private formBuilder: FormBuilder, private router: Router, private httpService: HttpService) {}
+	constructor(private formBuilder: FormBuilder, private router: Router) {}
 
 	ngOnInit(): void {}
 
 	async login() {
-		try {
-			const loginResponse = await this.httpService.request({ method: 'post', url: '/auth/login', body: this.loginForm.value });
-			localStorage.setItem('CONVOY_AUTH', JSON.stringify(loginResponse.data));
-			this.router.navigateByUrl('dashboard');
-		} catch (error) {}
+		localStorage.setItem('CONVOY_AUTH', JSON.stringify(this.loginForm.value));
+		this.router.navigateByUrl('dashboard');
 	}
 }
