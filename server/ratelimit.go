8ddba86f0a92e00a3c6b42b90d0c48d59a8eddcd
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-11-21 16:57:18 +0100

    Add test

--- server/ratelimit.go
+++ server/ratelimit.go
@@ -2,13 +2,18 @@ package server
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/frain-dev/convoy"
 	"github.com/go-chi/httprate"
 )
 
 func rateLimitByGroup() func(next http.Handler) http.Handler {
-	return httprate.Limit(convoy.RATE_LIMIT, convoy.RATE_LIMIT_DURATION, httprate.WithKeyFuncs(func (req *http.Request) (string, error) {
+	return rateLimitByGroupWithParams(convoy.RATE_LIMIT, convoy.RATE_LIMIT_DURATION)
+}
+
+func rateLimitByGroupWithParams(requestLimit int, windowLength time.Duration) func(next http.Handler) http.Handler {
+	return httprate.Limit(requestLimit, windowLength, httprate.WithKeyFuncs(func (req *http.Request) (string, error) {
 		return getGroupFromContext(req.Context()).UID, nil
 	}))
 }
---
+++ server/ratelimit_test.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/frain-dev/convoy"
+)
+
+func TestRateLimitByGroup(t *testing.T) {
+	type test struct {
+		name          string
+		requestsLimit int
+		windowLength  time.Duration
+		groupIDs        []string
+		respCodes     []int
+	}
+	tests := []test{
+		{
+			name:          "no-block",
+			requestsLimit: 3,
+			windowLength:  2 * time.Second,
+			groupIDs:       []string {"a", "a"},
+			respCodes:     []int{200, 200},
+		},
+		{
+			name:          "block-same-group",
+			requestsLimit: 2,
+			windowLength:  3 * time.Microsecond,
+			groupIDs:        []string {"b", "b", "b"},
+			respCodes:     []int{200, 429, 200},
+		},
+		{
+			name: "no-block-different-group",
+			requestsLimit: 1,
+			windowLength: 1 * time.Second,
+			groupIDs:       []string {"c", "d"},
+			respCodes:     []int{200, 200},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			})
+			router := rateLimitByGroupWithParams(tt.requestsLimit, tt.windowLength)(h)
+
+			for i, code := range tt.respCodes {
+				req := httptest.NewRequest("POST", "/", nil)
+				req = req.Clone(context.WithValue(req.Context(), groupCtx , &convoy.Group{UID: tt.groupIDs[i]}))
+				recorder := httptest.NewRecorder()
+				router.ServeHTTP(recorder, req)
+				if respCode := recorder.Result().StatusCode; respCode != code {
+					t.Errorf("resp.StatusCode(%v) = %v, want %v", i, respCode, code)
+				}
+			}
+		})
+	}
+}
