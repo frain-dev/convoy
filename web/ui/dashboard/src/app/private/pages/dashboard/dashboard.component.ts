3499efd45cacdb770afb27c654651e2ebb236334
horlah <emmanuel.ainaj@gmail.com>
2021-12-06 18:31:14 +0100

    test repo split

--- web/ui/dashboard/src/app/private/pages/dashboard/dashboard.component.ts
+++ web/ui/dashboard/src/app/private/pages/dashboard/dashboard.component.ts
@@ -84,6 +84,7 @@ export class DashboardComponent implements OnInit {
 	eventDeliveryFilteredByStatus: '' | 'Success' | 'Failure' | 'Retry' | 'Scheduled' | 'Processing' | 'Discarded' = '';
 	showOverlay = false;
 	showEventDeliveriesStatusDropdown = false;
+	testshowEventDeliveriesStatusDropdown = false;
 
 	constructor(private httpService: HttpService, private generalService: GeneralService, private router: Router, private formBuilder: FormBuilder, private route: ActivatedRoute) {}
 
