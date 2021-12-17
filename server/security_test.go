6d0ba796999fc0857f414d3008598b0a3d8e3c7f
Daniel Oluojomu <danvixent@gmail.com>
2021-12-17 23:57:18 +0100

    add TestApplicationHandler_GetAPIKeyByID

--- server/security_test.go
+++ server/security_test.go
@@ -3,10 +3,13 @@ package server
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/frain-dev/convoy"
 
 	"github.com/frain-dev/convoy/config"
 	"github.com/frain-dev/convoy/mocks"
@@ -244,6 +247,93 @@ func TestApplicationHandler_RevokeAPIKeys(t *testing.T) {
 			if w.Code != tc.statusCode {
 				t.Errorf("Want status '%d', got '%d'", tc.statusCode, w.Code)
 			}
+
+			verifyMatch(t, *w)
+		})
+	}
+}
+
+func TestApplicationHandler_GetAPIKeyByID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	groupRepo := mocks.NewMockGroupRepository(ctrl)
+	appRepo := mocks.NewMockApplicationRepository(ctrl)
+	eventRepo := mocks.NewMockEventRepository(ctrl)
+	eventDeliveryRepo := mocks.NewMockEventDeliveryRepository(ctrl)
+	eventQueue := mocks.NewMockQueuer(ctrl)
+	apiKeyRepo := mocks.NewMockAPIKeyRepo(ctrl)
+
+	app := newApplicationHandler(eventRepo, eventDeliveryRepo, appRepo, groupRepo, apiKeyRepo, eventQueue)
+
+	keyID := "12345"
+	apiKey := &convoy.APIKey{UID: keyID}
+
+	tt := []struct {
+		name           string
+		cfgPath        string
+		stripTimestamp bool
+		statusCode     int
+		keyID          string
+		dbFn           func(app *applicationHandler)
+	}{
+		{
+			name:           "should_find_api_key",
+			stripTimestamp: true,
+			cfgPath:        "./testdata/Auth_Config/no-auth-convoy.json",
+			statusCode:     http.StatusOK,
+			keyID:          keyID,
+			dbFn: func(app *applicationHandler) {
+				a, _ := app.apiKeyRepo.(*mocks.MockAPIKeyRepo)
+				a.EXPECT().FindAPIKeyByID(gomock.Any(), gomock.Any()).Times(1).Return(apiKey, nil)
+			},
+		},
+		{
+			name:           "should_fail_to_find_api_key",
+			stripTimestamp: false,
+			cfgPath:        "./testdata/Auth_Config/no-auth-convoy.json",
+			statusCode:     http.StatusInternalServerError,
+			keyID:          keyID,
+			dbFn: func(app *applicationHandler) {
+				a, _ := app.apiKeyRepo.(*mocks.MockAPIKeyRepo)
+				a.EXPECT().FindAPIKeyByID(gomock.Any(), gomock.Any()).Times(1).Return(nil, errors.New("abc"))
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			url := fmt.Sprintf("/api/v1/security/keys/%s", tc.keyID)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+			req.SetBasicAuth("test", "test")
+			w := httptest.NewRecorder()
+			rctx := chi.NewRouteContext()
+			req.Header.Add("Content-Type", "application/json")
+
+			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+			// Arrange Expectations
+			if tc.dbFn != nil {
+				tc.dbFn(app)
+			}
+
+			err := config.LoadConfig(tc.cfgPath)
+			if err != nil {
+				t.Errorf("Failed to load config file: %v", err)
+			}
+			initRealmChain(t)
+
+			router := buildRoutes(app)
+
+			// Act
+			router.ServeHTTP(w, req)
+
+			// Assert
+			if w.Code != tc.statusCode {
+				t.Errorf("Want status '%d', got '%d'", tc.statusCode, w.Code)
+			}
+			fmt.Println(w.Body.String())
 
 			verifyMatch(t, *w)
 		})
