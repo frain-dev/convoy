5ee887c435d1ad26030e6851802656831fd2753f
ogbanugot <ogbanugot@gmail.com>
2022-01-06 15:34:10 +0100

    add memberlist delegates

---
+++ memberlist/memberlist.go
@@ -0,0 +1,169 @@
+package memberlist
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/memberlist"
+)
+
+var (
+	mtx        sync.RWMutex
+	items      = map[string]string{}
+	broadcasts *memberlist.TransmitLimitedQueue
+)
+
+type broadcast struct {
+	msg    []byte
+	notify chan<- struct{}
+}
+
+type delegate struct{}
+
+type update struct {
+	Action string // kill
+}
+
+func init() {
+	flag.Parse()
+}
+
+func (b *broadcast) Invalidates(other memberlist.Broadcast) bool {
+	return false
+}
+
+func (b *broadcast) Message() []byte {
+	return b.msg
+}
+
+func (b *broadcast) Finished() {
+	if b.notify != nil {
+		close(b.notify)
+	}
+}
+
+func (d *delegate) NodeMeta(limit int) []byte {
+	return []byte{}
+}
+
+func (d *delegate) NotifyMsg(b []byte) {
+	if len(b) == 0 {
+		return
+	}
+
+	switch b[0] {
+	case 'd': // data
+		var updates []*update
+		if err := json.Unmarshal(b[1:], &updates); err != nil {
+			return
+		}
+		mtx.Lock()
+		for _, u := range updates {
+			switch u.Action {
+			case "add":
+				fmt.Printf("add")
+			case "kill":
+				os.Exit(0)
+			}
+		}
+		mtx.Unlock()
+	}
+}
+
+func (d *delegate) GetBroadcasts(overhead, limit int) [][]byte {
+	return broadcasts.GetBroadcasts(overhead, limit)
+}
+
+func (d *delegate) LocalState(join bool) []byte {
+	mtx.RLock()
+	m := items
+	mtx.RUnlock()
+	b, _ := json.Marshal(m)
+	return b
+}
+
+func (d *delegate) MergeRemoteState(buf []byte, join bool) {
+	if len(buf) == 0 {
+		return
+	}
+	if !join {
+		return
+	}
+	var m map[string]string
+	if err := json.Unmarshal(buf, &m); err != nil {
+		return
+	}
+	mtx.Lock()
+	for k, v := range m {
+		items[k] = v
+	}
+	mtx.Unlock()
+}
+
+type eventDelegate struct{}
+
+func (ed *eventDelegate) NotifyJoin(node *memberlist.Node) {
+	log.Printf("A node has joined: " + node.String())
+}
+
+func (ed *eventDelegate) NotifyLeave(node *memberlist.Node) {
+	log.Printf("A node has left: " + node.String())
+}
+
+func (ed *eventDelegate) NotifyUpdate(node *memberlist.Node) {
+	log.Printf("A node was updated: " + node.String())
+}
+
+//Create a memberlist or join one if a member is specified.
+func CreateMemberlist(members string) error {
+	hostname, _ := os.Hostname()
+	c := memberlist.DefaultLocalConfig()
+	c.Events = &eventDelegate{}
+	c.Delegate = &delegate{}
+	c.BindPort = 0
+	c.Name = hostname + "-" + uuid.NewString()
+	m, err := memberlist.Create(c)
+	if err != nil {
+		return err
+	}
+	if len(members) > 0 {
+		parts := strings.Split(members, ",")
+		_, err := m.Join(parts)
+		if err != nil {
+			return err
+		}
+	}
+	broadcasts = &memberlist.TransmitLimitedQueue{
+		NumNodes: func() int {
+			return m.NumMembers()
+		},
+		RetransmitMult: 3,
+	}
+	node := m.LocalNode()
+	log.Printf("Local member %s:%d\n", node.Addr, node.Port)
+	return nil
+}
+
+func killHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := json.Marshal([]*update{
+		{
+			Action: "kill",
+		},
+	})
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	broadcasts.QueueBroadcast(&broadcast{
+		msg:    append([]byte("d"), b...),
+		notify: nil,
+	})
+}
