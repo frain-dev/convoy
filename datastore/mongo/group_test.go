e5828cefc10bb814d6e0325586f07184237073cd
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-01-19 15:30:16 +0100

    fix add test

--- datastore/mongo/group_test.go
+++ datastore/mongo/group_test.go
@@ -6,10 +6,12 @@ package mongo
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/frain-dev/convoy/datastore"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func Test_FetchGroupByID(t *testing.T) {
@@ -50,6 +52,53 @@ func Test_CreateGroup(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, org.UID, newOrg.UID)
+}
+
+func Test_CannotCreateGroupWithExistingName(t *testing.T) {
+	db, closeFn := getDB(t)
+	defer closeFn()
+
+	groupRepo := NewGroupRepo(db)
+
+	org := &datastore.Group{
+		Name:           "Next group",
+		UID:            uuid.NewString(),
+		DocumentStatus: datastore.ActiveDocumentStatus,
+	}
+
+	require.NoError(t, groupRepo.CreateGroup(context.Background(), org))
+
+	org = &datastore.Group{
+		Name:           "Next group",
+		UID:            uuid.NewString(),
+		DocumentStatus: datastore.ActiveDocumentStatus,
+	}
+
+	require.Error(t, groupRepo.CreateGroup(context.Background(), org))
+}
+
+func Test_CanCreateGroupWithExistingNameThatHasBeenDeleted(t *testing.T) {
+	db, closeFn := getDB(t)
+	defer closeFn()
+
+	groupRepo := NewGroupRepo(db)
+
+	newOrg := &datastore.Group{
+		Name:           "Existing group",
+		UID:            uuid.NewString(),
+		DeletedAt:      primitive.NewDateTimeFromTime(time.Now()),
+		DocumentStatus: datastore.DeletedDocumentStatus,
+	}
+
+	require.NoError(t, groupRepo.CreateGroup(context.Background(), newOrg))
+
+	newOrg = &datastore.Group{
+		Name:           "Existing group",
+		UID:            uuid.NewString(),
+		DocumentStatus: datastore.ActiveDocumentStatus,
+	}
+
+	require.NoError(t, groupRepo.CreateGroup(context.Background(), newOrg))
 }
 
 func Test_LoadGroups(t *testing.T) {
