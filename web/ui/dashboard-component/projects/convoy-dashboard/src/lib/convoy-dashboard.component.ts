ff4f07653365d7e673b22f72c7c117428200e62f
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-01-19 14:10:01 +0100

    fix summary api query param (#375)
    * Emmanuel/fix/0.3 fixes (#238)
    
    * WIP
    
    * undo push
    
    * remove dashboard filters from url persistence
    fix clear filter actions to remove filter in URL
    add group id query param to config API
    
    * clear filters on changing active group
    add filters to URL from their intializing methods
    
    * persist active logs tab with query parameter
    
    Co-authored-by: horlah <emmanuel.ainaj@gmail.com>
    
    * Emmanuel/feature/add status filter (#239)
    
    * Emmanuel/fix/0.3-fixes (#234)
    
    * WIP
    
    * undo push
    
    * remove dashboard filters from url persistence
    fix clear filter actions to remove filter in URL
    add group id query param to config API
    
    * clear filters on changing active group
    add filters to URL from their intializing methods
    
    * persist active logs tab with query parameter
    
    * implement status filter for event delieveries
    
    Co-authored-by: Emmanuel Aina <emmanuel.ainaj@gmail.com>
    
    * Bump version to v0.3.0-alpha
    
    * fix summary API query param
    
    * remove console log
    
    Co-authored-by: Subomi Oluwalana <subomioluwalana71@gmail.com>

--- web/ui/dashboard-component/projects/convoy-dashboard/src/lib/convoy-dashboard.component.ts
+++ web/ui/dashboard-component/projects/convoy-dashboard/src/lib/convoy-dashboard.component.ts
@@ -207,14 +207,13 @@ export class ConvoyDashboardComponent implements OnInit {
 					currentChart.update();
 				}
 			}
-			console.log(Chart.getChart('dahboard_events_chart'));
 		} catch (error) {}
 	}
 
 	setDateForFilter(requestDetails: { startDate: Date; endDate: Date }) {
 		if (!requestDetails.endDate && !requestDetails.startDate) return { startDate: '', endDate: '' };
-		const startDate = requestDetails.startDate ? `${format(requestDetails.startDate, 'yyyy-M-d')}T00:00:00` : '';
-		const endDate = requestDetails.endDate ? `${format(requestDetails.endDate, 'yyyy-M-d')}T00:00:00` : '';
+		const startDate = requestDetails.startDate ? `${format(requestDetails.startDate, 'yyyy-MM-dd')}T00:00:00` : '';
+		const endDate = requestDetails.endDate ? `${format(requestDetails.endDate, 'yyyy-MM-dd')}T23:59:59` : '';
 		return { startDate, endDate };
 	}
 
