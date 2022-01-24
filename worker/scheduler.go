c13d1483c92a74c48b4f937bde16df963bd517b8
ogbanugot <ogbanugot@gmail.com>
2022-01-24 19:13:10 +0100

    add NewGroupTask func

--- worker/scheduler.go
+++ worker/scheduler.go
@@ -2,17 +2,30 @@ package worker
 
 import (
 	"context"
-	"log"
 
+	"github.com/frain-dev/convoy"
 	"github.com/frain-dev/convoy/datastore"
+	"github.com/frain-dev/convoy/worker/task"
+	log "github.com/sirupsen/logrus"
+	"github.com/vmihailenco/taskq/v3"
 )
 
-func NewGroupTask(groupRepo datastore.GroupRepository, filter *datastore.GroupFilter) {
+func NewGroupTask(groupRepo datastore.GroupRepository, applicationRepo datastore.ApplicationRepository, eventDeliveryRepo datastore.EventDeliveryRepository) {
 	go func() {
-		groups, err := groupRepo.LoadGroups(context.TODO(), filter)
-		if err != nil {
-			log.Fatalf("an error occurred while fetching Groups:", err)
-			return
+		for {
+			filter := &datastore.GroupFilter{}
+			groups, err := groupRepo.LoadGroups(context.Background(), filter)
+			if err != nil {
+				log.Fatalf("an error occurred while fetching Groups:%v", err)
+			}
+			for _, g := range groups {
+				name := convoy.TaskName(g.Name)
+				if t := taskq.Tasks.Get(string(name)); t == nil {
+					handler := task.ProcessEventDelivery(applicationRepo, eventDeliveryRepo, groupRepo)
+					log.Infof("Registering task handler for %s", g.Name)
+					task.CreateTask(name, *g, handler)
+				}
+			}
 		}
 	}()
 }
