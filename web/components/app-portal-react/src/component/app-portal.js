4f479596769a9e1276b86fced9138033ab583ced
horlah <emmanuel.ainaj@gmail.com>
2022-02-21 10:56:10 +0100

    [WIP] App Portal for react setup

---
+++ web/components/app-portal-react/src/component/app-portal.js
@@ -0,0 +1,19 @@
+import logo from '../logo.svg';
+
+function AppPortal() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React oo
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default AppPortal;
