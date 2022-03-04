0d58fac7c085be26920ac5fa092a28c29fbb937f
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-03-04 22:16:00 +0100

    fix metric issue in dashboard component (#493)
    * WIP
    
    * undo push
    
    * fix metric issue in dashboard component
    
    * undo app portal version update
    
    * version fix
    
    * update convoy dashboard

--- web/ui/dashboard-component/projects/convoy-dashboard/src/lib/convoy-dashboard.module.ts
+++ web/ui/dashboard-component/projects/convoy-dashboard/src/lib/convoy-dashboard.module.ts
@@ -6,10 +6,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PrismModule } from './prism/prism.module';
 import { ConvoyLoaderComponent } from './loader-component/loader.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
 	declarations: [ConvoyDashboardComponent, ConvoyLoaderComponent],
-	imports: [CommonModule, MatDatepickerModule, MatNativeDateModule, FormsModule, ReactiveFormsModule, PrismModule],
+	imports: [CommonModule, MatDatepickerModule, MatNativeDateModule, FormsModule, ReactiveFormsModule, PrismModule, SharedModule],
 	exports: [ConvoyDashboardComponent]
 })
 export class ConvoyDashboardModule {}
