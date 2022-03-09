e772b3d0fe4b89d4517b23012e37dfe2c425ed7b
Raymond Tukpe <jirevwe@users.noreply.github.com>
2022-03-09 16:06:42 +0100

    Prefer environment variables over convoy config file (#511)
    * feat: prefer environment variables over convoy config file
    
    * tests: add tests for env vars

--- config/realm.go
+++ config/realm.go
@@ -1,6 +1,12 @@
 package config
 
-import "github.com/frain-dev/convoy/auth"
+import (
+	"encoding/json"
+
+	"github.com/frain-dev/convoy/auth"
+)
+
+type BasicAuthConfig []BasicAuth
 
 type BasicAuth struct {
 	Username string    `json:"username"`
@@ -8,7 +14,27 @@ type BasicAuth struct {
 	Role     auth.Role `json:"role"`
 }
 
+// Decode loads in config from an env var named `CONVOY_BASIC_AUTH_CONFIG`
+func (b *BasicAuthConfig) Decode(value string) error {
+	config := BasicAuthConfig{}
+	err := json.Unmarshal([]byte(value), &config)
+
+	*b = config
+	return err
+}
+
+type APIKeyAuthConfig []APIKeyAuth
+
 type APIKeyAuth struct {
 	APIKey string    `json:"api_key"`
 	Role   auth.Role `json:"role"`
+}
+
+// Decode loads in config from an env var named `CONVOY_API_KEY_CONFIG`
+func (a *APIKeyAuthConfig) Decode(value string) error {
+	config := APIKeyAuthConfig{}
+	err := json.Unmarshal([]byte(value), &config)
+
+	*a = config
+	return err
 }
