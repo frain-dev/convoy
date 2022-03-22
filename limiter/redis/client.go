d3cbd6dcc21a88842a783c81680241618b96d139
Raymond Tukpe <rtukpe@gmail.com>
2022-03-22 17:12:58 +0100

    feat: update limiter package

--- limiter/redis/client.go
+++ limiter/redis/client.go
@@ -28,7 +28,7 @@ func NewRedisLimiter(dsn string) (*RedisLimiter, error) {
 	return r, nil
 }
 
-func (r RedisLimiter) Allow(ctx context.Context, key string, limit, duration int) (*redis_rate.Result, error) {
+func (r *RedisLimiter) Allow(ctx context.Context, key string, limit, duration int) (*redis_rate.Result, error) {
 	var d time.Duration
 
 	if duration == int(time.Hour) {
@@ -46,6 +46,31 @@ func (r RedisLimiter) Allow(ctx context.Context, key string, limit, duration int
 	}
 
 	result, err := r.limiter.Allow(ctx, key, l)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (r *RedisLimiter) ShouldAllow(ctx context.Context, key string, limit, duration int) (*redis_rate.Result, error) {
+	var d time.Duration
+
+	if duration == int(time.Hour) {
+		d = time.Hour
+	} else if duration == int(time.Minute) {
+		d = time.Minute
+	} else {
+		d = time.Second
+	}
+
+	l := redis_rate.Limit{
+		Period: d,
+		Rate:   1,
+		Burst:  1,
+	}
+
+	result, err := r.limiter.AllowN(ctx, key, l, 0)
 	if err != nil {
 		return nil, err
 	}
