83ad0a3e0db2b330c1716bba978050d868ed9b51
horlah <emmanuel.ainaj@gmail.com>
2022-03-24 00:34:14 +0100

    build app portal for vue

---
+++ web/components/app-portal-vue/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ConvoyAppVueSample } from '@/entry.esm';
+
+export default defineComponent({
+	name: 'ServeDev'
+	// components: {
+	//  ConvoyAppVueSample,
+	// }
+});
+</script>
+
+<template>
+	<div id="app">
+		<convoy-app token="CO.u6N7iWNvZdJv5ru0.j2dslvpamxFfE4HMQu2Rn4JTADYAayzfMG8wsc8aYHWogHyNiDF3KypnoRHvARj6" />
+	</div>
+</template>
---
+++ web/components/app-portal-vue/src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as ConvoyApp } from './convoy-app.vue';
