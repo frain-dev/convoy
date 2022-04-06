259f6cfe0e7192590fbe75a08f2969814759621e
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-04-06 21:33:13 +0100

    Updated angular app portal assets (#582)
    * updated all images on angular app portal
    
    * deleted assets folder and content
    
    * updated app id in events amd events delivery requests
    
    * fix for fetching deliveries for sidebar on page load
    
    * published angular convoy-app@0.0.11
    
    * made changes to dashboard init
    
    * made changes to dashboard init
    
    * published angular convoy-app@0.0.13

--- web/components/app-portal-angular/projects/convoy-app/src/lib/convoy-app.component.ts
+++ web/components/app-portal-angular/projects/convoy-app/src/lib/convoy-app.component.ts
@@ -86,7 +86,6 @@ export class ConvoyAppComponent implements OnInit {
 	showAddEndpointModal = false;
 	isCreatingNewEndpoint = false;
 	@Input('token') token!: string;
-	@Input('appId') appId!: string;
 	@Input('apiURL') apiURL: string = '';
 
 	constructor(private convyAppService: ConvoyAppService, private router: Router, private formBuilder: FormBuilder, private route: ActivatedRoute, private datePipe: DatePipe) {}
@@ -96,8 +95,8 @@ export class ConvoyAppComponent implements OnInit {
 	}
 
 	async initDashboard() {
-		await Promise.all([this.getEvents(), this.getEventDeliveries(), this.getAppDetails()]);
-
+		await Promise.all([await this.getAppDetails(), this.getEvents(), this.getEventDeliveries()]);
+	
 		// get active tab from url and apply, after getting the details from above requests so that the data is available ahead
 		this.toggleActiveTab(this.route.snapshot.queryParams.activeTab ?? 'events');
 		return;
@@ -231,7 +230,7 @@ export class ConvoyAppComponent implements OnInit {
 
 		try {
 			const eventsResponse = await this.convyAppService.request({
-				url: this.getAPIURL(`/events?appId=${this.appId || ''}&sort=AESC&page=${this.eventsPage || 1}&startDate=${startDate}&endDate=${endDate}`),
+				url: this.getAPIURL(`/events?appId=${this.appDetails?.uid || ''}&sort=AESC&page=${this.eventsPage || 1}&startDate=${startDate}&endDate=${endDate}`),
 				method: 'get',
 				token: this.token
 			});
@@ -245,9 +244,9 @@ export class ConvoyAppComponent implements OnInit {
 				this.isloadingMoreEvents = false;
 				return;
 			}
-
 			this.events = eventsResponse.data;
 			this.displayedEvents = await this.setEventsDisplayed(eventsResponse.data.content);
+			this.getEventDeliveriesForSidebar(eventsResponse.data.content[0].uid);
 			this.isloadingEvents = false;
 		} catch (error) {
 			this.isloadingEvents = false;
@@ -265,6 +264,7 @@ export class ConvoyAppComponent implements OnInit {
 			});
 
 			this.appDetails = appDetailsResponse.data;
+			
 		} catch (error) {
 			return error;
 		}
@@ -278,7 +278,7 @@ export class ConvoyAppComponent implements OnInit {
 		try {
 			const eventDeliveriesResponse = await this.convyAppService.request({
 				url: this.getAPIURL(
-					`/eventdeliveries?appId=${this.appId || ''}&eventId=${requestDetails.eventId || ''}&page=${this.eventDeliveriesPage || 1}&startDate=${startDate}&endDate=${endDate}&status=${eventDeliveryStatusFilterQuery || ''}`
+					`/eventdeliveries?appId=${this.appDetails?.uid || ''}&eventId=${requestDetails.eventId || ''}&page=${this.eventDeliveriesPage || 1}&startDate=${startDate}&endDate=${endDate}&status=${eventDeliveryStatusFilterQuery || ''}`
 				),
 				method: 'get',
 				token: this.token
