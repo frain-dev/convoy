0ae713504e5ebecdb2b1fa2cc08e0abaf57576b3
Daniel Oluojomu <danvixent@gmail.com>
2022-05-18 14:24:02 +0100

    fix: fix go vet

--- retrystrategies/exponentialBackoff_test.go
+++ retrystrategies/exponentialBackoff_test.go
@@ -11,43 +11,43 @@ func TestExponentialBackoffRetryStrategy(t *testing.T) {
 		expectedDuration time.Duration
 		attempts         uint64
 		jitterFn         JitterFn
-		millis           []uint
+		millis           []uint64
 	}{
 		{
 			name:             "no-delay-for-initial-attempt",
 			expectedDuration: time.Duration(0) * time.Millisecond,
 			attempts:         0,
-			jitterFn: func(u uint) int {
+			jitterFn: func(u uint64) int {
 				return int(u)
 			},
-			millis: []uint{0, 100, 200, 1000},
+			millis: []uint64{0, 100, 200, 1000},
 		},
 		{
 			name:             "duration-dependent-on-attempts",
 			expectedDuration: time.Duration(100) * time.Millisecond,
 			attempts:         1,
-			jitterFn: func(u uint) int {
+			jitterFn: func(u uint64) int {
 				return int(u)
 			},
-			millis: []uint{0, 100, 200, 1000},
+			millis: []uint64{0, 100, 200, 1000},
 		},
 		{
 			name:             "duration-dependent-on-attempts-wraps",
 			expectedDuration: time.Duration(5000) * time.Millisecond,
 			attempts:         7,
-			jitterFn: func(u uint) int {
+			jitterFn: func(u uint64) int {
 				return int(u)
 			},
-			millis: []uint{0, 100, 200, 1000, 2000, 5000},
+			millis: []uint64{0, 100, 200, 1000, 2000, 5000},
 		},
 		{
 			name:             "jitterfn-affects-duration",
 			expectedDuration: time.Duration(500) * time.Millisecond,
 			attempts:         1,
-			jitterFn: func(u uint) int {
+			jitterFn: func(u uint64) int {
 				return 500
 			},
-			millis: []uint{0, 100, 200, 1000},
+			millis: []uint64{0, 100, 200, 1000},
 		},
 	}
 
