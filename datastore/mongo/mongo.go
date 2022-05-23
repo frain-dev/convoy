5addb3044a33dae61df7b8bcc15c2966d354b013
Daniel Oluojomu <danvixent@gmail.com>
2022-05-23 12:53:16 +0100

    feat: add index for app_metadata.group_id in events collection

--- datastore/mongo/mongo.go
+++ datastore/mongo/mongo.go
@@ -112,6 +112,7 @@ func (c *Client) ensureMongoIndices() {
 	c.ensureIndex(EventCollection, "uid", true, nil)
 	c.ensureIndex(EventCollection, "event_type", false, nil)
 	c.ensureIndex(EventCollection, "app_metadata.uid", false, nil)
+	c.ensureIndex(EventCollection, "app_metadata.group_id", false, nil)
 	c.ensureIndex(AppCollections, "group_id", false, nil)
 	c.ensureIndex(EventDeliveryCollection, "status", false, nil)
 	c.ensureCompoundIndex(AppCollections)
