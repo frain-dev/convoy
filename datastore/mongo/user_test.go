3d877129d5a4fadc8faa437860db9bfc13777a59
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-05-29 13:07:44 +0100

    add test for user repository

--- datastore/mongo/user_test.go
+++ datastore/mongo/user_test.go
@@ -5,6 +5,8 @@ package mongo
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/frain-dev/convoy/datastore"
@@ -17,13 +19,7 @@ func Test_CreateUser(t *testing.T) {
 	defer closeFn()
 
 	userRepo := NewUserRepo(db)
-	user := &datastore.User{
-		UID:            uuid.NewString(),
-		FirstName:      "test",
-		LastName:       "test",
-		Email:          "test@test.com",
-		DocumentStatus: datastore.ActiveDocumentStatus,
-	}
+	user := generateUser(t)
 
 	require.NoError(t, userRepo.CreateUser(context.Background(), user))
 	newUser, err := userRepo.FindUserByID(context.Background(), user.UID)
@@ -32,4 +28,146 @@ func Test_CreateUser(t *testing.T) {
 	require.Equal(t, user.UID, newUser.UID)
 	require.Equal(t, user.FirstName, newUser.FirstName)
 	require.Equal(t, user.LastName, newUser.LastName)
+}
+
+func Test_FindUserByEmail(t *testing.T) {
+	db, closeFn := getDB(t)
+	defer closeFn()
+
+	userRepo := NewUserRepo(db)
+	user := generateUser(t)
+
+	_, err := userRepo.FindUserByEmail(context.Background(), user.Email)
+	
+	require.Error(t, err)
+	require.True(t, errors.Is(err, datastore.ErrUserNotFound))
+
+	require.NoError(t, userRepo.CreateUser(context.Background(), user))
+
+	newUser, err := userRepo.FindUserByEmail(context.Background(), user.Email)
+	require.NoError(t, err)
+
+	require.Equal(t, user.UID, newUser.UID)
+	require.Equal(t, user.FirstName, newUser.FirstName)
+	require.Equal(t, user.Email, newUser.Email)
+}
+
+func Test_FindUserByID(t *testing.T) {
+	db, closeFn := getDB(t)
+	defer closeFn()
+
+	userRepo := NewUserRepo(db)
+	user := generateUser(t)
+
+	_, err := userRepo.FindUserByID(context.Background(), user.UID)
+
+	require.Error(t, err)
+	require.True(t, errors.Is(err, datastore.ErrUserNotFound))
+
+	require.NoError(t, userRepo.CreateUser(context.Background(), user))
+
+	newUser, err := userRepo.FindUserByID(context.Background(), user.UID)
+	require.NoError(t, err)
+
+	require.Equal(t, user.UID, newUser.UID)
+	require.Equal(t, user.FirstName, newUser.FirstName)
+	require.Equal(t, user.Email, newUser.Email)
+}
+
+func Test_LoadUsersPaged(t *testing.T) {
+	type Expected struct {
+		paginationData datastore.PaginationData
+	}
+
+	tests := []struct {
+		name     string
+		pageData datastore.Pageable
+		count    int
+		expected Expected
+	}{
+		{
+			name:     "Load Users Paged - 10 records",
+			pageData: datastore.Pageable{Page: 1, PerPage: 3},
+			count:    10,
+			expected: Expected{
+				paginationData: datastore.PaginationData{
+					Total:     10,
+					TotalPage: 4,
+					Page:      1,
+					PerPage:   3,
+					Prev:      0,
+					Next:      2,
+				},
+			},
+		},
+
+		{
+			name:     "Load Users Paged - 12 records",
+			pageData: datastore.Pageable{Page: 2, PerPage: 4},
+			count:    12,
+			expected: Expected{
+				paginationData: datastore.PaginationData{
+					Total:     12,
+					TotalPage: 3,
+					Page:      2,
+					PerPage:   4,
+					Prev:      1,
+					Next:      3,
+				},
+			},
+		},
+
+		{
+			name:     "Load Users Paged - 5 records",
+			pageData: datastore.Pageable{Page: 1, PerPage: 3},
+			count:    5,
+			expected: Expected{
+				paginationData: datastore.PaginationData{
+					Total:     5,
+					TotalPage: 2,
+					Page:      1,
+					PerPage:   3,
+					Prev:      0,
+					Next:      2,
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			db, closeFn := getDB(t)
+			defer closeFn()
+
+			userRepo := NewUserRepo(db)
+			for i := 0; i < tc.count; i++ {
+				user := &datastore.User{
+					UID:            uuid.NewString(),
+					FirstName:      "test",
+					LastName:       "test",
+					Email:          fmt.Sprintf("%s@test.com", uuid.NewString()),
+					DocumentStatus: datastore.ActiveDocumentStatus,
+				}
+				require.NoError(t, userRepo.CreateUser(context.Background(), user))
+			}
+
+			_, pageable, err := userRepo.LoadUsersPaged(context.Background(), tc.pageData)
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expected.paginationData.Page, pageable.Page)
+			require.Equal(t, tc.expected.paginationData.PerPage, pageable.PerPage)
+			require.Equal(t, tc.expected.paginationData.Prev, pageable.Prev)
+			require.Equal(t, tc.expected.paginationData.Next, pageable.Next)
+		})
+	}
+}
+
+func generateUser(t *testing.T) *datastore.User {
+	return &datastore.User{
+		UID:            uuid.NewString(),
+		FirstName:      "test",
+		LastName:       "test",
+		Email:          fmt.Sprintf("%s@test.com", uuid.NewString()),
+		DocumentStatus: datastore.ActiveDocumentStatus,
+	}
 }
