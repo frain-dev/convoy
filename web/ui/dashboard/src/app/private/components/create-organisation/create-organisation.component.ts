52f9058b227b9e0c4ecd53ce8da2308422906803
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-06-08 22:45:13 +0100

    Organisation settings ui (#719)
    * auth integration
    
    * organisations ui
    
    * organisations ui
    
    * updates on organisation dropdwon
    
    * updated settings page
    
    * fixed http service bug
    
    * fixed http service bug
    
    * took out billing
    
    * added delete organisation
    
    * added delete modal

---
+++ web/ui/dashboard/src/app/private/components/create-organisation/create-organisation.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { GeneralService } from 'src/app/services/general/general.service';
+import { CreateOrganisationService } from './create-organisation.service';
+
+@Component({
+	selector: 'app-create-organisation',
+	templateUrl: './create-organisation.component.html',
+	styleUrls: ['./create-organisation.component.scss']
+})
+export class CreateOrganisationComponent implements OnInit {
+	@Output() closeModal = new EventEmitter<any>();
+	loading: boolean = false;
+	addOrganisationForm: FormGroup = this.formBuilder.group({
+		name: ['', Validators.required]
+	});
+	constructor(private createOrganisationService: CreateOrganisationService, private formBuilder: FormBuilder, private generalService: GeneralService) {}
+
+	ngOnInit(): void {}
+
+	close() {
+		this.closeModal.emit();
+	}
+
+	async addNewOrganisation() {
+		if (this.addOrganisationForm.invalid) {
+			(<any>this.addOrganisationForm).values(this.addOrganisationForm.controls).forEach((control: FormControl) => {
+				control?.markAsTouched();
+			});
+			return;
+		}
+		this.loading = true;
+		try {
+			const response = await this.createOrganisationService.addOrganisation(this.addOrganisationForm.value);
+			if (response.status == true) {
+				this.generalService.showNotification({ style: 'success', message: response.message });
+				this.closeModal.emit();
+			}
+			this.loading = false;
+		} catch {
+			this.loading = false;
+		}
+	}
+}
