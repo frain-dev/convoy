ad6d131b33a436813c24766a966e2eaf1a8d961b
Daniel Oluojomu <danvixent@gmail.com>
2022-06-08 01:09:18 +0100

    feat: - add TemplateName type - add more fields to Notification

--- notification/notification.go
+++ notification/notification.go
@@ -3,11 +3,15 @@ package notification
 import "context"
 
 type Notification struct {
-	Text           string
-	Email          string
-	LogoURL        string
-	TargetURL      string
-	EndpointStatus string
+	Text              string
+	Email             string
+	LogoURL           string
+	TargetURL         string
+	EndpointStatus    string
+	EmailTemplateName string
+	InviteURL         string
+	OrganisationName  string
+	InviterName       string
 }
 
 type Sender interface {
