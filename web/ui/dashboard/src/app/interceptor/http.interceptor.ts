e00a737fa5c03d1853f3de43e09522729178b461
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-06-08 21:11:51 +0100

    auth integration (#716)
    * auth integration
    
    * updates on organisation dropdwon
    
    * fixed http service bug
    
    * fixed http service bug

--- web/ui/dashboard/src/app/interceptor/http.interceptor.ts
+++ web/ui/dashboard/src/app/interceptor/http.interceptor.ts
@@ -3,10 +3,11 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { GeneralService } from '../services/general/general.service';
 
 @Injectable()
 export class HttpIntercepter implements HttpInterceptor {
-	constructor(private router: Router) {}
+	constructor(private router: Router, private generalService: GeneralService) {}
 
 	intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 		return next.handle(request).pipe(
@@ -14,7 +15,16 @@ export class HttpIntercepter implements HttpInterceptor {
 				return httpEvent;
 			}),
 			catchError((error: HttpErrorResponse) => {
-				if (error.status === 401) this.router.navigate(['/login'], { replaceUrl: true });
+				if (error.status === 401) {
+					this.router.navigate(['/login'], { replaceUrl: true });
+					localStorage.removeItem('CONVOY_AUTH');
+				}
+				let errorMessage: string;
+				error.error?.message ? (errorMessage = error.error?.message) : (errorMessage = 'An error occured, please try again');
+				this.generalService.showNotification({
+					message: errorMessage,
+					style: 'error'
+				});
 				return throwError(error);
 			})
 		);
