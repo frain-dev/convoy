91b4821e8c77ff594c4bc5f58b3560fdc527cdf1
ogbanugot <ogbanugot@gmail.com>
2022-06-11 16:55:47 +0100

    Add comments

--- util/strings.go
+++ util/strings.go
@@ -21,7 +21,6 @@ var src = rand.NewSource(time.Now().UnixNano())
 
 func GenerateRandomString(n int) (string, error) {
 	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
 			cache, remain = src.Int63(), letterIdxMax
