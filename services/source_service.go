f242c901596cc32c95df55841b3ed8e6bb5484b2
Subomi Oluwalana <subomioluwalana71@gmail.com>
2022-06-11 17:35:15 +0100

    apply validation to updatesource

--- services/source_service.go
+++ services/source_service.go
@@ -73,6 +73,17 @@ func (s *SourceService) UpdateSource(ctx context.Context, g *datastore.Group, so
 		source.IsDisabled = *sourceUpdate.IsDisabled
 	}
 
+	if sourceUpdate.Verifier.Type == datastore.HMacVerifier && sourceUpdate.Verifier.HMac == nil {
+		return nil, NewServiceError(http.StatusBadRequest, errors.New("Invalid verifier config for hmac"))
+	}
+
+	if sourceUpdate.Verifier.Type == datastore.APIKeyVerifier && sourceUpdate.Verifier.ApiKey == nil {
+		return nil, NewServiceError(http.StatusBadRequest, errors.New("Invalid verifier config for api key"))
+	}
+
+	if sourceUpdate.Verifier.Type == datastore.BasicAuthVerifier && sourceUpdate.Verifier.BasicAuth == nil {
+		return nil, NewServiceError(http.StatusBadRequest, errors.New("Invalid verifier config for basic auth"))
+	}
 	err := s.sourceRepo.UpdateSource(ctx, g.UID, source)
 	if err != nil {
 		return nil, NewServiceError(http.StatusBadRequest, errors.New("an error occurred while updating source"))
