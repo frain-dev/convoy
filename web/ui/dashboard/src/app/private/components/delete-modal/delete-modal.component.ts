e464555ef8d24ad1dca6ecd1b5d295ecc3aa1efa
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-06-24 12:44:23 +0100

    bugfixes for v6 (#786)
    * fixed project update error
    
    * removed email from reset password
    
    * copy changes, forgot password validation
    
    * updated create subscription form
    
    * added analytics
    
    * user redirect to dashboard on accepting invite
    
    * team invites updates
    
    * additional changes
    
    * moved analytics modal to middle
    
    * moved analytics modal to middle
    
    * updated analytics modal
    
    * app delete feature
    
    * updated delete app
    
    * review updates
    
    * review updates
    
    * updated org switch

---
+++ web/ui/dashboard/src/app/private/components/delete-modal/delete-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+	selector: 'app-delete-modal',
+	templateUrl: './delete-modal.component.html',
+	styleUrls: ['./delete-modal.component.scss']
+})
+export class DeleteModalComponent implements OnInit {
+	@Output() closeModal = new EventEmitter<any>();
+	@Output() deleteData = new EventEmitter<any>();
+	@Input() isLoading: boolean = false;
+	@Input() deleteText!: string;
+	constructor() {}
+
+	ngOnInit(): void {}
+}
