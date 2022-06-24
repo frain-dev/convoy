9c861eeeeaf869e9a1bddba605778dde66d39c91
horlah <emmanuel.ainaj@gmail.com>
2022-06-24 11:11:56 +0100

    install tailwind

---
+++ web/ui/dashboard/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,ts}'],
+	content: ['./src/**/*.{html,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
