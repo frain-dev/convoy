5e1090678d94cfa28c2ab392e4cc02af2b39b09d
horlah <emmanuel.ainaj@gmail.com>
2022-06-26 15:18:28 +0100

    implement custom spacing in tailwind

--- web/ui/dashboard/tailwind.config.js
+++ web/ui/dashboard/tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+function customSpacing() {
+	const maxSpace = 384;
+	// const minSpace = 2;
+	const spaces = {};
+
+	for (let i = 2; i <= maxSpace; ) {
+		const value = i + 'px';
+		spaces[value] = value;
+		i = i + 2;
+	}
+
+	return spaces;
+}
+
 module.exports = {
 	mode: 'jit',
 	purge: ['./src/**/*.{html,ts}'],
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			spacing: customSpacing()
+		},
+		screens: {
+			desktop: '1050px'
+		}
 	},
 	plugins: []
 };
