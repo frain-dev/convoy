ba21157d979569df6fed5a7ce438567a05c86b6d
oluwadaminiola <pelumioni25@gmail.com>
2022-07-05 09:45:50 +0100

    Revert "updated: updated create app form with tailwind"
    This reverts commit 350fd5bed086a333410940a39f548da5217f2a6b.

--- web/ui/dashboard/src/app/private/components/create-app/create-app.module.ts
+++ web/ui/dashboard/src/app/private/components/create-app/create-app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateAppComponent } from './create-app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from '../tooltip/tooltip.module';
 import { LoaderModule } from '../loader/loader.module';
-import { InputComponent } from 'src/app/components/input/input.component';
-import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @NgModule({
 	declarations: [CreateAppComponent],
-	imports: [CommonModule, ReactiveFormsModule, FormsModule, LoaderModule, InputComponent, ButtonComponent],
+	imports: [CommonModule, ReactiveFormsModule, FormsModule, TooltipModule, LoaderModule],
 	exports: [CreateAppComponent]
 })
 export class CreateAppModule {}
