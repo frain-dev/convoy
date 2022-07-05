981ec19828fe9cfa6a61ff70a928824499d93ac1
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-07-06 00:19:20 +0100

    Updated create app form with tailwind (#829)
    * update: updated create app form with tailwind
    
    * updated create app form with required tags

--- web/ui/dashboard/src/app/private/components/create-app/create-app.module.ts
+++ web/ui/dashboard/src/app/private/components/create-app/create-app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateAppComponent } from './create-app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TooltipModule } from '../tooltip/tooltip.module';
 import { LoaderModule } from '../loader/loader.module';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { InputComponent } from 'src/app/components/input/input.component';
 
 @NgModule({
 	declarations: [CreateAppComponent],
-	imports: [CommonModule, ReactiveFormsModule, FormsModule, TooltipModule, LoaderModule],
+	imports: [CommonModule, ReactiveFormsModule, FormsModule, ButtonComponent, InputComponent, LoaderModule],
 	exports: [CreateAppComponent]
 })
 export class CreateAppModule {}
