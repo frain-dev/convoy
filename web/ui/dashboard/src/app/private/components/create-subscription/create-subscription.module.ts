edcb958b12f615d012d1fee8af96e8f4f1c18c40
oluwadaminiola <pelumioni25@gmail.com>
2022-07-05 23:33:17 +0100

    updated subscription form with tailwind

--- web/ui/dashboard/src/app/private/components/create-subscription/create-subscription.module.ts
+++ web/ui/dashboard/src/app/private/components/create-subscription/create-subscription.module.ts
@@ -7,10 +7,28 @@ import { CreateSourceModule } from '../create-source/create-source.module';
 import { TooltipModule } from '../tooltip/tooltip.module';
 import { LoaderModule } from '../loader/loader.module';
 import { CreateEndpointModule } from '../../pages/project/apps/app-details/create-endpoint/create-endpoint.module';
+import { InputComponent } from 'src/app/components/input/input.component';
+import { SelectComponent } from 'src/app/components/select/select.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { TooltipComponent } from 'src/app/components/tooltip/tooltip.component';
+import { ToggleComponent } from 'src/app/components/toggle/toggle.component';
 
 @NgModule({
 	declarations: [CreateSubscriptionComponent],
-	imports: [CommonModule, ReactiveFormsModule, CreateAppModule, CreateSourceModule, TooltipModule, LoaderModule, CreateEndpointModule],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		CreateAppModule,
+		CreateSourceModule,
+		TooltipModule,
+		LoaderModule,
+		CreateEndpointModule,
+		InputComponent,
+		SelectComponent,
+		ButtonComponent,
+		TooltipComponent,
+		ToggleComponent
+	],
 	exports: [CreateSubscriptionComponent]
 })
 export class CreateSubscriptionModule {}
