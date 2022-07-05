a68cf45609ffc8ea5a0a4925352594673f3da510
oluwadaminiola <pelumioni25@gmail.com>
2022-07-06 00:17:59 +0100

    update: update create organisation form with tailwind

--- web/ui/dashboard/src/app/private/components/create-organisation/create-organisation.module.ts
+++ web/ui/dashboard/src/app/private/components/create-organisation/create-organisation.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateOrganisationComponent } from './create-organisation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from 'src/app/components/input/input.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @NgModule({
 	declarations: [CreateOrganisationComponent],
-	imports: [CommonModule, ReactiveFormsModule, FormsModule],
+	imports: [CommonModule, ReactiveFormsModule, FormsModule, InputComponent, ButtonComponent],
 	exports: [CreateOrganisationComponent]
 })
 export class CreateOrganisationModule {}
