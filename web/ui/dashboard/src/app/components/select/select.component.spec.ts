0e8ace21936354f494c78a9e865873a7fa27edac
oluwadaminiola <pelumioni25@gmail.com>
2022-07-05 14:07:58 +0100

    created select component

---
+++ web/ui/dashboard/src/app/components/select/select.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectComponent } from './select.component';
+
+describe('SelectComponent', () => {
+  let component: SelectComponent;
+  let fixture: ComponentFixture<SelectComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SelectComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web/ui/dashboard/src/app/components/select/select.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { TooltipComponent } from '../tooltip/tooltip.component';
+
+@Component({
+	selector: 'convoy-select',
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule, TooltipComponent],
+	templateUrl: './select.component.html',
+	styleUrls: ['./select.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => SelectComponent),
+			multi: true
+		}
+	]
+})
+export class SelectComponent implements OnInit, ControlValueAccessor {
+	@Input('options') options!: Array<any>;
+	@Input('name') name!: string;
+	@Input('errorMessage') errorMessage!: string;
+	@Input('label') label!: string;
+	@Input('formControlName') formControlName!: string;
+	@Input('required') required = false;
+	@Input('placeholder') placeholder!: string;
+	@Input('tooltipPosition') tooltipPosition!: string;
+	@Input('tooltipSize') tooltipSize!: string;
+	@Input('tooltipContent') tooltipContent!: string;
+  @Output('onChange') onChange = new EventEmitter<any>();
+	control!: any;
+	constructor(private controlContainer: ControlContainer) {}
+
+	ngOnInit(): void {
+		if (this.controlContainer.control?.get(this.formControlName)) this.control = this.controlContainer.control.get(this.formControlName);
+	}
+
+  registerOnChange() {}
+
+	registerOnTouched() {}
+
+	writeValue() {}
+
+	setDisabledState() {}
+}
