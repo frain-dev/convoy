57c18d20ee926193fa8d05d9e1aff7b4b3f2c396
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-07-06 18:00:20 +0100

    created modal component with tailwind (#838)
    * created modal component with tailwind
    
    * updated styles for modal component
    
    * added closeModal function to modal component backdrop
    
    * updated modal component button and title

---
+++ web/ui/dashboard/src/app/components/modal/modal.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+	selector: 'convoy-modal',
+	standalone: true,
+	imports: [CommonModule, ButtonComponent],
+	templateUrl: './modal.component.html',
+	styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+	@Input('position') position: 'full' | 'left' | 'right' | 'center' = 'right';
+	@Input('size') size: 'small' | 'medium' | 'large' = 'medium';
+	@Input('title') title!: string;
+	@Input('class') class!: string;
+	@Output('closeModal') closeModal = new EventEmitter<any>();
+	modalSizes = { small: 'max-w-[380px]', medium: 'max-w-[460px]', large: 'max-w-[600px]' };
+	modalType = {
+		full: `h-screen w-screen top-0 right-0 bottom-0 overflow-y-auto translate-x-0`,
+		left: `h-screen top-0 left-0 bottom-0 overflow-y-auto translate-x-0`,
+		right: `h-screen top-0 right-0 bottom-0 overflow-y-auto translate-x-0`,
+		center: `h-[400px] top-[50%] left-[50%] -translate-x-2/4 -translate-y-2/4 rounded-[16px]`
+	};
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	get classes(): string {
+		return `${this.modalType[this.position]} ${this.position === 'full' ? '' : this.modalSizes[this.size]} ${this.class}`;
+	}
+}
