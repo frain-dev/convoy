df99f009bb8c661eca3d46fbc36dd9b4d2dbec20
oluwadaminiola <pelumioni25@gmail.com>
2022-07-07 12:19:56 +0100

    updated dropdown classes

--- web/ui/dashboard/src/app/components/dropdown/dropdown.component.ts
+++ web/ui/dashboard/src/app/components/dropdown/dropdown.component.ts
@@ -10,7 +10,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DropdownComponent implements OnInit {
 	@Input('position') position: 'right' | 'left' = 'right';
-	@Input('size') size: 'sm' | 'md' | 'lg' | 'xl' = 'sm';
+	@Input('size') size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
 	@Input('class') class!: string;
 	@Input('show') show = false;
 	sizes = { sm: 'w-[140px]', md: 'w-[200px]', lg: 'w-[249px]', xl: 'w-[350px]' };
@@ -20,10 +20,8 @@ export class DropdownComponent implements OnInit {
 	ngOnInit(): void {}
 
 	get classes(): string {
-		return `absolute top-[110%] w-full bg-white-100 border border-grey-10 rounded-12px shadow-default z-10 transition-all ease-in-out duration-300 ${this.sizes[this.size]} ${this.position === 'right' ? 'right-[5%]' : 'left-[5%]'} ${
+		return `${this.sizes[this.size]} ${this.position === 'right' ? 'right-[5%]' : 'left-[5%]'} ${
 			this.show ? 'opacity-100 h-fit overflow-y-auto pointer-events-auto' : 'opacity-0 h-0 overflow-hidden pointer-events-none'
 		} ${this.class}`;
 	}
-
-
 }
