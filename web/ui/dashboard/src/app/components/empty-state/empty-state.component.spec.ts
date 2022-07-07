0d6a2dd6cda074f5841cd5096a67ab872aaf4e89
oluwadaminiola <pelumioni25@gmail.com>
2022-07-07 22:24:33 +0100

    created empty state component

---
+++ web/ui/dashboard/src/app/components/empty-state/empty-state.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmptyStateComponent } from './empty-state.component';
+
+describe('EmptyStateComponent', () => {
+  let component: EmptyStateComponent;
+  let fixture: ComponentFixture<EmptyStateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmptyStateComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmptyStateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
