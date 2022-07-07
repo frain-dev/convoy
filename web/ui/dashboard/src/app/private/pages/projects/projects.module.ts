c93ebc34ea3a0e51897bf4edcd96d2116e19e3c2
oluwadaminiola <pelumioni25@gmail.com>
2022-07-07 08:55:17 +0100

    updated organsiation modal on projects page

--- web/ui/dashboard/src/app/private/pages/projects/projects.module.ts
+++ web/ui/dashboard/src/app/private/pages/projects/projects.module.ts
@@ -5,11 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateOrganisationModule } from '../../components/create-organisation/create-organisation.module';
 import { LoaderModule } from '../../components/loader/loader.module';
 import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
 
 const routes: Routes = [{ path: '', component: ProjectsComponent }];
 
 @NgModule({
 	declarations: [ProjectsComponent],
-	imports: [CommonModule, RouterModule.forChild(routes), CreateOrganisationModule, LoaderModule, ButtonComponent]
+	imports: [CommonModule, RouterModule.forChild(routes), CreateOrganisationModule, LoaderModule, ButtonComponent, ModalComponent]
 })
 export class ProjectsModule {}
