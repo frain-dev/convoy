72364fbed9708a2c6bf218b9e890dc5bd2e318be
oluwadaminiola <pelumioni25@gmail.com>
2022-07-07 08:38:21 +0100

    updated projects page with tailwind

--- web/ui/dashboard/src/app/private/pages/projects/projects.module.ts
+++ web/ui/dashboard/src/app/private/pages/projects/projects.module.ts
@@ -4,11 +4,12 @@ import { ProjectsComponent } from './projects.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateOrganisationModule } from '../../components/create-organisation/create-organisation.module';
 import { LoaderModule } from '../../components/loader/loader.module';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 const routes: Routes = [{ path: '', component: ProjectsComponent }];
 
 @NgModule({
 	declarations: [ProjectsComponent],
-	imports: [CommonModule, RouterModule.forChild(routes), CreateOrganisationModule, LoaderModule]
+	imports: [CommonModule, RouterModule.forChild(routes), CreateOrganisationModule, LoaderModule, ButtonComponent]
 })
 export class ProjectsModule {}
