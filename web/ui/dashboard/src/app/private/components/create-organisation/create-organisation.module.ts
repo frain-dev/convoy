e904c2a5fbab7520911b1d53ef2e0238a6eb6766
oluwadaminiola <pelumioni25@gmail.com>
2022-07-08 12:23:47 +0100

    updated create organisation form to contain modal component

--- web/ui/dashboard/src/app/private/components/create-organisation/create-organisation.module.ts
+++ web/ui/dashboard/src/app/private/components/create-organisation/create-organisation.module.ts
@@ -4,10 +4,11 @@ import { CreateOrganisationComponent } from './create-organisation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from 'src/app/components/input/input.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
 
 @NgModule({
 	declarations: [CreateOrganisationComponent],
-	imports: [CommonModule, ReactiveFormsModule, FormsModule, InputComponent, ButtonComponent],
+	imports: [CommonModule, ReactiveFormsModule, FormsModule, InputComponent, ButtonComponent, ModalComponent],
 	exports: [CreateOrganisationComponent]
 })
 export class CreateOrganisationModule {}
