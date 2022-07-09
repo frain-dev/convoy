7add44c310ad4473a8a2acc6779c2a0eff613f3a
oluwadaminiola <pelumioni25@gmail.com>
2022-07-09 12:51:18 +0100

    updated subscriptions page with tailwind

--- web/ui/dashboard/src/app/private/pages/project/subscriptions/subscriptions.module.ts
+++ web/ui/dashboard/src/app/private/pages/project/subscriptions/subscriptions.module.ts
@@ -4,11 +4,13 @@ import { SubscriptionsComponent } from './subscriptions.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateSubscriptionModule } from 'src/app/private/components/create-subscription/create-subscription.module';
 import { DeleteModalModule } from 'src/app/private/components/delete-modal/delete-modal.module';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
 
 const routes: Routes = [{ path: '', component: SubscriptionsComponent }];
 
 @NgModule({
 	declarations: [SubscriptionsComponent],
-	imports: [CommonModule, RouterModule.forChild(routes), CreateSubscriptionModule, DeleteModalModule]
+	imports: [CommonModule, RouterModule.forChild(routes), CreateSubscriptionModule, DeleteModalModule, ButtonComponent, ModalComponent]
 })
 export class SubscriptionsModule {}
