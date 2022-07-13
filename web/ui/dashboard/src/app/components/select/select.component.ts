f902d68ce641122423beb982fdd8b269f1d50f97
oluwadaminiola <pelumioni25@gmail.com>
2022-07-13 13:45:33 +0100

    updated select component tooltip specs

--- web/ui/dashboard/src/app/components/select/select.component.ts
+++ web/ui/dashboard/src/app/components/select/select.component.ts
@@ -4,44 +4,44 @@ import { ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveForm
 import { TooltipComponent } from '../tooltip/tooltip.component';
 
 @Component({
-    selector: 'convoy-select',
-    standalone: true,
-    imports: [CommonModule, ReactiveFormsModule, TooltipComponent],
-    templateUrl: './select.component.html',
-    styleUrls: ['./select.component.scss'],
-    providers: [
-      {
-        provide: NG_VALUE_ACCESSOR,
-        useExisting: forwardRef(() => SelectComponent),
-        multi: true
-      }
-    ]
+	selector: 'convoy-select',
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule, TooltipComponent],
+	templateUrl: './select.component.html',
+	styleUrls: ['./select.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => SelectComponent),
+			multi: true
+		}
+	]
 })
 export class SelectComponent implements OnInit, ControlValueAccessor {
-    @Input('options') options!: Array<any>;
-    @Input('name') name!: string;
-    @Input('errorMessage') errorMessage!: string;
-    @Input('label') label!: string;
-    @Input('formControlName') formControlName!: string;
-    @Input('required') required = false;
-    @Input('placeholder') placeholder!: string;
-    @Input('class') class!: string;
-    @Input('tooltipPosition') tooltipPosition!: string;
-    @Input('tooltipSize') tooltipSize!: string;
-    @Input('tooltipContent') tooltipContent!: string;
-    @Output('onChange') onChange = new EventEmitter<any>();
-    control!: any;
-    constructor(private controlContainer: ControlContainer) {}
+	@Input('options') options!: Array<any>;
+	@Input('name') name!: string;
+	@Input('errorMessage') errorMessage!: string;
+	@Input('label') label!: string;
+	@Input('formControlName') formControlName!: string;
+	@Input('required') required = false;
+	@Input('placeholder') placeholder!: string;
+	@Input('className') class!: string;
+	@Input('tooltipPosition') tooltipPosition: 'left' | 'right' = 'left';
+	@Input('tooltipSize') tooltipSize: 'sm' | 'md' = 'md';
+	@Input('tooltipContent') tooltipContent!: string;
+	@Output('onChange') onChange = new EventEmitter<any>();
+	control!: any;
+	constructor(private controlContainer: ControlContainer) {}
 
-    ngOnInit(): void {
-      if (this.controlContainer.control?.get(this.formControlName)) this.control = this.controlContainer.control.get(this.formControlName);
-    }
+	ngOnInit(): void {
+		if (this.controlContainer.control?.get(this.formControlName)) this.control = this.controlContainer.control.get(this.formControlName);
+	}
 
-    registerOnChange() {}
+	registerOnChange() {}
 
-    registerOnTouched() {}
+	registerOnTouched() {}
 
-    writeValue() {}
+	writeValue() {}
 
-    setDisabledState() {}
+	setDisabledState() {}
 }
