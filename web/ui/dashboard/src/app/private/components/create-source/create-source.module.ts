8b9ff851a4cde68b5d22adb0bbf456f05e2f0961
oluwadaminiola <pelumioni25@gmail.com>
2022-07-14 03:28:25 +0100

    removed modal from create source component

--- web/ui/dashboard/src/app/private/components/create-source/create-source.module.ts
+++ web/ui/dashboard/src/app/private/components/create-source/create-source.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateSourceComponent } from './create-source.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ModalComponent } from 'src/app/components/modal/modal.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
 import { InputComponent } from 'src/app/components/input/input.component';
 import { SelectComponent } from 'src/app/components/select/select.component';
@@ -11,7 +10,7 @@ import { CardComponent } from 'src/app/components/card/card.component';
 
 @NgModule({
 	declarations: [CreateSourceComponent],
-	imports: [CommonModule, ReactiveFormsModule, ModalComponent, ButtonComponent, InputComponent, SelectComponent, RadioComponent, CardComponent],
+	imports: [CommonModule, ReactiveFormsModule, ButtonComponent, InputComponent, SelectComponent, RadioComponent, CardComponent],
 	exports: [CreateSourceComponent]
 })
 export class CreateSourceModule {}
