09c8ec535dd0162bfa3aa3e03ac4a3492dbba0ac
Daniel Oluojomu <danvixent@gmail.com>
2022-07-21 21:03:12 +0100

    fix: fix double prometheus registration

--- internal/pkg/metrics/metrics.go
+++ internal/pkg/metrics/metrics.go
@@ -27,9 +27,11 @@ func Reg() *prometheus.Registry {
 	return reg
 }
 
+// Reset is only intended for use in tests
 func Reset() {
 	requestDuration, reg = nil, nil
 	re, rd = sync.Once{}, sync.Once{}
+	prometheus.DefaultRegisterer = prometheus.NewRegistry()
 }
 
 func RequestDuration() *prometheus.HistogramVec {
