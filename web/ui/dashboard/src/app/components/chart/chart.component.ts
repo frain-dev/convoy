58648c1694b7622eb2b46e99ebf92b62921acf6b
oluwadaminiola <pelumioni25@gmail.com>
2022-07-25 10:35:46 +0100

    added pagination for chartdata in chart component

--- web/ui/dashboard/src/app/components/chart/chart.component.ts
+++ web/ui/dashboard/src/app/components/chart/chart.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { CHARTDATA } from 'src/app/models/global.model';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
 	selector: 'convoy-chart',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, ButtonComponent],
 	templateUrl: './chart.component.html',
 	styleUrls: ['./chart.component.scss']
 })
@@ -13,10 +14,41 @@ export class ChartComponent implements OnInit {
     @Input('chartData') chartData!: CHARTDATA[];
 	@Input('isLoading') isLoading: boolean = false;
 	@Input('frequency') frequency: 'daily' | 'weekly' | 'monthly' | 'yearly' = 'daily';
+    paginatedData!: CHARTDATA[];
+	pageSize = 30;
+	pageNumber = 1;
+	pages = 1;
 
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit() {
+		this.paginateChartData();
+	}
+
+	generateRandomHeight(maxHeight: number) {
+		const randomNum = Math.floor(Math.random() * maxHeight);
+		return randomNum;
+	}
+
+	paginateChartData() {
+		this.pages = Math.ceil(this.chartData.length / this.pageSize);
+		this.paginate();
+	}
+
+	prevPage() {
+		if (this.pageNumber === 1) return;
+		this.pageNumber--;
+		this.paginate();
+	}
+	nextPage() {
+		if (this.pageNumber === this.pages) return;
+		this.pageNumber++;
+		this.paginate();
+	}
+
+	paginate() {
+		this.paginatedData = this.chartData.slice((this.pageNumber - 1) * this.pageSize, this.pageNumber * this.pageSize);
+	}
 
 	counter(i: number) {
 		return new Array(i);
