2d3ccc7defd61df7f178f1446be2c0b756f771d2
oluwadaminiola <pelumioni25@gmail.com>
2022-08-11 07:32:01 +0100

    tooltip component in stories

---
+++ web/ui/dashboard/src/stories/tooltip/tooltip.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TooltipComponent } from './tooltip.component';
+
+describe('TooltipComponent', () => {
+  let component: TooltipComponent;
+  let fixture: ComponentFixture<TooltipComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TooltipComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TooltipComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
