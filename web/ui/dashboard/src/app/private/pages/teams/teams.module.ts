10367a0865beef3014bb3e23d2383279423ead79
oluwadaminiola <pelumioni25@gmail.com>
2022-08-12 11:35:26 +0100

    updated bage imports

--- web/ui/dashboard/src/app/private/pages/teams/teams.module.ts
+++ web/ui/dashboard/src/app/private/pages/teams/teams.module.ts
@@ -9,7 +9,7 @@ import { PageComponent } from 'src/app/components/page/page.component';
 import { ModalComponent } from 'src/app/components/modal/modal.component';
 import { DropdownComponent } from 'src/app/components/dropdown/dropdown.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
-import { BadgeComponent } from 'src/app/components/badge/badge.component';
+import { BadgeComponent } from 'src/stories/badge/badge.component';
 import { EmptyStateComponent } from 'src/app/components/empty-state/empty-state.component';
 import { InputComponent } from 'src/app/components/input/input.component';
 import { CardComponent } from 'src/app/components/card/card.component';
