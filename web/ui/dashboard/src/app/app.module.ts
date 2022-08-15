0a2990e5d83a0f14d494e53541f71e1f77317903
oluwadaminiola <pelumioni25@gmail.com>
2022-08-15 21:05:20 +0100

    removed notification component from stories, updated import

--- web/ui/dashboard/src/app/app.module.ts
+++ web/ui/dashboard/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NotificationComponent } from 'src/stories/notification/notification.component';
+import { NotificationComponent } from 'src/app/components/notification/notification.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
