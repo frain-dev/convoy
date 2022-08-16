9829cee6746433d2670abdf5dd33145d9602bd5a
oluwadaminiola <pelumioni25@gmail.com>
2022-08-16 12:32:45 +0100

    updated toggle stories

--- web/ui/dashboard/src/stories/toggle/Toggle.stories.ts
+++ web/ui/dashboard/src/stories/toggle/Toggle.stories.ts
@@ -1,23 +1,55 @@
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Story, Meta } from '@storybook/angular/types-6-0';
-import { ToggleComponent } from './toggle.component';
+import { moduleMetadata } from '@storybook/angular';
+import { ToggleComponent } from '../../app/components/toggle/toggle.component';
 
 export default {
 	title: 'Example/Toggle',
 	component: ToggleComponent,
+    decorators: [
+		moduleMetadata({
+			imports: [ReactiveFormsModule]
+		})
+	],
 	argTypes: {
 		isChecked: {
 			control: { type: 'boolean' }
 		},
 		label: {
 			control: { type: 'string' }
+		},
+		formControlName: {
+			control: { type: 'text' }
 		}
 	}
 } as Meta;
-const Template: Story<ToggleComponent> = (args: ToggleComponent) => ({
-	props: args
-});
+
+const Template: Story<ToggleComponent> = (args: ToggleComponent) => {
+	const formGroup = new FormGroup({
+		toggleForm: new FormControl(undefined)
+	});
+
+	return {
+		component: ToggleComponent,
+		template: `
+        <form [formGroup]="form">
+          <convoy-toggle
+            [label]="label"
+            [isChecked]="isChecked"
+            formControlName="toggleForm"
+          >
+          </convoy-toggle>
+        </form>
+      `,
+		props: {
+			...args,
+			form: formGroup
+		}
+	};
+};
 
 export const Base = Template.bind({});
 Base.args = {
-	isChecked: false
+	isChecked: false,
+    label: 'Convoy toggle'
 };
