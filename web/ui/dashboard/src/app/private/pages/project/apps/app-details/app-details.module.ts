93faf232398ac55f732f1c98fd4760f6e4d17645
oluwadaminiola <pelumioni25@gmail.com>
2022-09-15 22:45:24 +0100

    updated app details component with cli keys and devices

--- web/ui/dashboard/src/app/private/pages/project/apps/app-details/app-details.module.ts
+++ web/ui/dashboard/src/app/private/pages/project/apps/app-details/app-details.module.ts
@@ -9,10 +9,11 @@ import { EmptyStateComponent } from 'src/app/components/empty-state/empty-state.
 import { ListItemComponent } from 'src/app/components/list-item/list-item.component';
 import { ModalComponent } from 'src/app/components/modal/modal.component';
 import { SkeletonLoaderComponent } from 'src/app/components/skeleton-loader/skeleton-loader.component';
-import { CliComponent } from './cli/cli.component';
 import { SendEventComponent } from './send-event/send-event.component';
 import { CopyButtonComponent } from 'src/app/components/copy-button/copy-button.component';
 import { DeleteModalComponent } from 'src/app/private/components/delete-modal/delete-modal.component';
+import { CliKeysComponent } from './cli-keys/cli-keys.component';
+import { DevicesComponent } from './devices/devices.component';
 
 const routes: Routes = [
 	{
@@ -35,7 +36,8 @@ const routes: Routes = [
 		CreateEndpointModule,
         SendEventComponent,
 		CopyButtonComponent,
-        CliComponent,
+        CliKeysComponent,
+        DevicesComponent,
 		RouterModule.forChild(routes)
 	]
 })
