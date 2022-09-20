bc868762499c6b122b8c90a306942d87bac305d2
Raymond Tukpe <rtukpe@gmail.com>
2022-09-20 13:11:20 +0100

    fix: return nil when metadata fields are emtpy

--- datastore/models.go
+++ datastore/models.go
@@ -527,9 +527,9 @@ type Subscription struct {
 	SourceID   string             `json:"-" bson:"source_id"`
 	EndpointID string             `json:"-" bson:"endpoint_id"`
 
-	Source   *Source      `json:"source_metadata,omitempty" bson:"-"`
-	Endpoint *Endpoint    `json:"endpoint_metadata,omitempty" bson:"-"`
-	App      *Application `json:"app_metadata,omitempty" bson:"-"`
+	Source   *Source      `json:"source_metadata" bson:"-"`
+	Endpoint *Endpoint    `json:"endpoint_metadata" bson:"-"`
+	App      *Application `json:"app_metadata" bson:"-"`
 
 	// subscription config
 	AlertConfig  *AlertConfiguration  `json:"alert_config,omitempty" bson:"alert_config,omitempty"`
