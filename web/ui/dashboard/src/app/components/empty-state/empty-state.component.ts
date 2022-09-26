4bba6f6e0e05c16e36af6e5ab21cf1e173378c47
oluwadaminiola <pelumioni25@gmail.com>
2022-09-26 11:16:40 +0100

    updated empty state, radio and tooltip components

--- web/ui/dashboard/src/app/components/empty-state/empty-state.component.ts
+++ web/ui/dashboard/src/app/components/empty-state/empty-state.component.ts
@@ -11,6 +11,7 @@ import { ButtonComponent } from '../button/button.component'
 })
 export class EmptyStateComponent implements OnInit {
 	@Input('imgSrc') imgSrc!: string;
+	@Input('imgHeight') imgHeight!: string;
 	@Input('heading') heading!: string;
 	@Input('description') description!: string;
 	@Input('buttonText') buttonText!: string;
--- web/ui/dashboard/src/app/components/tooltip/tooltip.component.ts
+++ web/ui/dashboard/src/app/components/tooltip/tooltip.component.ts
@@ -10,11 +10,19 @@ import { ButtonComponent } from '../button/button.component';
 	styleUrls: ['./tooltip.component.scss']
 })
 export class TooltipComponent implements OnInit {
-	@Input('size') size: 'sm'| 'md' = 'md';
-	@Input('position') position: 'left'| 'right' = 'left';
+	@Input('size') size: 'sm' | 'md' = 'md';
+	@Input('position') position: 'left' | 'right' = 'left';
 	@Input('img') img!: string;
+	@Input('tooltipContent') tooltipContent!: string;
+	@Input('type') type?: 'primary' | 'white' = 'primary';
 
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	get classes(): string {
+		return `${this.position === 'right' ? '-right-[160px] after:right-[157px]' : '-right-[16px] after:right-[15px]'} ${
+			this.type === 'primary' ? 'bg-primary-100 after:border-t-primary-100 bottom-[30px] text-white-100 w-192px' : 'shadow-[0px_20px_25px_-5px_rgba(51,65,85,0.1),0px_10px_10px_-5px_rgba(51,65,85,0.04)] bg-white-100 rounded-bl-[0] bottom-[calc(100%+20px)] text-black after:border-t-white-100 after:left-0 after:w-20px'
+		}`;
+	}
 }
