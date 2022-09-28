12abd460c1df9dfd950f2eef2b2f12837d8bc9ef
Daniel Oluojomu <danvixent@gmail.com>
2022-09-28 14:31:57 +0100

    feat: add source id filter to get events (#1043)

--- analytics/event_analytics_test.go
+++ analytics/event_analytics_test.go
@@ -22,7 +22,6 @@ func provideEventAnalytics(ctrl *gomock.Controller) *EventAnalytics {
 }
 
 func Test_TrackEventAnalytics(t *testing.T) {
-
 	tests := []struct {
 		name    string
 		dbFn    func(ea *EventAnalytics)
@@ -37,7 +36,7 @@ func Test_TrackEventAnalytics(t *testing.T) {
 				gomock.InOrder(
 					orgRepo.EXPECT().LoadOrganisationsPaged(gomock.Any(), gomock.Any()).Return([]datastore.Organisation{{UID: "123"}}, datastore.PaginationData{}, nil),
 					groupRepo.EXPECT().LoadGroups(gomock.Any(), gomock.Any()).Return([]*datastore.Group{{UID: "123456", Name: "test-group"}}, nil),
-					eventRepo.EXPECT().LoadEventsPaged(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, datastore.PaginationData{}, nil),
+					eventRepo.EXPECT().LoadEventsPaged(gomock.Any(), gomock.Any()).Return(nil, datastore.PaginationData{}, nil),
 					orgRepo.EXPECT().LoadOrganisationsPaged(gomock.Any(), gomock.Any()).Return(nil, datastore.PaginationData{}, nil),
 				)
 			},
