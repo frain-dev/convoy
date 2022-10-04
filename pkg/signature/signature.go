f5209051a2aa56581fb3579e1b7ea5154a181490
Subomi Oluwalana <subomioluwalana71@gmail.com>
2022-10-03 19:37:56 -0700

    feat: completed generating simple signatures

--- pkg/signature/signature.go
+++ pkg/signature/signature.go
@@ -1,6 +1,29 @@
 package main
 
-import "encoding/json"
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"hash"
+)
+
+var (
+	// ErrFailedToEncodePayload is the error we return when we cannot encode webhook payload
+	ErrFailedToEncodePayload = errors.New("Failed to encode payload")
+
+	// ErrInvalidEncoding is the error returned when an invalid encoding is provided.
+	// TODO(subomi): Can I use this format where I'm using this error
+	ErrInvalidEncoding = errors.New("Invalid encoding format - %s")
+
+	// ErrInvalidHash is the error returned when a unsupported hash is supplied.
+	ErrInvalidHash = errors.New("Hash not supported")
+)
 
 type Scheme struct {
 	Secret   []string
@@ -14,14 +37,89 @@ type Signature struct {
 	// The order of this Schemes is a core part of this API.
 	// We use the index as the version number. That is:
 	// Index 0 = v0, Index 1 = v1
-	Schemes   []Scheme
-	Versioned bool
-	Timestamp bool
+	Schemes []Scheme
+
+	// This flag allows for backward-compatible implemtation
+	// of this type. You're either generating a simplistic header
+	// or a complex header.
+	Advanced bool
 
 	// Cached value
 	computedValue string
 }
 
 func (s *Signature) ComputeHeaderValue() (string, error) {
-	return "", nil
+	fmt.Println(string(s.Payload))
+	buf := bytes.NewBuffer([]byte{})
+	encoder := json.NewEncoder(buf)
+	encoder.SetEscapeHTML(false)
+
+	err := encoder.Encode(s.Payload)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(buf.String())
+
+	tBuf := bytes.TrimSuffix(buf.Bytes(), []byte("\n"))
+
+	sch := s.Schemes[len(s.Schemes)-1]
+	sec := sch.Secret[len(sch.Secret)-1]
+
+	var hStr string
+	switch sch.Encoding {
+	case "hex":
+		if hStr, err = s.generateHexSignature(sch.Hash, sec, tBuf); err != nil {
+			return "", err
+		}
+	case "base64":
+		if hStr, err = s.generateBase64Signature(sch.Hash, sec, tBuf); err != nil {
+			return "", err
+		}
+	default:
+		return "", ErrInvalidEncoding
+	}
+
+	return hStr, nil
+}
+
+func (s *Signature) generateHexSignature(hash, secret string, buf []byte) (string, error) {
+	h, err := s.signPayload(hash, secret, buf)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h), nil
+}
+
+func (s *Signature) generateBase64Signature(hash, secret string, buf []byte) (string, error) {
+	h, err := s.signPayload(hash, secret, buf)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(h), nil
+}
+
+func (s *Signature) signPayload(hash, secret string, buf []byte) ([]byte, error) {
+	fn, err := s.getHashFunction(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	h := hmac.New(fn, []byte(secret))
+	h.Write(buf)
+
+	return h.Sum(nil), nil
+}
+
+func (s *Signature) getHashFunction(algo string) (func() hash.Hash, error) {
+	switch algo {
+	case "SHA256":
+		return sha256.New, nil
+	case "SHA512":
+		return sha512.New, nil
+	default:
+		return nil, ErrInvalidHash
+	}
 }
--- pkg/signature/signature_test.go
+++ pkg/signature/signature_test.go
@@ -8,53 +8,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_ComputeHeaderValue(t *testing.T) {
+func Test_Advanced_Signatures(t *testing.T) {
 	tests := map[string]struct {
-		signature     *Signature
-		assertion     require.ComparisonAssertionFunc
-		expectedValue string
-		expectedError error
+		signature *Signature
+		assertion require.ComparisonAssertionFunc
+		expected  string
 	}{
-		"should_generate_non_versioned_signature": {
-			signature: &Signature{
-				Payload: json.RawMessage("Test Payload Body"),
-				Schemes: []Scheme{
-					{
-						Secret:   []string{"secret"},
-						Hash:     "SHA512",
-						Encoding: "base64",
-					},
-				},
-				Versioned: false,
-				Timestamp: false,
-			},
-			assertion:     require.Equal,
-			expectedValue: "4Jl5BpIybLLyS0Fha6xQrONto2+t+VK9emOp60D3keV2YOtQZC9FL5zCy6MoYiJk9+H0HwZN7iKK+M9S6oUfrg==",
-			expectedError: nil,
-		},
-		"should_generate_non_versioned_signature_with_last_scheme": {
-			signature: &Signature{
-				Payload: json.RawMessage("Test Payload Body"),
-				Schemes: []Scheme{
-					{
-						Secret:   []string{"secret"},
-						Hash:     "SHA512",
-						Encoding: "hex",
-					},
-					{
-						Secret:   []string{"secret"},
-						Hash:     "SHA512",
-						Encoding: "base64",
-					},
-				},
-				Versioned: false,
-				Timestamp: false,
-			},
-			assertion:     require.Equal,
-			expectedValue: "4Jl5BpIybLLyS0Fha6xQrONto2+t+VK9emOp60D3keV2YOtQZC9FL5zCy6MoYiJk9+H0HwZN7iKK+M9S6oUfrg==",
-			expectedError: nil,
-		},
-		"should_generate_multiple_values_for_rolled_secrets": {
+		"should_generate_multiple_signatures_for_rolled_secrets": {
 			signature: &Signature{
 				Payload: json.RawMessage("Test Payload Body"),
 				Schemes: []Scheme{
@@ -64,17 +24,15 @@ func Test_ComputeHeaderValue(t *testing.T) {
 						Encoding: "hex",
 					},
 				},
-				Versioned: true,
-				Timestamp: true,
+				Advanced: true,
 			},
 			assertion: require.Equal,
-			expectedValue: `
+			expected: `
 				v0=e88d7e30fa711ac90e4c38710764e37e7dedd274d32bdef49f6627ea6d63d5f1,
 				v0=e438ab8eeeeb423cac6c5fa664d70116e72ec40c2416e912711cf4911fe06515
 			`,
-			expectedError: nil,
 		},
-		"should_generate_multiple_versions": {
+		"should_generate_multiple_signatures_for_multiple_schemes": {
 			signature: &Signature{
 				Payload: json.RawMessage("Test Payload Body"),
 				Schemes: []Scheme{
@@ -89,17 +47,15 @@ func Test_ComputeHeaderValue(t *testing.T) {
 						Encoding: "base64",
 					},
 				},
-				Versioned: true,
-				Timestamp: true,
+				Advanced: true,
 			},
 			assertion: require.Equal,
-			expectedValue: `
+			expected: `
 				v1=vh3shYSLzp2RdCAG4c+gtzkehvKA9yoC7VHS1M6GmpUSMaFRRr9UV+Vy7hdTtlbjMFA5ghC1Rl6+J0wLCwu3Cg==,
 				v0=6caf1bbbda9764281d0160dfa9a401c15186c8c3730e00e0276d894bace2f441
 			`,
-			expectedError: nil,
 		},
-		"should_map_scheme_to_version_correctly": {
+		"should_map_signatures_to_schemes_correctly": {
 			signature: &Signature{
 				Payload: json.RawMessage("Test Payload Body"),
 				Schemes: []Scheme{
@@ -114,11 +70,9 @@ func Test_ComputeHeaderValue(t *testing.T) {
 						Encoding: "base64",
 					},
 				},
-				Versioned: true,
-				Timestamp: true,
+				Advanced: true,
 			},
-			assertion:     assertVersionMapping,
-			expectedError: nil,
+			assertion: assertVersionMapping,
 		},
 		"should_include_timestamp_in_computed_value": {
 			signature: &Signature{
@@ -130,12 +84,10 @@ func Test_ComputeHeaderValue(t *testing.T) {
 						Encoding: "hex",
 					},
 				},
-				Versioned: true,
-				Timestamp: true,
+				Advanced: true,
 			},
-			assertion:     require.Contains,
-			expectedValue: "t=",
-			expectedError: nil,
+			assertion: require.Contains,
+			expected:  "t=",
 		},
 	}
 
@@ -148,7 +100,80 @@ func Test_ComputeHeaderValue(t *testing.T) {
 			require.NoError(t, err)
 
 			// Assert.
-			tc.assertion(t, strings.Join(strings.Fields(tc.expectedValue), ""), sig)
+			tc.assertion(t, strings.Join(strings.Fields(tc.expected), ""), sig)
+		})
+	}
+}
+
+func Test_Simple_Signatures(t *testing.T) {
+	tests := map[string]struct {
+		signature *Signature
+		expected  string
+		assertion require.ComparisonAssertionFunc
+	}{
+		"should_generate_simple_hex_signature": {
+			signature: &Signature{
+				Payload: json.RawMessage(`{"b": {}, "e": "123", "a": 1}`),
+				Schemes: []Scheme{
+					{
+						Secret:   []string{"secret"},
+						Hash:     "SHA512",
+						Encoding: "hex",
+					},
+				},
+				Advanced: false,
+			},
+			assertion: require.Equal,
+			expected: "c5dcfeda3f5a3154145234b2d0a533fc2b230f88da0fac0724619fd5" +
+				"cdde673ad6c474dfd5c02367768bd3b3bd3595cc860e606a37e2f9362e054d5aa14d7cc4",
+		},
+		"should_generate_simple_base64_signature": {
+			signature: &Signature{
+				Payload: json.RawMessage(`{"b": {}, "e": "123", "a": 1}`),
+				Schemes: []Scheme{
+					{
+						Secret:   []string{"secret"},
+						Hash:     "SHA512",
+						Encoding: "base64",
+					},
+				},
+				Advanced: false,
+			},
+			assertion: require.Equal,
+			expected:  "xdz+2j9aMVQUUjSy0KUz/CsjD4jaD6wHJGGf1c3eZzrWxHTf1cAjZ3aL07O9NZXMhg5gajfi+TYuBU1aoU18xA==",
+		},
+		"should_generate_simple_signature_with_last_item_from_multiple_schemes": {
+			signature: &Signature{
+				Payload: json.RawMessage(`{"b": {}, "e": "123", "a": 1}`),
+				Schemes: []Scheme{
+					{
+						Secret:   []string{"secret"},
+						Hash:     "SHA512",
+						Encoding: "hex",
+					},
+					{
+						Secret:   []string{"secret"},
+						Hash:     "SHA512",
+						Encoding: "base64",
+					},
+				},
+				Advanced: false,
+			},
+			assertion: require.Equal,
+			expected:  "xdz+2j9aMVQUUjSy0KUz/CsjD4jaD6wHJGGf1c3eZzrWxHTf1cAjZ3aL07O9NZXMhg5gajfi+TYuBU1aoU18xA==",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			// Arrange
+
+			// Act.
+			sig, err := tc.signature.ComputeHeaderValue()
+			require.NoError(t, err)
+
+			// Assert.
+			tc.assertion(t, tc.expected, sig)
 		})
 	}
 }
