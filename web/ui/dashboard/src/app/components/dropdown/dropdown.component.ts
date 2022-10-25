d197e8abe7872af0690af422d9fd9504e62133e1
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2022-10-25 03:57:55 +0100

    Fix: ui fixes (#1087)
    * updated account settings menu
    
    * update:update dropdown to accept custom toggle
    
    * fix: fix create app bug
    
    * update:update personal setitngs page with updated select form
    
    * update:update select form ui
    
    * update:update components using select form
    
    * update:ui updates
    
    Co-authored-by: Emmanuel Aina <emmanuel.ainaj@gmail.com>

--- web/ui/dashboard/src/app/components/dropdown/dropdown.component.ts
+++ web/ui/dashboard/src/app/components/dropdown/dropdown.component.ts
@@ -11,17 +11,17 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class DropdownComponent implements OnInit {
 	@Input('onSelectOption') onSelectOption = new EventEmitter();
-	@Input('position') position: 'right' | 'left' = 'right';
-	@Input('size') size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
+	@Input('position') position: 'right' | 'left' | 'center' = 'right';
+	@Input('size') size: 'sm' | 'md' | 'lg' | 'xl' | 'full' = 'md';
 	@Input('active') active: boolean = false;
 	@Input('className') class!: string;
 	@Input('buttonText') buttonText!: string;
 	@Input('buttonClass') buttonClass = '';
 	@Input('buttonColor') buttonColor!: 'primary' | 'success' | 'warning' | 'danger' | 'grey';
 	@Input('buttonSize') buttonSize: 'sm' | 'md' | 'lg' = 'md';
-	@Input('buttonType') buttonType: 'default' | 'outline' | 'clear' | 'text' | 'link' = 'default';
+	@Input('buttonType') buttonType: 'default' | 'outline' | 'clear' | 'text' | 'link' | 'unstyled' = 'default';
 	@Input('buttonTexture') buttonTexture: 'deep' | 'light' = 'deep';
-	sizes = { sm: 'w-[140px]', md: 'w-[200px]', lg: 'w-[249px]', xl: 'w-[350px]' };
+	sizes = { sm: 'w-[140px]', md: 'w-[200px]', lg: 'w-[249px]', xl: 'w-[350px]', full: 'w-full' };
 	show = false;
 
 	constructor() {}
@@ -29,10 +29,15 @@ export class DropdownComponent implements OnInit {
 	ngOnInit(): void {}
 
 	get classes(): string {
-		return `${this.sizes[this.size]} ${this.position === 'right' ? 'right-[5%]' : 'left-[5%]'} ${this.show ? 'opacity-100 h-fit overflow-y-auto pointer-events-auto' : 'opacity-0 h-0 overflow-hidden pointer-events-none'} ${this.class}`;
+		const positions = {
+			right: 'right-[5%]',
+			left: 'left-[5%]',
+			center: 'left-0'
+		};
+		return `${this.sizes[this.size]} ${positions[this.position]} ${this.show ? 'opacity-100 h-fit overflow-y-auto pointer-events-auto' : 'opacity-0 h-0 overflow-hidden pointer-events-none'} ${this.class}`;
 	}
 
 	get buttonClasses(): string {
-		return `${this.active ? 'text-primary-100 !bg-primary-500' : ''} ${this.buttonClass}`;
+		return `${this.active ? 'text-primary-100 !bg-primary-500' : ''} empty:hidden ${this.buttonClass}`;
 	}
 }
