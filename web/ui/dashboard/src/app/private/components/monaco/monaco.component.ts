2e74060848b19506c2cc6fcb5e79ff33700f7ace
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-11-03 17:55:58 +0100

    create component for editing for snippet

---
+++ web/ui/dashboard/src/app/private/components/monaco/monaco.component.ts
@@ -0,0 +1,46 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MonacoService } from './monaco.service';
+import { first } from 'rxjs/operators';
+declare var monaco: typeof import('monaco-editor');
+
+@Component({
+	selector: 'convoy-monaco',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './monaco.component.html',
+	styleUrls: ['./monaco.component.scss']
+})
+export class MonacoComponent implements AfterViewInit {
+	public _editor: any;
+	@ViewChild('editorContainer', { static: true }) _editorContainer!: ElementRef;
+
+	constructor(private monacoService: MonacoService) {}
+
+	ngAfterViewInit(): void {
+		this.initMonaco();
+		this.monacoService.load();
+	}
+
+	private initMonaco(): void {
+		if (!this.monacoService.loaded) {
+			this.monacoService.loadingFinished.pipe(first()).subscribe(() => {
+				this.initMonaco();
+			});
+			return;
+		}
+
+		this._editor = monaco.editor.create(this._editorContainer.nativeElement, {
+			value: ['{', '\t"test": "works?"', '}'].join('\n'),
+			language: 'json',
+			formatOnPaste: true,
+			formatOnType: true,
+			minimap: { enabled: false }
+		});
+	}
+
+	// call this.monacoComponent.getValue() to get value of the editor
+	public getValue(): string {
+		return this._editor.getValue();
+	}
+}
