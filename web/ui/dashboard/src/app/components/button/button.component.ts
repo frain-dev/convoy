56baaa84fdfbb72036e04b37e9f6130a2606257e
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-11-04 04:21:40 +0100

    complete button rewrite and implementation

--- web/ui/dashboard/src/app/components/button/button.component.ts
+++ web/ui/dashboard/src/app/components/button/button.component.ts
@@ -2,21 +2,20 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output
 import { CommonModule } from '@angular/common';
 
 @Component({
-	selector: 'convoy-button',
+	selector: 'convoy-button, [convoy-button]',
 	standalone: true,
+	host: { class: 'flex items-center justify-center disabled:opacity-50', '[class]': 'classes' },
 	imports: [CommonModule],
 	templateUrl: './button.component.html',
 	styleUrls: ['./button.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnInit {
-	@Input('disable') disable = false;
 	@Input('buttonText') buttonText!: string;
-	@Input('buttonType') buttonType!: 'button' | 'submit' | 'reset';
+	@Input('fill') fill: 'default' | 'outline' | 'clear' | 'text' | 'link' = 'default';
 	@Input('className') class = '';
 	@Input('size') size: 'xs' | 'sm' | 'md' | 'lg' = 'md';
-	@Input('type') type: 'default' | 'outline' | 'clear' | 'text' | 'link' | 'icon' | 'unstyled' = 'default';
-	@Input('color') color: 'primary' | 'success' | 'warning' | 'danger' | 'grey' = 'primary';
+	@Input('color') color: 'primary' | 'success' | 'warning' | 'danger' | 'grey' | 'transparent' = 'primary';
 	@Input('texture') texture: 'deep' | 'light' = 'deep';
 	@Input('index') tabIndex = 0;
 	buttonSizes = { xs: 'py-[1px] px-8px  text-12', sm: `py-6px px-16px text-12`, md: `py-10px px-36px text-14`, lg: `py-10px px-36px w-full text-14` };
@@ -34,9 +33,8 @@ export class ButtonComponent implements OnInit {
 			outline: `border rounded-[10px] border-${this.color}-${colorLevel} text-${this.color}-100`,
 			clear: `border-none text-${this.color}-100`,
 			text: `border-0 text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
-			link: `border-none text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''} underline decoration-${this.color}-${colorLevel}`,
-			unstyled: ''
+			link: `border-none text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''} underline decoration-${this.color}-${colorLevel}`
 		};
-		return `${this.type !== 'text' && this.type !== 'icon' && this.type !== 'unstyled' ? this.buttonSizes[this.size] : ''} ${this.buttonTypes[this.type]} ${this.class}`;
+		return `${this.fill !== 'text' ? this.buttonSizes[this.size] : ''} ${this.buttonTypes[this.fill]} ${this.class}`;
 	}
 }
