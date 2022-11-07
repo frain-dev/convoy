8b53187ea59914326e8aec26c4853f42bf9b667b
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-11-07 01:00:24 +0100

    rewite input component and test

--- web/ui/dashboard/src/app/components/input/input.component.ts
+++ web/ui/dashboard/src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { Component, Directive, ElementRef, forwardRef, Input, OnInit, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { TooltipComponent } from '../tooltip/tooltip.component';
@@ -48,4 +48,100 @@ export class InputComponent implements OnInit, ControlValueAccessor {
 	writeValue() {}
 
 	setDisabledState() {}
+}
+
+/* ================== Input directive ================== */
+@Directive({
+	selector: '[convoy-input]',
+	standalone: true,
+	host: {
+		class: 'transition-all duration-[.3s] w-full font-light text-14 placeholder:text-grey-40 text-grey-100 border border-primary-500 valid:border-primary-500 disabled:border-primary-500 disabled:bg-[#F7F9FC] hover:bg-primary-500 hover:border-grey-20 focus:border-primary-100 focus:bg-white-100 outline-none rounded-4px placeholder:opacity-[.48] bg-[#F7F9FC] py-12px px-16px appearance-none',
+		'[ngClass]': "{ 'pointer-events-none': readonly, 'appearance-none': type !== 'password' }"
+	}
+})
+export class InputDirective implements OnInit {
+	type!: string;
+	showLoginPassword = false;
+
+	constructor(private element: ElementRef) {}
+
+	ngOnInit(): void {
+		this.type = this.element.nativeElement.getAttribute('type');
+	}
+}
+
+/* ================== Input field directive ================== */
+@Directive({
+	selector: '[convoy-input-field]',
+	standalone: true,
+	host: {
+		class: 'w-full relative mb-24px'
+	}
+})
+export class InputFieldDirective {}
+
+/* ================== Password input component ================== */
+@Component({
+	selector: 'convoy-password-field',
+	standalone: true,
+	imports: [CommonModule],
+	template: `
+		<div class="w-full relative">
+			<ng-content></ng-content>
+		</div>
+	`
+})
+export class PasswordInputFieldComponent implements OnInit {
+	ngOnInit(): void {}
+}
+
+/* ================== Input error component ================== */
+@Component({
+	selector: 'convoy-input-error',
+	standalone: true,
+	imports: [CommonModule],
+	template: `
+		<div class="flex items-center text-12 text-danger-100 mt-8px">
+			<img src="assets/img/input-error-icon.svg" class="mr-6px w-16px" alt="input error icon" />
+			<span><ng-content></ng-content></span>
+		</div>
+	`
+})
+export class InputErrorComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
+
+/* ================== Input label component ================== */
+@Component({
+	selector: 'convoy-label, [convoy-label]',
+	standalone: true,
+	imports: [CommonModule, TooltipComponent],
+	host: {
+		class: 'w-full font-medium text-12 text-grey-40 mb-8px flex items-center justify-between'
+	},
+	template: `
+		<div class="flex items-center">
+			<ng-content></ng-content>
+			<convoy-tooltip *ngIf="tooltip" class="ml-4px">{{ tooltip }}</convoy-tooltip>
+		</div>
+		<span *ngIf="required === 'true'" class="text-10 bg-grey-10 rounded-4px px-1 font-normal">required</span>
+	`,
+	styleUrls: ['./input.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => LabelComponent),
+			multi: true
+		}
+	]
+})
+export class LabelComponent implements OnInit {
+	@Optional() @Input('tooltip') tooltip!: string;
+	@Optional() @Input('required') required: 'false' | 'true' = 'false';
+
+	constructor() {}
+
+	ngOnInit(): void {}
 }
--- web/ui/dashboard/src/app/private/components/create-app/create-app.module.ts
+++ web/ui/dashboard/src/app/private/components/create-app/create-app.module.ts
@@ -4,7 +4,7 @@ import { CreateAppComponent } from './create-app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoaderModule } from '../loader/loader.module';
 import { ButtonComponent } from 'src/app/components/button/button.component';
-import { InputComponent } from 'src/app/components/input/input.component';
+import { InputComponent, InputDirective, InputErrorComponent, InputFieldDirective, LabelComponent, PasswordInputFieldComponent } from 'src/app/components/input/input.component';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
 import { TooltipComponent } from 'src/app/components/tooltip/tooltip.component';
@@ -12,7 +12,23 @@ import { RadioComponent } from 'src/app/components/radio/radio.component';
 
 @NgModule({
 	declarations: [CreateAppComponent],
-	imports: [CommonModule, ReactiveFormsModule, FormsModule, ButtonComponent, InputComponent, RadioComponent, LoaderModule, CardComponent, ConfirmationModalComponent, TooltipComponent],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		FormsModule,
+		ButtonComponent,
+		InputComponent,
+		RadioComponent,
+		LoaderModule,
+		CardComponent,
+		ConfirmationModalComponent,
+		TooltipComponent,
+		InputFieldDirective,
+		InputDirective,
+		LabelComponent,
+		InputErrorComponent,
+		PasswordInputFieldComponent
+	],
 	exports: [CreateAppComponent]
 })
 export class CreateAppModule {}
