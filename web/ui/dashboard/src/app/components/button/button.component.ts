97e56b11200da56150075f82e4285b75c633d1dd
oluwadaminiola <pelumioni25@gmail.com>
2022-11-09 11:55:34 +0100

    update: update components where dropdown is used

--- web/ui/dashboard/src/app/components/button/button.component.ts
+++ web/ui/dashboard/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
