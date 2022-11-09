f9c155d4832f235cc1b302eb411b650be45896e9
oluwadaminiola <pelumioni25@gmail.com>
2022-11-09 20:30:18 +0100

    update: remove custom button attributes

--- web/ui/dashboard/src/app/components/dropdown/dropdown.component.ts
+++ web/ui/dashboard/src/app/components/dropdown/dropdown.component.ts
@@ -14,20 +14,11 @@ export class DropdownComponent implements OnInit {
 	@Input('position') position: 'right' | 'left' | 'center' = 'right';
 	@Input('size') size: 'sm' | 'md' | 'lg' | 'xl' | 'full' = 'md';
 	@Input('active') active: boolean = false;
-	@Input('className') class!: string;
-	@Input('buttonText') buttonText!: string;
-	@Input('buttonClass') buttonClass = '';
-	@Input('buttonColor') buttonColor!: 'primary' | 'success' | 'warning' | 'danger' | 'grey';
-	@Input('buttonSize') buttonSize: 'sm' | 'md' | 'lg' = 'md';
-	@Input('buttonFill') buttonFill: 'default' | 'outline' | 'clear' | 'text' | 'link' = 'default';
-	@Input('buttonTexture') buttonTexture: 'deep' | 'light' = 'deep';
+
 	show = false;
 
 	constructor() {}
 
 	ngOnInit(): void {}
 
-	get buttonClasses(): string {
-		return `${this.active ? 'text-primary-100 !bg-primary-500' : ''} empty:hidden ${this.buttonClass}`;
-	}
 }
