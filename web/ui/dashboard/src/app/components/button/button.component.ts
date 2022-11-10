77ffb26e58b2bef52550dbcbd489962d1acee910
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-11-10 08:33:11 +0100

    fix buttons on dateand time picker, sdk comoponent

--- web/ui/dashboard/src/app/components/button/button.component.ts
+++ web/ui/dashboard/src/app/components/button/button.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
 	selector: 'convoy-button, [convoy-button]',
 	standalone: true,
-	host: { class: 'flex items-center justify-center disabled:opacity-50', '[class]': 'classes' },
+	host: { '[class]': 'classes' },
 	imports: [CommonModule],
 	templateUrl: './button.component.html',
 	styleUrls: ['./button.component.scss'],
@@ -13,13 +13,11 @@ import { CommonModule } from '@angular/common';
 export class ButtonComponent implements OnInit {
 	@Input('buttonText') buttonText!: string;
 	@Input('fill') fill: 'default' | 'outline' | 'clear' | 'text' | 'link' = 'default';
-	@Input('className') class = '';
 	@Input('size') size: 'xs' | 'sm' | 'md' | 'lg' = 'md';
 	@Input('color') color: 'primary' | 'success' | 'warning' | 'danger' | 'grey' | 'transparent' = 'primary';
 	@Input('texture') texture: 'deep' | 'light' = 'deep';
 	@Input('index') tabIndex = 0;
 	buttonSizes = { xs: 'py-[1px] px-8px  text-12', sm: `py-6px px-16px text-12`, md: `py-10px px-36px text-14`, lg: `py-10px px-36px w-full text-14` };
-	buttonTypes: any = {};
 
 	constructor() {}
 
@@ -27,13 +25,13 @@ export class ButtonComponent implements OnInit {
 
 	get classes(): string {
 		const colorLevel = this.texture == 'deep' ? '100' : '500';
-		this.buttonTypes = {
+		const buttonTypes = {
 			default: `bg-${this.color}-${colorLevel} text-${this.texture == 'deep' ? 'white' : this.color}-100 border-none rounded-8px`,
 			outline: `border rounded-[10px] border-${this.color}-${colorLevel} text-${this.color}-100`,
 			clear: `border-none text-${this.color}-100`,
 			text: `border-0 text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
 			link: `border-none text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''} underline decoration-${this.color}-${colorLevel}`
 		};
-		return `${this.fill !== 'text' ? this.buttonSizes[this.size] : ''} ${this.buttonTypes[this.fill]} ${this.class}`;
+		return `${this.fill !== 'text' ? this.buttonSizes[this.size] : ''} ${buttonTypes[this.fill]} flex items-center justify-center disabled:opacity-50`;
 	}
 }
