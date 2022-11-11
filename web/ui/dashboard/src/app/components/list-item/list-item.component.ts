cea68aae595e7c351d517170578ea1014daec6ee
oluwadaminiola <pelumioni25@gmail.com>
2022-11-11 12:44:30 +0100

    update: add hasBorder attribute to list component

--- web/ui/dashboard/src/app/components/list-item/list-item.component.ts
+++ web/ui/dashboard/src/app/components/list-item/list-item.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'convoy-list-item, [convoy-list-item]',
 	standalone: true,
+	host: { class: 'flex items-center justify-between py-10px transition-all duration-300', '[class]': "hasBorder?'border-b border-grey-10':''" },
 	imports: [CommonModule],
-	host: { class: 'flex items-center justify-between border-b border-grey-10 py-10px transition-all duration-300' },
 	template: `
 		<ng-content></ng-content>
 	`
 })
 export class ListItemComponent implements OnInit {
+	@Input('hasBorder') hasBorder = true;
 	constructor() {}
 
 	ngOnInit(): void {}
