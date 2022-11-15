b1384710dfe51d9bb05377fb56a13a9fad752701
oluwadaminiola <pelumioni25@gmail.com>
2022-11-16 00:53:38 +0100

    chore: add create endpoints/send event components to private components and update

---
+++ web/ui/dashboard/src/app/private/components/create-endpoint/create-endpoint.component.ts
@@ -0,0 +1,84 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputComponent } from 'src/app/components/input/input.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { RadioComponent } from 'src/app/components/radio/radio.component';
+import { TooltipComponent } from 'src/app/components/tooltip/tooltip.component';
+import { ENDPOINT } from 'src/app/models/app.model';
+import { GeneralService } from 'src/app/services/general/general.service';
+import { ActivatedRoute } from '@angular/router';
+import { EndpointsService } from '../../pages/project/endpoints/endpoints.service';
+import { CardComponent } from 'src/app/components/card/card.component';
+
+@Component({
+	selector: 'convoy-create-endpoint',
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule, InputComponent, ButtonComponent, RadioComponent, TooltipComponent, CardComponent],
+	templateUrl: './create-endpoint.component.html',
+	styleUrls: ['./create-endpoint.component.scss']
+})
+export class CreateEndpointComponent implements OnInit {
+	@Input() appId!: string;
+	@Input() selectedEndpoint?: ENDPOINT;
+	@Output() onAction = new EventEmitter<any>();
+	savingEndpoint = false;
+	addNewEndpointForm: FormGroup = this.formBuilder.group({
+		name: [],
+		support_email: [],
+		slack_webhook_url: [],
+		url: ['', Validators.required],
+		secret: [null],
+		http_timeout: [null],
+		description: ['', Validators.required],
+		authentication: this.formBuilder.group({
+			type: ['api_key'],
+			api_key: this.formBuilder.group({
+				header_name: [''],
+				header_value: ['']
+			})
+		}),
+		advanced_signatures: [null, Validators.required]
+	});
+	token: string = this.route.snapshot.params.token;
+
+	constructor(private formBuilder: FormBuilder, private generalService: GeneralService, private endpointService: EndpointsService, private route: ActivatedRoute) {}
+
+	ngOnInit() {
+		if (this.selectedEndpoint) this.updateEndpointForm();
+	}
+
+	async saveEndpoint() {
+		if (this.addNewEndpointForm.invalid) return this.addNewEndpointForm.markAsTouched();
+		this.savingEndpoint = true;
+
+		if (!this.addNewEndpointForm.value.authentication.api_key.header_name && !this.addNewEndpointForm.value.authentication.api_key.header_value) delete this.addNewEndpointForm.value.authentication;
+
+		try {
+			const response = this.selectedEndpoint
+				? await this.endpointService.editEndpoint({ appId: this.appId, endpointId: this.selectedEndpoint?.uid || '', body: this.addNewEndpointForm.value, token: this.token })
+				: await this.endpointService.addNewEndpoint({ appId: this.appId, body: this.addNewEndpointForm.value, token: this.token });
+			this.generalService.showNotification({ message: response.message, style: 'success' });
+			this.onAction.emit({ action: 'savedEndpoint', data: response.data });
+			this.addNewEndpointForm.reset();
+			this.savingEndpoint = false;
+			return;
+		} catch {
+			this.savingEndpoint = false;
+			return;
+		}
+	}
+
+	updateEndpointForm() {
+		if (this.selectedEndpoint) {
+			this.addNewEndpointForm.patchValue(this.selectedEndpoint);
+			this.addNewEndpointForm.patchValue({
+				url: this.selectedEndpoint.target_url
+			});
+		}
+	}
+
+	cancel() {
+		this.onAction.emit({ action: 'close' });
+	}
+}
---
+++ web/ui/dashboard/src/app/private/components/send-event/send-event.component.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
+import { SelectComponent } from 'src/app/components/select/select.component';
+import { InputComponent } from 'src/app/components/input/input.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EndpointsService } from '../../pages/project/endpoints/endpoints.service';
+import { GeneralService } from 'src/app/services/general/general.service';
+
+@Component({
+	selector: 'convoy-send-event',
+	standalone: true,
+	imports: [CommonModule, ModalComponent, SelectComponent, InputComponent, ButtonComponent, ReactiveFormsModule],
+	templateUrl: './send-event.component.html',
+	styleUrls: ['./send-event.component.scss']
+})
+export class SendEventComponent implements OnInit {
+	@Input() appId!: string;
+	@Output() onAction = new EventEmitter<any>();
+	isSendingNewEvent = false;
+	sendEventForm: FormGroup = this.formBuilder.group({
+		data: ['', Validators.required],
+		event_type: ['', Validators.required]
+	});
+	constructor(private formBuilder: FormBuilder, private router: Router, private endpointService: EndpointsService, private generalService: GeneralService) {}
+
+	ngOnInit() {}
+
+	cancel() {
+		this.onAction.emit({ action: 'cancel' });
+	}
+
+
+
+	async sendNewEvent() {
+		if (this.sendEventForm.invalid) return this.sendEventForm.markAsTouched();
+
+		if (!this.convertStringToJson(this.sendEventForm.value.data)) return;
+		this.sendEventForm.value.data = this.convertStringToJson(this.sendEventForm.value.data);
+
+		this.isSendingNewEvent = true;
+		try {
+			const response = await this.endpointService.sendEvent({ body: this.sendEventForm.value });
+			this.generalService.showNotification({ message: response.message, style: 'success' });
+			this.sendEventForm.reset();
+			this.onAction.emit({ action: 'sentEvent' });
+			this.isSendingNewEvent = false;
+			this.router.navigate(['/projects/' + this.endpointService.projectId + '/events'], { queryParams: { eventsApp: this.appId } });
+		} catch {
+			this.isSendingNewEvent = false;
+		}
+	}
+
+
+	convertStringToJson(str: string) {
+		try {
+			const jsonObject = JSON.parse(str);
+			return jsonObject;
+		} catch {
+			this.generalService.showNotification({ message: 'Event data is not entered in correct JSON format', style: 'error' });
+			return false;
+		}
+	}
+}
