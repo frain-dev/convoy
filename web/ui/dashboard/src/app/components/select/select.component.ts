320a1c6b8ca2535e936b5518717a41b92732a55d
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2022-11-23 22:07:51 +0100

    auto close select dropdowns for single selections

--- web/ui/dashboard/src/app/components/select/select.component.ts
+++ web/ui/dashboard/src/app/components/select/select.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../button/button.component';
-import { DropdownComponent } from '../dropdown/dropdown.component';
+import { DropdownComponent, DropdownOptionDirective } from '../dropdown/dropdown.component';
 import { TooltipComponent } from '../tooltip/tooltip.component';
 
 @Component({
 	selector: 'convoy-select',
 	standalone: true,
-	imports: [CommonModule, ReactiveFormsModule, TooltipComponent, DropdownComponent, ButtonComponent],
+	imports: [CommonModule, ReactiveFormsModule, TooltipComponent, DropdownComponent, ButtonComponent, DropdownOptionDirective],
 	templateUrl: './select.component.html',
 	styleUrls: ['./select.component.scss'],
 	providers: [
