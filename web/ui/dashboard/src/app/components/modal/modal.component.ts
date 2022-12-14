a67b091a3f5bf207200587ed3596f6c8de005a64
oluwadaminiola <pelumioni25@gmail.com>
2022-12-14 21:23:36 +0100

    update: update modal component

--- web/ui/dashboard/src/app/components/modal/modal.component.ts
+++ web/ui/dashboard/src/app/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { ModalDialogComponent } from '../modal-dialog/modal-dialog.component';
 import { OverlayDirective } from '../overlay/overlay.directive';
@@ -16,17 +16,16 @@ export class ModalComponent implements OnInit {
 	@Input('size') size: 'sm' | 'md' | 'lg' = 'md';
 	@Input('title') title!: string;
 	@Input('id') id!: string;
-	@Input('className') class!: string;
 	@Output('closeModal') closeModal = new EventEmitter<any>();
-
 
 	constructor() {}
 
 	ngOnInit(): void {}
 
-	// get classes(): string {
-	// 	return `${this.modalType[this.position]} ${this.position === 'full' ? 'bg-[#fafafe]' : this.modalSizes[this.size]} ${this.class}`;
-	// }
+	@HostListener('document:keydown', ['$event'])
+	public onFocusIn(event: any): any {
+		if (event.key === 'Escape') {
+			this.closeModal.emit();
+		}
+	}
 }
-
-
