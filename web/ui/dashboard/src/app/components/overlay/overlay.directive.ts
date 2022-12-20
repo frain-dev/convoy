e88ec9020d4f78b55c0367bab25b798bc3980efd
oluwadaminiola <pelumioni25@gmail.com>
2022-12-20 10:38:57 +0100

    fix: reduce overlay z-index

--- web/ui/dashboard/src/app/components/overlay/overlay.directive.ts
+++ web/ui/dashboard/src/app/components/overlay/overlay.directive.ts
@@ -3,7 +3,7 @@ import { Directive, Input } from '@angular/core';
 @Directive({
 	selector: '[convoyOverlay]',
 	standalone: true,
-	host: { class: 'fixed h-screen w-screen top-0 right-0 bottom-0 z-50', '[class]': "overlayHasBackdrop ? 'bg-black bg-opacity-50':''" }
+	host: { class: 'fixed h-screen w-screen top-0 right-0 bottom-0 z-[5]', '[class]': "overlayHasBackdrop ? 'bg-black bg-opacity-50':''" }
 })
 export class OverlayDirective {
 	@Input('overlayHasBackdrop') overlayHasBackdrop = false;
