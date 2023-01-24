e4b0a286da4582eda7c99fecd4967bb7e1c2adf4
Raymond Tukpe <rtukpe@gmail.com>
2023-01-24 13:47:06 +0100

    chore: add SQL generator lib

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -9,6 +9,7 @@ import (
 	"github.com/frain-dev/convoy/datastore"
 	"github.com/frain-dev/convoy/internal/pkg/migrator"
 	"github.com/frain-dev/convoy/pkg/log"
+	"gopkg.in/guregu/null.v4"
 
 	"github.com/spf13/cobra"
 )
@@ -42,62 +43,39 @@ func addRunCommand() *cobra.Command {
 				log.Fatal(err)
 			}
 
-			o := postgres.NewOrgRepo(db.GetDB())
+			// o := postgres.NewOrgRepo(db.GetDB())
 			// _ = o.CreateOrganisation(cmd.Context(), &datastore.Organisation{
 			// 	OwnerID: "xxx",
 			// 	Name:    "123",
 			// })
 
-			orgs, _, err := o.LoadOrganisationsPaged(cmd.Context(), datastore.Pageable{
-				Page:    1,
-				PerPage: 10,
-			})
+			// orgs, _, err := o.LoadOrganisationsPaged(cmd.Context(), datastore.Pageable{
+			// 	Page:    1,
+			// 	PerPage: 10,
+			// })
 
-			if err != nil {
-				fmt.Printf("orgs: %+v", err)
-				return
-			}
+			// if err != nil {
+			// 	fmt.Printf("orgs: %+v", err)
+			// 	return
+			// }
 
 			// fmt.Printf("org id: %+v\n", orgs[0].UID)
 			// fmt.Printf("pageable: %+v\n", pageable)
 
-			p := postgres.NewProjectRepo(db.GetDB())
-			err = p.UpdateProject(cmd.Context(), &datastore.Project{
-				Name:           "CCC",
-				Type:           datastore.IncomingProject,
-				OrganisationID: orgs[0].UID,
-				Config: &datastore.ProjectConfig{
-					RateLimitCount:     1000,
-					RateLimitDuration:  60,
-					StrategyType:       datastore.ExponentialStrategyProvider,
-					StrategyDuration:   100,
-					StrategyRetryCount: 10,
-					SignatureHeader:    config.DefaultSignatureHeader,
-					SignatureHash:      "SHA256",
-					RetentionPolicy:    "300d",
-				},
-			})
-			if err != nil {
-				fmt.Printf("err: %+v", err)
-				return
-			}
-
-			// c := postgres.NewConfigRepo(db.GetDB())
-			// err = c.UpdateConfiguration(cmd.Context(), &datastore.Configuration{
-			// 	UID:                "default",
-			// 	IsAnalyticsEnabled: true,
-			// 	IsSignupEnabled:    true,
-			// 	StoragePolicy: &datastore.StoragePolicyConfiguration{
-			// 		Type: datastore.S3,
-			// 		S3: &datastore.S3Storage{
-			// 			Bucket:       null.NewString("Bucket", true),
-			// 			AccessKey:    null.NewString("AccessKey", true),
-			// 			SecretKey:    null.NewString("SecretKey", true),
-			// 			Region:       null.NewString("Region", true),
-			// 			SessionToken: null.NewString("SessionToken", true),
-			// 			Endpoint:     null.NewString("Endpoint", true),
-			// 		},
-			// 		// OnPrem: datastore.DefaultStoragePolicy.OnPrem,
+			// p := postgres.NewProjectRepo(db.GetDB())
+			// err = p.UpdateProject(cmd.Context(), &datastore.Project{
+			// 	Name:           "CCC",
+			// 	Type:           datastore.IncomingProject,
+			// 	OrganisationID: orgs[0].UID,
+			// 	Config: &datastore.ProjectConfig{
+			// 		RateLimitCount:     1000,
+			// 		RateLimitDuration:  60,
+			// 		StrategyType:       datastore.ExponentialStrategyProvider,
+			// 		StrategyDuration:   100,
+			// 		StrategyRetryCount: 10,
+			// 		SignatureHeader:    config.DefaultSignatureHeader,
+			// 		SignatureHash:      "SHA256",
+			// 		RetentionPolicy:    "300d",
 			// 	},
 			// })
 			// if err != nil {
@@ -105,14 +83,37 @@ func addRunCommand() *cobra.Command {
 			// 	return
 			// }
 
-			// cfg, err := c.LoadConfiguration(cmd.Context())
-			// if err != nil {
-			// 	fmt.Printf("err: %+v", err)
-			// 	return
-			// }
+			c := postgres.NewConfigRepo(db.GetDB())
+			err = c.UpdateConfiguration(cmd.Context(), &datastore.Configuration{
+				UID:                "default",
+				IsAnalyticsEnabled: true,
+				IsSignupEnabled:    true,
+				StoragePolicy: &datastore.StoragePolicyConfiguration{
+					Type: datastore.OnPrem,
+					S3: &datastore.S3Storage{
+						Bucket:       null.NewString("Bucket", true),
+						AccessKey:    null.NewString("AccessKey", true),
+						SecretKey:    null.NewString("SecretKey", true),
+						Region:       null.NewString("Region", true),
+						SessionToken: null.NewString("SessionToken", true),
+						Endpoint:     null.NewString("Endpoint", true),
+					},
+					OnPrem: datastore.DefaultStoragePolicy.OnPrem,
+				},
+			})
+			if err != nil {
+				fmt.Printf("err: %+v", err)
+				return
+			}
 
-			// fmt.Printf("config: %+v\n", cfg.StoragePolicy.OnPrem)
-			// fmt.Printf("config: %+v\n", cfg.StoragePolicy.S3)
+			cfg, err := c.LoadConfiguration(cmd.Context())
+			if err != nil {
+				fmt.Printf("err: %+v", err)
+				return
+			}
+
+			fmt.Printf("config: %+v\n", cfg.StoragePolicy.OnPrem)
+			fmt.Printf("config: %+v\n", cfg.StoragePolicy.S3)
 
 			// for _, v := range projects {
 			// 	fmt.Printf("Proj: %+v\n", v)
--- database/postgres/configuration.go
+++ database/postgres/configuration.go
@@ -2,60 +2,12 @@ package postgres
 
 import (
 	"context"
+	"time"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/frain-dev/convoy/datastore"
 	"github.com/jmoiron/sqlx"
 	"gopkg.in/guregu/null.v4"
-)
-
-const (
-	createConfiguration = `
-	INSERT INTO convoy.configurations(
-		id, is_analytics_enabled, is_signup_enabled, 
-		storage_policy_type, on_prem_path, 
-		s3_bucket, s3_access_key, s3_secret_key, 
-		s3_region, s3_session_token, s3_endpoint
-	  ) 
-	  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
-	`
-
-	fetchConfiguration = `
-	SELECT 
-		id,
-		is_analytics_enabled,
-		is_signup_enabled,
-		storage_policy_type AS "storage_policy.type",
-		on_prem_path AS "storage_policy.on_prem.path",
-		s3_bucket AS "storage_policy.s3.bucket",
-		s3_access_key AS "storage_policy.s3.access_key",
-		s3_secret_key AS "storage_policy.s3.secret_key",
-		s3_region AS "storage_policy.s3.region",
-		s3_session_token AS "storage_policy.s3.session_token",
-		s3_endpoint AS "storage_policy.s3.endpoint",
-		created_at,
-		updated_at,
-		deleted_at
-	FROM convoy.configurations
-	WHERE id = 'default';
-	`
-
-	updateConfiguration = `
-	UPDATE
-		convoy.configurations
-	SET
-		is_analytics_enabled = $1,
-		is_signup_enabled = $2,
-		storage_policy_type = $3,
-		on_prem_path = $4,
-		s3_bucket = $5,
-		s3_access_key = $6,
-		s3_secret_key = $7,
-		s3_region = $8,
-		s3_session_token = $9, 
-		s3_endpoint = $10,
-		updated_at = now()
-	WHERE id = 'default';
-	`
 )
 
 type configRepo struct {
@@ -82,38 +34,58 @@ func (c *configRepo) CreateConfiguration(ctx context.Context, config *datastore.
 		}
 	}
 
-	cResult, err := c.db.Exec(createConfiguration,
-		config.UID,
-		config.IsAnalyticsEnabled,
-		config.IsSignupEnabled,
-		config.StoragePolicy.Type,
-		config.StoragePolicy.OnPrem.Path,
-		config.StoragePolicy.S3.Bucket,
-		config.StoragePolicy.S3.AccessKey,
-		config.StoragePolicy.S3.SecretKey,
-		config.StoragePolicy.S3.Region,
-		config.StoragePolicy.S3.SessionToken,
-		config.StoragePolicy.S3.Endpoint,
-	)
+	var id string
+	query := sq.Insert("convoy.configurations").
+		Columns("id", "is_analytics_enabled",
+			"is_signup_enabled", "storage_policy_type",
+			"on_prem_path", "s3_bucket", "s3_access_key",
+			"s3_secret_key", "s3_region",
+			"s3_session_token", "s3_endpoint").
+		Values(config.UID,
+			config.IsAnalyticsEnabled,
+			config.IsSignupEnabled,
+			config.StoragePolicy.Type,
+			config.StoragePolicy.OnPrem.Path,
+			config.StoragePolicy.S3.Bucket,
+			config.StoragePolicy.S3.AccessKey,
+			config.StoragePolicy.S3.SecretKey,
+			config.StoragePolicy.S3.Region,
+			config.StoragePolicy.S3.SessionToken,
+			config.StoragePolicy.S3.Endpoint).
+		Suffix("RETURNING \"id\"").
+		RunWith(c.db).
+		PlaceholderFormat(sq.Dollar)
+
+	err := query.QueryRowContext(ctx).Scan(&id)
 	if err != nil {
 		return err
-	}
-
-	rowsAffected, err := cResult.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	if rowsAffected < 1 {
-		return ErrProjectNotCreated
 	}
 
 	return nil
 }
 
 func (c *configRepo) LoadConfiguration(ctx context.Context) (*datastore.Configuration, error) {
+	query := sq.Select(
+		"id",
+		"is_analytics_enabled",
+		"is_signup_enabled",
+		"storage_policy_type AS \"storage_policy.type\"",
+		"on_prem_path AS \"storage_policy.on_prem.path\"",
+		"s3_bucket AS \"storage_policy.s3.bucket\"",
+		"s3_access_key AS \"storage_policy.s3.access_key\"",
+		"s3_secret_key AS \"storage_policy.s3.secret_key\"",
+		"s3_region AS \"storage_policy.s3.region\"",
+		"s3_session_token AS \"storage_policy.s3.session_token\"",
+		"s3_endpoint AS \"storage_policy.s3.endpoint\"",
+	).From("convoy.configurations").Where("id = 'default'")
+
+	sql, _, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
 	var config datastore.Configuration
-	err := c.db.Get(&config, fetchConfiguration)
+	err = c.db.GetContext(ctx, &config, sql)
 	if err != nil {
 		return nil, err
 	}
@@ -121,9 +93,9 @@ func (c *configRepo) LoadConfiguration(ctx context.Context) (*datastore.Configur
 	return &config, nil
 }
 
-func (c *configRepo) UpdateConfiguration(ctx context.Context, config *datastore.Configuration) error {
-	if config.StoragePolicy.Type == datastore.OnPrem {
-		config.StoragePolicy.S3 = &datastore.S3Storage{
+func (c *configRepo) UpdateConfiguration(ctx context.Context, cfg *datastore.Configuration) error {
+	if cfg.StoragePolicy.Type == datastore.OnPrem {
+		cfg.StoragePolicy.S3 = &datastore.S3Storage{
 			Bucket:       null.NewString("", false),
 			AccessKey:    null.NewString("", false),
 			SecretKey:    null.NewString("", false),
@@ -132,24 +104,34 @@ func (c *configRepo) UpdateConfiguration(ctx context.Context, config *datastore.
 			Endpoint:     null.NewString("", false),
 		}
 	} else {
-		config.StoragePolicy.OnPrem = &datastore.OnPremStorage{
+		cfg.StoragePolicy.OnPrem = &datastore.OnPremStorage{
 			Path: null.NewString("", false),
 		}
 	}
 
-	result, err := c.db.Exec(updateConfiguration,
-		config.IsAnalyticsEnabled,
-		config.IsSignupEnabled,
-		config.StoragePolicy.Type,
-		config.StoragePolicy.OnPrem.Path,
-		config.StoragePolicy.S3.Bucket,
-		config.StoragePolicy.S3.AccessKey,
-		config.StoragePolicy.S3.SecretKey,
-		config.StoragePolicy.S3.Region,
-		config.StoragePolicy.S3.SessionToken,
-		config.StoragePolicy.S3.Endpoint,
-	)
+	query := sq.Update("convoy.configurations").
+		Set("is_analytics_enabled", cfg.IsAnalyticsEnabled).
+		Set("is_signup_enabled", cfg.IsSignupEnabled).
+		Set("storage_policy_type", cfg.StoragePolicy.Type).
+		Set("on_prem_path", cfg.StoragePolicy.OnPrem.Path).
+		Set("s3_bucket", cfg.StoragePolicy.S3.Bucket).
+		Set("s3_access_key", cfg.StoragePolicy.S3.AccessKey).
+		Set("s3_secret_key", cfg.StoragePolicy.S3.SecretKey).
+		Set("s3_region", cfg.StoragePolicy.S3.Region).
+		Set("s3_session_token", cfg.StoragePolicy.S3.SessionToken).
+		Set("s3_endpoint", cfg.StoragePolicy.S3.Endpoint).
+		Set("updated_at", time.Now()).
+		Where("id = 'default'").RunWith(c.db).
+		PlaceholderFormat(sq.Dollar)
 
+	sql, _, err := query.ToSql()
+	if err != nil {
+		return err
+	}
+
+	println(sql)
+
+	result, err := query.ExecContext(ctx)
 	if err != nil {
 		return err
 	}
