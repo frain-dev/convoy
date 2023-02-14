045d8e6558b2758fef8c29ffa44fede6dd1484db
Daniel Oluojomu <danvixent@gmail.com>
2023-02-14 21:25:56 +0000

    fix: add postgres_test.go

---
+++ database/postgres/postgres_test.go
@@ -0,0 +1,18 @@
+package postgres
+
+import (
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/stretchr/testify/require"
+)
+
+func getDB(t *testing.T) (*sqlx.DB, func()) {
+	db, err := NewDB()
+	require.NoError(t, err)
+
+	return db.dbx, func() {
+		require.NoError(t, db.TruncateTables())
+	}
+}
