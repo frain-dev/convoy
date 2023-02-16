9c31ae62546c99f8926b05f250d0a6458fa364f4
oluwadaminiola <pelumioni25@gmail.com>
2023-02-16 02:54:58 +0100

    feat: add file input

---
+++ web/ui/dashboard/src/app/components/file-input/file-input.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FileInputComponent } from './file-input.component';
+
+describe('FileInputComponent', () => {
+  let component: FileInputComponent;
+  let fixture: ComponentFixture<FileInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ FileInputComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FileInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
