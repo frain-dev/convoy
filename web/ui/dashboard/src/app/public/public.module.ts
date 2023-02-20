fc38fa280bd472b49739f4b513584f277c7d1e38
oluwadaminiola <pelumioni25@gmail.com>
2023-02-20 15:36:33 +0100

    chore: remove public components

--- web/ui/dashboard/src/app/public/public.module.ts
+++ web/ui/dashboard/src/app/public/public.module.ts
@@ -5,34 +5,28 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 	{
-		path: '',
-		component: PublicComponent,
-		children: [
-			{
-				path: 'login',
-				loadComponent: () => import('./login/login.component').then(mod => mod.LoginComponent)
-			},
-			{
-				path: 'signup',
-				loadComponent: () => import('./signup/signup.component').then(mod => mod.SignupComponent)
-			},
-			{
-				path: 'forgot-password',
-				loadComponent: () => import('./forgot-password/forgot-password.component').then(mod => mod.ForgotPasswordComponent)
-			},
-			{
-				path: 'reset-password',
-				loadComponent: () => import('./reset-password/reset-password.component').then(mod => mod.ResetPasswordComponent)
-			},
-			{
-				path: 'accept-invite',
-				loadComponent: () => import('./accept-invite/accept-invite.component').then(mod => mod.AcceptInviteComponent)
-			},
-			{
-				path: 'verify-email',
-				loadComponent: () => import('./verify-email/verify-email.component').then(mod => mod.VerifyEmailComponent)
-			}
-		]
+		path: 'login',
+		loadComponent: () => import('./login/login.component').then(mod => mod.LoginComponent)
+	},
+	{
+		path: 'signup',
+		loadComponent: () => import('./signup/signup.component').then(mod => mod.SignupComponent)
+	},
+	{
+		path: 'forgot-password',
+		loadComponent: () => import('./forgot-password/forgot-password.component').then(mod => mod.ForgotPasswordComponent)
+	},
+	{
+		path: 'reset-password',
+		loadComponent: () => import('./reset-password/reset-password.component').then(mod => mod.ResetPasswordComponent)
+	},
+	{
+		path: 'accept-invite',
+		loadComponent: () => import('./accept-invite/accept-invite.component').then(mod => mod.AcceptInviteComponent)
+	},
+	{
+		path: 'verify-email',
+		loadComponent: () => import('./verify-email/verify-email.component').then(mod => mod.VerifyEmailComponent)
 	}
 ];
 
