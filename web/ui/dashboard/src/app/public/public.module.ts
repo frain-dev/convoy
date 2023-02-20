625bdf94c4c5dfabf499e2556358f25a8350d8af
oluwadaminiola <pelumioni25@gmail.com>
2023-02-20 15:39:00 +0100

    chore: remove public component declaration

--- web/ui/dashboard/src/app/public/public.module.ts
+++ web/ui/dashboard/src/app/public/public.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PublicComponent } from './public.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -31,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	declarations: [PublicComponent],
+	declarations: [],
 	imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class PublicModule {}
