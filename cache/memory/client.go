b373bb7c7b37f559844d1bf36720b7052a3b62c5
Daniel Oluojomu <danvixent@gmail.com>
2023-03-01 11:22:59 +0000

    fix: set default ttl to 0

--- cache/memory/client.go
+++ cache/memory/client.go
@@ -16,7 +16,7 @@ const cacheSize = 128000
 
 func NewMemoryCache() *MemoryCache {
 	c := cache.New(&cache.Options{
-		LocalCache: cache.NewTinyLFU(cacheSize, time.Hour),
+		LocalCache: cache.NewTinyLFU(cacheSize, 0),
 	})
 
 	return &MemoryCache{cache: c}
