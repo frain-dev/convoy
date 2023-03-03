1b7a0b0195a87ea36deb2cd2ed038331adc9cace
Dotun Jolaoso <dotunjolaosho@gmail.com>
2023-03-03 15:28:55 +0100

    feat: add migrate create command

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/frain-dev/convoy/config"
 	"github.com/frain-dev/convoy/database/postgres"
 	"github.com/frain-dev/convoy/internal/pkg/migrator"
@@ -48,6 +52,7 @@ func addMigrateCommand(a *app) *cobra.Command {
 
 	cmd.AddCommand(addUpCommand())
 	cmd.AddCommand(addDownCommand())
+	cmd.AddCommand(addCreateCommand())
 
 	return cmd
 }
@@ -110,6 +115,35 @@ func addDownCommand() *cobra.Command {
 	}
 
 	cmd.Flags().IntVar(&max, "max", 1, "The maximum number of migrations to rollback")
+
+	return cmd
+}
+
+func addCreateCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "creates a new migration file",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			fileName := fmt.Sprintf("sql/%v.sql", time.Now().Unix())
+			f, err := os.Create(fileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer f.Close()
+
+			lines := []string{"-- +migrate Up", "-- +migrate Down"}
+			for _, line := range lines {
+				_, err := f.WriteString(line + "\n\n")
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		},
+	}
 
 	return cmd
 }
