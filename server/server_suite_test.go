a752e7cdbd33e6c70e4a2c04a06e19dd45daa5e2
Daniel Oluojomu <danvixent@gmail.com>
2023-03-06 12:33:47 +0000

    fix: remove unneeded lines

--- server/server_suite_test.go
+++ server/server_suite_test.go
@@ -38,11 +38,11 @@ import (
 
 // TEST HELPERS.
 func getPostgresDSN() string {
-	return "postgres://admin:password@localhost:5432/convoy?sslmode=disable"
+	return os.Getenv("TEST_POSTGRES_DSN")
 }
 
 func getRedisDSN() string {
-	return "redis://localhost:6379"
+	return os.Getenv("TEST_REDIS_DSN")
 }
 
 func getConfig() config.Configuration {
