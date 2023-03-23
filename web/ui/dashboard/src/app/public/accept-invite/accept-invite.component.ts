f03555f78585b96b3a204548ee30fc0771329ad5
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2023-03-23 11:37:35 +0000

    Fix: accept invite page

--- web/ui/dashboard/src/app/public/accept-invite/accept-invite.component.ts
+++ web/ui/dashboard/src/app/public/accept-invite/accept-invite.component.ts
@@ -19,7 +19,6 @@ export class AcceptInviteComponent implements OnInit {
 	showPassword = false;
 	showConfirmPassword = false;
 	loading = false;
-	token!: string;
 	fetchingDetails = false;
 	userDetailsAvailable = false;
 	isInviteAccepted = false;
@@ -37,8 +36,8 @@ export class AcceptInviteComponent implements OnInit {
 	constructor(private formBuilder: FormBuilder, private acceptInviteService: AcceptInviteService, private route: ActivatedRoute, private router: Router, private generalService: GeneralService) {}
 
 	ngOnInit() {
-		this.token = this.route.snapshot.queryParams.token;
-		this.getUserDetails(this.token);
+		const token = this.route.snapshot.queryParams['invite-token'];
+		this.getUserDetails(token);
 	}
 
 	async getUserDetails(token: string) {
@@ -46,16 +45,19 @@ export class AcceptInviteComponent implements OnInit {
 		try {
 			const response = await this.acceptInviteService.getUserDetails(token);
 			response.data.user ? (this.userDetailsAvailable = true) : (this.userDetailsAvailable = false);
+
 			const inviteeDetails = response.data.token;
 			if (inviteeDetails?.organisation_name) this.organisationName = inviteeDetails?.organisation_name;
 			inviteeDetails.status === 'accepted' ? (this.isInviteAccepted = true) : (this.isInviteAccepted = false);
 			const userDetails = response.data.user;
+
 			this.acceptInviteForm.patchValue({
 				first_name: userDetails?.first_name ? userDetails.first_name : '',
 				last_name: userDetails?.last_name ? userDetails.last_name : '',
 				email: inviteeDetails.invitee_email,
 				role: { type: inviteeDetails.role.type }
 			});
+
 			this.fetchingDetails = false;
 		} catch {
 			this.fetchingDetails = false;
@@ -69,10 +71,13 @@ export class AcceptInviteComponent implements OnInit {
 
 		this.loading = true;
 		try {
-			const response = await this.acceptInviteService.acceptInvite({ token: this.token, body: this.acceptInviteForm.value });
+			const token = this.route.snapshot.queryParams['invite-token'];
+			const response = await this.acceptInviteService.acceptInvite({ token: token, body: this.acceptInviteForm.value });
 			this.loading = false;
+
 			const authDetails = localStorage.getItem('CONVOY_AUTH');
 			this.generalService.showNotification({ style: 'success', message: response.message });
+
 			this.userDetailsAvailable && authDetails !== 'undefined' ? this.router.navigateByUrl('projects') : this.router.navigateByUrl('login');
 		} catch (error: any) {
 			this.loading = false;
