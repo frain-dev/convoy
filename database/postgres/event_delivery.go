1e9ca8a24378b4a8d354ddae86e197ec0dacfa1e
Raymond Tukpe <jirevwe@users.noreply.github.com>
2023-03-23 13:11:10 +0100

    feat: add portal links pagination (#1475)

--- database/postgres/event_delivery.go
+++ database/postgres/event_delivery.go
@@ -560,7 +560,7 @@ func (e *eventDeliveryRepo) LoadEventDeliveriesPaged(ctx context.Context, projec
 		countQuery = e.db.Rebind(countQuery)
 
 		// count the row number before the first row
-		rows, err = e.db.QueryxContext(ctx, countQuery, qargs...)
+		rows, err := e.db.QueryxContext(ctx, countQuery, qargs...)
 		if err != nil {
 			return nil, datastore.PaginationData{}, err
 		}
@@ -570,6 +570,7 @@ func (e *eventDeliveryRepo) LoadEventDeliveriesPaged(ctx context.Context, projec
 				return nil, datastore.PaginationData{}, err
 			}
 		}
+		rows.Close()
 	}
 
 	ids := make([]string, len(eventDeliveries))
