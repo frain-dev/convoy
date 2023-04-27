939bfbb3653f7ed97d629a0883b29dacea928e26
Dotun Jolaoso <dotunjolaosho@gmail.com>
2023-04-27 19:43:43 +0100

    fix: remove api/ui/build folder

--- api/ui/build/assets/monaco-editor/esm/vs/base/common/observableImpl/logging.js
+++
@@ -1,6 +0,0 @@
-let globalObservableLogger;
-export function setLogger(logger) {
-    globalObservableLogger = logger;
-}
-export class ConsoleObservableLogger {
-}
--- api/ui/build/assets/monaco-editor/esm/vs/base/common/observableImpl/utils.js
+++
@@ -1,1 +0,0 @@
-export const __dummy = 0;
