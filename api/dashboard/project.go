964dd27d303e7ed3b93fee6a668a291f8eb16885
Subomi Oluwalana <subomioluwalana71@gmail.com>
2023-05-04 15:36:55 +0100

    fix: removed wrong authorisation (#1562)

--- api/dashboard/project.go
+++ api/dashboard/project.go
@@ -40,11 +40,6 @@ func (a *DashboardHandler) GetProjectStatistics(w http.ResponseWriter, r *http.R
 		return
 	}
 
-	if err = a.A.Authz.Authorize(r.Context(), "project.manage", project); err != nil {
-		_ = render.Render(w, r, util.NewErrorResponse("Unauthorized", http.StatusForbidden))
-		return
-	}
-
 	projectService := createProjectService(a)
 
 	err = projectService.FillProjectStatistics(r.Context(), project)
