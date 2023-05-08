a29478063864c58190e50588bafacfae866ee64f
Dotun Jolaoso <dotunjolaosho@gmail.com>
2023-05-08 11:52:34 +0100

    feat: add support for ee release workflow

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,11 @@
 # Set version
 tag=$1
+etag="$tag Enterprise Edition"
 : > ./VERSION && echo $tag >  VERSION
+: > ./ee/VERSION && echo $etag > ./ee/VERSION
 
 # Commit version number & push
-git add VERSION
+git add VERSION ./ee/VERSION
 git commit -m "Bump version to $tag"
 git push origin
 
