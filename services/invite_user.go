4f1c81a77c08bba91a29d0017cc971f1db0c09f5
Subomi Oluwalana <subomioluwalana71@gmail.com>
2023-05-09 21:37:27 +0100

    fix: added logs back

--- services/invite_user.go
+++ services/invite_user.go
@@ -42,7 +42,9 @@ func (iu *InviteUserService) Run(ctx context.Context) (*datastore.OrganisationIn
 
 	err := iu.InviteRepo.CreateOrganisationInvite(ctx, iv)
 	if err != nil {
-		return nil, &ServiceError{ErrMsg: "failed to invite member", Err: err}
+		errMsg := "failed to invite member"
+		log.FromContext(ctx).WithError(err).Error(errMsg)
+		return nil, &ServiceError{ErrMsg: errMsg, Err: err}
 	}
 
 	err = sendInviteEmail(ctx, iv, iu.User, iu.Organisation, iu.Queue)
