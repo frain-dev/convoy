6dea340219c43dce155a0e7212414ee6903d6470
Daniel Oluojomu <danvixent@gmail.com>
2023-05-31 14:05:31 +0100

    fix: refactor source service (#1623)

--- api/public/source.go
+++ api/public/source.go
@@ -18,12 +18,6 @@ import (
 	"github.com/go-chi/render"
 )
 
-func createSourceService(a *PublicHandler) *services.SourceService {
-	sourceRepo := postgres.NewSourceRepo(a.A.DB)
-
-	return services.NewSourceService(sourceRepo, a.A.Cache)
-}
-
 // CreateSource
 // @Summary Create a source
 // @Description This endpoint creates a source
@@ -49,8 +43,14 @@ func (a *PublicHandler) CreateSource(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sourceService := createSourceService(a)
-	source, err := sourceService.CreateSource(r.Context(), &newSource, project)
+	cs := services.CreateSourceService{
+		SourceRepo: postgres.NewSourceRepo(a.A.DB),
+		Cache:      a.A.Cache,
+		NewSource:  &newSource,
+		Project:    project,
+	}
+
+	source, err := cs.Run(r.Context())
 	if err != nil {
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
@@ -147,8 +147,6 @@ func (a *PublicHandler) UpdateSource(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sourceService := createSourceService(a)
-
 	source, err := postgres.NewSourceRepo(a.A.DB).FindSourceByID(r.Context(), project.UID, chi.URLParam(r, "sourceID"))
 	if err != nil {
 		if err == datastore.ErrSourceNotFound {
@@ -160,7 +158,15 @@ func (a *PublicHandler) UpdateSource(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	source, err = sourceService.UpdateSource(r.Context(), project, &sourceUpdate, source)
+	us := services.UpdateSourceService{
+		SourceRepo:   postgres.NewSourceRepo(a.A.DB),
+		Cache:        a.A.Cache,
+		Project:      project,
+		SourceUpdate: &sourceUpdate,
+		Source:       source,
+	}
+
+	source, err = us.Run(r.Context())
 	if err != nil {
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
