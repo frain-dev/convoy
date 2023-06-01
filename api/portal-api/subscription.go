995b21e2d817def169e0fbed771888c6f5eba491
Daniel Oluojomu <danvixent@gmail.com>
2023-06-01 09:07:13 +0100

    Refactor subscription service (#1624)
    * fix: refactor subscription service
    
    * fix: refactor FindSubscriptionByIDService definitions
    
    * fix: check subscription not found error
    
    * fix: move find subscription by id into handlers
    
    * fix: fix defintion error

--- api/portal-api/subscription.go
+++ api/portal-api/subscription.go
@@ -1,6 +1,7 @@
 package portalapi
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -17,14 +18,6 @@ import (
 
 	m "github.com/frain-dev/convoy/internal/pkg/middleware"
 )
-
-func createSubscriptionService(a *PortalLinkHandler) *services.SubcriptionService {
-	subRepo := postgres.NewSubscriptionRepo(a.A.DB)
-	endpointRepo := postgres.NewEndpointRepo(a.A.DB)
-	sourceRepo := postgres.NewSourceRepo(a.A.DB)
-
-	return services.NewSubscriptionService(subRepo, endpointRepo, sourceRepo)
-}
 
 func (a *PortalLinkHandler) GetSubscriptions(w http.ResponseWriter, r *http.Request) {
 	portalLink, err := a.retrievePortalLink(r)
@@ -89,9 +82,13 @@ func (a *PortalLinkHandler) GetSubscription(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	subService := createSubscriptionService(a)
-	subscription, err := subService.FindSubscriptionByID(r.Context(), project, subId, false)
+	subscription, err := postgres.NewSubscriptionRepo(a.A.DB).FindSubscriptionByID(r.Context(), project.UID, subId)
 	if err != nil {
+		log.FromContext(r.Context()).WithError(err).Error("failed to find subscription")
+		if errors.Is(err, datastore.ErrSubscriptionNotFound) {
+			_ = render.Render(w, r, util.NewErrorResponse("failed to find subscription", http.StatusNotFound))
+			return
+		}
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
 	}
@@ -113,8 +110,15 @@ func (a *PortalLinkHandler) CreateSubscription(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	subService := createSubscriptionService(a)
-	subscription, err := subService.CreateSubscription(r.Context(), project, &sub)
+	cs := services.CreateSubcriptionService{
+		SubRepo:         postgres.NewSubscriptionRepo(a.A.DB),
+		EndpointRepo:    postgres.NewEndpointRepo(a.A.DB),
+		SourceRepo:      postgres.NewSourceRepo(a.A.DB),
+		Project:         project,
+		NewSubscription: &sub,
+	}
+
+	subscription, err := cs.Run(r.Context())
 	if err != nil {
 		a.A.Logger.WithError(err).Error("failed to create subscription")
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
@@ -130,9 +134,14 @@ func (a *PortalLinkHandler) DeleteSubscription(w http.ResponseWriter, r *http.Re
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
 	}
-	subService := createSubscriptionService(a)
-	sub, err := subService.FindSubscriptionByID(r.Context(), project, chi.URLParam(r, "subscriptionID"), true)
+
+	sub, err := postgres.NewSubscriptionRepo(a.A.DB).FindSubscriptionByID(r.Context(), project.UID, chi.URLParam(r, "subscriptionID"))
 	if err != nil {
+		log.FromContext(r.Context()).WithError(err).Error("failed to find subscription")
+		if errors.Is(err, datastore.ErrSubscriptionNotFound) {
+			_ = render.Render(w, r, util.NewErrorResponse("failed to find subscription", http.StatusNotFound))
+			return
+		}
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
 	}
@@ -162,10 +171,16 @@ func (a *PortalLinkHandler) UpdateSubscription(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	subscription := chi.URLParam(r, "subscriptionID")
+	us := services.UpdateSubscriptionService{
+		SubRepo:        postgres.NewSubscriptionRepo(a.A.DB),
+		EndpointRepo:   postgres.NewEndpointRepo(a.A.DB),
+		SourceRepo:     postgres.NewSourceRepo(a.A.DB),
+		ProjectId:      project.UID,
+		SubscriptionId: chi.URLParam(r, "subscriptionID"),
+		Update:         &update,
+	}
 
-	subService := createSubscriptionService(a)
-	sub, err := subService.UpdateSubscription(r.Context(), project.UID, subscription, &update)
+	sub, err := us.Run(r.Context())
 	if err != nil {
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
