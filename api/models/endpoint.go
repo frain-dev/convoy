e647dbfe13cc6b241f35de872d9ac0b6807b40f1
Dotun Jolaoso <dotunjolaosho@gmail.com>
2023-06-06 07:48:35 +0100

    fix: fix deprecate comment

--- api/models/endpoint.go
+++ api/models/endpoint.go
@@ -1,116 +1,118 @@
 package models
 
 import (
-    "github.com/frain-dev/convoy/datastore"
-    m "github.com/frain-dev/convoy/internal/pkg/middleware"
-    "github.com/frain-dev/convoy/util"
-    "net/http"
-    "strings"
+	"github.com/frain-dev/convoy/datastore"
+	m "github.com/frain-dev/convoy/internal/pkg/middleware"
+	"github.com/frain-dev/convoy/util"
+	"net/http"
+	"strings"
 )
 
 type CreateEndpoint struct {
-    URL                string `json:"url" valid:"required~please provide a url for your endpoint"`
-    Secret             string `json:"secret"`
-    OwnerID            string `json:"owner_id"`
-    Description        string `json:"description"`
-    AdvancedSignatures bool   `json:"advanced_signatures"`
-    Name               string `json:"name" valid:"required~please provide your endpointName"`
-    SupportEmail       string `json:"support_email" valid:"email~please provide a valid email"`
-    IsDisabled         bool   `json:"is_disabled"`
-    SlackWebhookURL    string `json:"slack_webhook_url"`
+	URL                string `json:"url" valid:"required~please provide a url for your endpoint"`
+	Secret             string `json:"secret"`
+	OwnerID            string `json:"owner_id"`
+	Description        string `json:"description"`
+	AdvancedSignatures bool   `json:"advanced_signatures"`
+	Name               string `json:"name" valid:"required~please provide your endpointName"`
+	SupportEmail       string `json:"support_email" valid:"email~please provide a valid email"`
+	IsDisabled         bool   `json:"is_disabled"`
+	SlackWebhookURL    string `json:"slack_webhook_url"`
 
-    HttpTimeout       string                  `json:"http_timeout"`
-    RateLimit         int                     `json:"rate_limit"`
-    RateLimitDuration string                  `json:"rate_limit_duration"`
-    Authentication    *EndpointAuthentication `json:"authentication"`
-    AppID             string                  // Deprecated but necessary for backward compatibility
+	HttpTimeout       string                  `json:"http_timeout"`
+	RateLimit         int                     `json:"rate_limit"`
+	RateLimitDuration string                  `json:"rate_limit_duration"`
+	Authentication    *EndpointAuthentication `json:"authentication"`
+	// Deprecated but necessary for backward compatibility
+	AppID string
 }
 
 func (cE *CreateEndpoint) Validate() error {
-    return util.Validate(cE)
+	return util.Validate(cE)
 }
 
 type UpdateEndpoint struct {
-    URL                string  `json:"url" valid:"required~please provide a url for your endpoint"`
-    Secret             string  `json:"secret"`
-    Description        string  `json:"description"`
-    AdvancedSignatures *bool   `json:"advanced_signatures"`
-    Name               *string `json:"name" valid:"required~please provide your endpointName"`
-    SupportEmail       *string `json:"support_email" valid:"email~please provide a valid email"`
-    IsDisabled         *bool   `json:"is_disabled"`
-    SlackWebhookURL    *string `json:"slack_webhook_url"`
+	URL                string  `json:"url" valid:"required~please provide a url for your endpoint"`
+	Secret             string  `json:"secret"`
+	Description        string  `json:"description"`
+	AdvancedSignatures *bool   `json:"advanced_signatures"`
+	Name               *string `json:"name" valid:"required~please provide your endpointName"`
+	SupportEmail       *string `json:"support_email" valid:"email~please provide a valid email"`
+	IsDisabled         *bool   `json:"is_disabled"`
+	SlackWebhookURL    *string `json:"slack_webhook_url"`
 
-    HttpTimeout       string                  `json:"http_timeout"`
-    RateLimit         int                     `json:"rate_limit"`
-    RateLimitDuration string                  `json:"rate_limit_duration"`
-    Authentication    *EndpointAuthentication `json:"authentication"`
+	HttpTimeout       string                  `json:"http_timeout"`
+	RateLimit         int                     `json:"rate_limit"`
+	RateLimitDuration string                  `json:"rate_limit_duration"`
+	Authentication    *EndpointAuthentication `json:"authentication"`
 }
 
 func (uE *UpdateEndpoint) Validate() error {
-    return util.Validate(uE)
+	return util.Validate(uE)
 }
 
 type DynamicEndpoint struct {
-    URL                string `json:"url" valid:"required~please provide a url for your endpoint"`
-    Secret             string `json:"secret"`
-    OwnerID            string `json:"owner_id"`
-    Description        string `json:"description"`
-    AdvancedSignatures bool   `json:"advanced_signatures"`
-    Name               string `json:"name"`
-    SupportEmail       string `json:"support_email"`
-    IsDisabled         bool   `json:"is_disabled"`
-    SlackWebhookURL    string `json:"slack_webhook_url"`
+	URL                string `json:"url" valid:"required~please provide a url for your endpoint"`
+	Secret             string `json:"secret"`
+	OwnerID            string `json:"owner_id"`
+	Description        string `json:"description"`
+	AdvancedSignatures bool   `json:"advanced_signatures"`
+	Name               string `json:"name"`
+	SupportEmail       string `json:"support_email"`
+	IsDisabled         bool   `json:"is_disabled"`
+	SlackWebhookURL    string `json:"slack_webhook_url"`
 
-    HttpTimeout       string                  `json:"http_timeout"`
-    RateLimit         int                     `json:"rate_limit"`
-    RateLimitDuration string                  `json:"rate_limit_duration"`
-    Authentication    *EndpointAuthentication `json:"authentication"`
-    AppID             string                  // Deprecated but necessary for backward compatibility
+	HttpTimeout       string                  `json:"http_timeout"`
+	RateLimit         int                     `json:"rate_limit"`
+	RateLimitDuration string                  `json:"rate_limit_duration"`
+	Authentication    *EndpointAuthentication `json:"authentication"`
+	// Deprecated but necessary for backward compatibility
+	AppID string
 }
 
 func (dE *DynamicEndpoint) Validate() error {
-    return util.Validate(dE)
+	return util.Validate(dE)
 }
 
 type QueryListEndpoint struct {
-    // The name of the endpoint
-    Name string `json:"q" example:"endpoint-1"`
-    // The owner ID of the endpoint
-    OwnerID string `json:"owner_id" example:"01H0JA5MEES38RRK3HTEJC647K"`
-    Pageable
+	// The name of the endpoint
+	Name string `json:"q" example:"endpoint-1"`
+	// The owner ID of the endpoint
+	OwnerID string `json:"owner_id" example:"01H0JA5MEES38RRK3HTEJC647K"`
+	Pageable
 }
 
 type QueryListEndpointResponse struct {
-    datastore.Pageable
-    *datastore.Filter
+	datastore.Pageable
+	*datastore.Filter
 }
 
 func (q *QueryListEndpoint) Transform(r *http.Request) *QueryListEndpointResponse {
-    return &QueryListEndpointResponse{
-        Pageable: m.GetPageableFromContext(r.Context()),
-        Filter: &datastore.Filter{
-            Query:   strings.TrimSpace(r.URL.Query().Get("q")),
-            OwnerID: r.URL.Query().Get("ownerId"),
-        },
-    }
+	return &QueryListEndpointResponse{
+		Pageable: m.GetPageableFromContext(r.Context()),
+		Filter: &datastore.Filter{
+			Query:   strings.TrimSpace(r.URL.Query().Get("q")),
+			OwnerID: r.URL.Query().Get("ownerId"),
+		},
+	}
 }
 
 type EndpointAuthentication struct {
-    Type   datastore.EndpointAuthenticationType `json:"type,omitempty" valid:"optional,in(api_key)~unsupported authentication type"`
-    ApiKey *ApiKey                              `json:"api_key"`
+	Type   datastore.EndpointAuthenticationType `json:"type,omitempty" valid:"optional,in(api_key)~unsupported authentication type"`
+	ApiKey *ApiKey                              `json:"api_key"`
 }
 
 func (ea *EndpointAuthentication) Transform() *datastore.EndpointAuthentication {
-    if ea == nil {
-        return nil
-    }
+	if ea == nil {
+		return nil
+	}
 
-    return &datastore.EndpointAuthentication{
-        Type:   ea.Type,
-        ApiKey: ea.ApiKey.transform(),
-    }
+	return &datastore.EndpointAuthentication{
+		Type:   ea.Type,
+		ApiKey: ea.ApiKey.transform(),
+	}
 }
 
 type EndpointResponse struct {
-    *datastore.Endpoint
+	*datastore.Endpoint
 }
