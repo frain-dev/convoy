7f162fd368c2b40f76d28ac91311a32a9ba422fa
Dotun Jolaoso <dotunjolaosho@gmail.com>
2023-06-06 16:36:22 +0100

    feat: refactor meta event request and response models

--- api/dashboard/meta_event.go
+++ api/dashboard/meta_event.go
@@ -1,11 +1,11 @@
 package dashboard
 
 import (
+	"github.com/frain-dev/convoy/api/models"
 	"net/http"
 
 	"github.com/frain-dev/convoy/database/postgres"
 	"github.com/frain-dev/convoy/datastore"
-	m "github.com/frain-dev/convoy/internal/pkg/middleware"
 	"github.com/frain-dev/convoy/services"
 	"github.com/frain-dev/convoy/util"
 	"github.com/go-chi/chi/v5"
@@ -13,39 +13,42 @@ import (
 )
 
 func (a *DashboardHandler) GetMetaEventsPaged(w http.ResponseWriter, r *http.Request) {
-	searchParams, err := getSearchParams(r)
+	var q *models.QueryListMetaEvent
+	data, err := q.Transform(r)
 	if err != nil {
 		_ = render.Render(w, r, util.NewErrorResponse(err.Error(), http.StatusBadRequest))
 		return
 	}
 
-	pageable := m.GetPageableFromContext(r.Context())
 	project, err := a.retrieveProject(r)
 	if err != nil {
 		_ = render.Render(w, r, util.NewServiceErrResponse(err))
 		return
 	}
 
-	f := &datastore.Filter{SearchParams: searchParams, Pageable: pageable}
-	metaEvents, paginationData, err := postgres.NewMetaEventRepo(a.A.DB).LoadMetaEventsPaged(r.Context(), project.UID, f)
+	metaEvents, paginationData, err := postgres.NewMetaEventRepo(a.A.DB).LoadMetaEventsPaged(r.Context(), project.UID, data.Filter)
 	if err != nil {
 		_ = render.Render(w, r, util.NewErrorResponse("an error occurred while fetching meta events", http.StatusInternalServerError))
 		return
 	}
 
+	resp := models.NewListResponse(metaEvents, func(metaEvent datastore.MetaEvent) models.MetaEventResponse {
+		return models.MetaEventResponse{MetaEvent: &metaEvent}
+	})
 	_ = render.Render(w, r, util.NewServerResponse("Meta events fetched successfully",
-		pagedResponse{Content: metaEvents, Pagination: &paginationData}, http.StatusOK))
+		pagedResponse{Content: resp, Pagination: &paginationData}, http.StatusOK))
 }
 
 func (a *DashboardHandler) GetMetaEvent(w http.ResponseWriter, r *http.Request) {
-	metEvent, err := a.retrieveMetaEvent(r)
+	metaEvent, err := a.retrieveMetaEvent(r)
 	if err != nil {
 		_ = render.Render(w, r, util.NewErrorResponse(err.Error(), http.StatusNotFound))
 		return
 	}
 
+	resp := &models.MetaEventResponse{MetaEvent: metaEvent}
 	_ = render.Render(w, r, util.NewServerResponse("Meta event fetched successfully",
-		metEvent, http.StatusOK))
+		resp, http.StatusOK))
 }
 
 func (a *DashboardHandler) ResendMetaEvent(w http.ResponseWriter, r *http.Request) {
@@ -63,7 +66,8 @@ func (a *DashboardHandler) ResendMetaEvent(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	_ = render.Render(w, r, util.NewServerResponse("Meta event processed for retry successfully", metaEvent, http.StatusOK))
+	resp := &models.MetaEventResponse{MetaEvent: metaEvent}
+	_ = render.Render(w, r, util.NewServerResponse("Meta event processed for retry successfully", resp, http.StatusOK))
 }
 
 func (a *DashboardHandler) retrieveMetaEvent(r *http.Request) (*datastore.MetaEvent, error) {
