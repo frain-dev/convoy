f3aff9ae051cca429aa68c60749dc31fade1c366
Dotun Jolaoso <dotunjolaosho@gmail.com>
2023-06-13 17:36:51 +0100

    feat: add worker task for deleting archived tasks

---
+++ worker/task/delete_archived_task.go
@@ -0,0 +1,70 @@
+package task
+
+import (
+	"context"
+	"fmt"
+	"github.com/frain-dev/convoy"
+	"github.com/frain-dev/convoy/internal/pkg/rdb"
+	"github.com/frain-dev/convoy/pkg/log"
+	"github.com/frain-dev/convoy/queue"
+	"github.com/frain-dev/convoy/queue/redis"
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
+	"github.com/hibiken/asynq"
+	"time"
+)
+
+func DeleteArchivedTasks(r queue.Queuer, rd *rdb.Redis) func(context.Context, *asynq.Task) error {
+	pool := goredis.NewPool(rd.Client())
+	rs := redsync.New(pool)
+
+	return func(ctx context.Context, t *asynq.Task) error {
+		const mutexName = "convoy:deletearchivedtasks:mutex"
+		mutex := rs.NewMutex(mutexName, redsync.WithExpiry(time.Second), redsync.WithTries(1))
+
+		tctx, cancel := context.WithTimeout(ctx, time.Second*2)
+		defer cancel()
+
+		err := mutex.LockContext(tctx)
+		if err != nil {
+			return fmt.Errorf("failed to obtain lock: %v", err)
+		}
+
+		defer func() {
+			tctx, cancel := context.WithTimeout(ctx, time.Second*2)
+			defer cancel()
+
+			// Release the lock so other processes or threads can obtain a lock.
+			ok, err := mutex.UnlockContext(tctx)
+			if !ok || err != nil {
+				log.WithError(err).Error("failed to release lock")
+			}
+		}()
+
+		queues := []string{
+			string(convoy.EventQueue),
+			string(convoy.CreateEventQueue),
+			string(convoy.SearchIndexQueue),
+			string(convoy.ScheduleQueue),
+			string(convoy.DefaultQueue),
+			string(convoy.StreamQueue),
+			string(convoy.MetaEventQueue),
+		}
+
+		var q *redis.RedisQueue
+		q, ok := r.(*redis.RedisQueue)
+		if !ok {
+			log.Errorf("invalid queue type: %T", r)
+		}
+
+		for _, queue := range queues {
+			_, err := q.Inspector().DeleteAllArchivedTasks(queue)
+			if err != nil {
+				log.FromContext(ctx).WithError(err).Errorf("failed to delete archived task from queue - %s", queue)
+				continue
+			}
+		}
+
+		return nil
+	}
+}
