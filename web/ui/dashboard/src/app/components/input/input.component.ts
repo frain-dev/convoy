9990971f35fd7d9147b02bf726bbab48936e390d
Emmanuel Aina <emmanuel.ainaj@gmail.com>
2023-06-19 10:18:09 +0100

    make inputs more legible

--- web/ui/dashboard/src/app/components/input/input.component.ts
+++ web/ui/dashboard/src/app/components/input/input.component.ts
@@ -7,7 +7,7 @@ import { TooltipComponent } from '../tooltip/tooltip.component';
 	selector: '[convoy-input]',
 	standalone: true,
 	host: {
-		class: 'transition-all duration-[.3s] w-full font-normal text-14 placeholder:text-new.gray-300 text-new.gray-600 border border-new.primary-25 valid:border-new.primary-25 disabled:text-new.gray-400 disabled:border-new.primary-25 disabled:bg-white-100 hover:border-new.primary-25 focus:border-new.primary-300 focus:bg-white-100 outline-none rounded-4px placeholder:opacity-[.48] bg-white-100 py-12px px-16px appearance-none',
+		class: 'transition-all duration-[.3s] w-full font-normal text-14 placeholder:text-new.gray-300 text-new.gray-600 border border-new.primary-25 valid:border-new.primary-25 disabled:text-new.gray-400 disabled:border-new.primary-25 hover:border-new.primary-25 focus:border-new.primary-300 outline-none rounded-4px placeholder:text-14 bg-white-100 py-12px px-16px appearance-none',
 		'[ngClass]': "{ 'pointer-events-none': readonly, 'appearance-none': type !== 'password' }"
 	}
 })
@@ -74,14 +74,14 @@ export class InputErrorComponent implements OnInit {
 	standalone: true,
 	imports: [CommonModule, TooltipComponent],
 	host: {
-		class: 'w-full font-normal text-12 text-new.gray-400 mb-8px flex items-center justify-between'
+		class: 'w-full text-12 text-new.gray-500 mb-8px flex items-center justify-between'
 	},
 	template: `
 		<div class="flex items-center">
 			<ng-content></ng-content>
 			<convoy-tooltip *ngIf="tooltip" class="ml-4px" size="sm">{{ tooltip }}</convoy-tooltip>
 		</div>
-		<span *ngIf="required === 'true'" class="text-10 text-new.gray-400 px-1 font-medium">required</span>
+		<span *ngIf="required === 'true'" class="text-10 text-new.gray-400 px-6px rounded-22px font-medium bg-new.gray-100">required</span>
 	`,
 	styleUrls: ['./input.component.scss']
 })
