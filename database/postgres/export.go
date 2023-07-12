aca8bf753f73c130f3bdcfeadae092a86d755775
Daniel Oluojomu <danvixent@gmail.com>
2023-07-12 23:29:23 +0100

    fix: fix memory consumption issue

--- database/postgres/export.go
+++ database/postgres/export.go
@@ -1,9 +1,11 @@
 package postgres
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"math"
 	"time"
 
@@ -36,7 +38,9 @@ const (
 	where = ` WHERE deleted_at IS NULL AND project_id = $1 AND created_at < $2`
 )
 
-func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID string, createdAt time.Time) (json.RawMessage, int64, error) {
+// ExportRecords exports the records from the given table and writes them in json format to the passed writer.
+// It's the callers responsibility to close the writer.
+func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID string, createdAt time.Time, w io.Writer) (int64, error) {
 	c := &struct {
 		Count int64 `db:"count"`
 	}{}
@@ -44,49 +48,52 @@ func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID str
 	countQuery := fmt.Sprintf(count, tableName, where)
 	err := e.db.QueryRowxContext(ctx, countQuery, projectID, createdAt).StructScan(c)
 	if err != nil {
-		return nil, 0, err
+		return 0, err
 	}
 
 	if c.Count == 0 { // nothing to export
-		return nil, 0, nil
+		return 0, nil
 	}
 
 	var (
 		batchSize  = 2000
 		numDocs    int64
-		records    []json.RawMessage
 		numBatches = int(math.Ceil(float64(c.Count) / float64(batchSize)))
 	)
+
+	_, err = w.Write([]byte(`[`))
+	if err != nil {
+		return 0, err
+	}
 
 	q := fmt.Sprintf(exportRepoQ, tableName, where)
 	for i := 0; i < numBatches; i++ {
 		offset := i * batchSize
 
-		rs, n, err := e.querybatch(ctx, q, projectID, createdAt, batchSize, offset)
+		n, err := e.querybatch(ctx, q, projectID, createdAt, batchSize, offset, w)
 		if err != nil {
-			return nil, 0, fmt.Errorf("failed to remarshal results: %v", err)
+			return 0, fmt.Errorf("failed to remarshal results: %v", err)
 		}
 
 		numDocs += n
-		records = append(records, rs...)
 	}
 
-	v, err := json.Marshal(records)
+	_, err = w.Write([]byte(`]`))
 	if err != nil {
-		return nil, 0, fmt.Errorf("failed to remarshal results: %v", err)
+		return 0, err
 	}
 
-	return v, numDocs, nil
+	return numDocs, nil
 }
 
-func (e *exportRepo) querybatch(ctx context.Context, q, projectID string, createdAt time.Time, batchSize, offset int) ([]json.RawMessage, int64, error) {
+func (e *exportRepo) querybatch(ctx context.Context, q, projectID string, createdAt time.Time, batchSize, offset int, w io.Writer) (int64, error) {
 	var numDocs int64
 
 	// Calling rows.Close() manually in places before we return is important here to prevent
 	//  a memory leak, we cannot use defer in a loop because this can fill up the function stack quickly
 	rows, err := e.db.QueryxContext(ctx, q, projectID, createdAt, batchSize, offset)
 	if err != nil {
-		return nil, 0, err
+		return 0, err
 	}
 	defer rows.Close()
 
@@ -96,11 +103,24 @@ func (e *exportRepo) querybatch(ctx context.Context, q, projectID string, create
 		var record json.RawMessage
 		err = rows.Scan(&record)
 		if err != nil {
-			return nil, 0, err
+			return 0, err
 		}
 
 		records = append(records, record)
 	}
 
-	return records, numDocs, nil
+	m, err := json.Marshal(records)
+	if err != nil {
+		return 0, fmt.Errorf("failed to remarshal results: %v", err)
+	}
+
+	m = bytes.TrimPrefix(m, []byte(`[`))
+	m = bytes.TrimSuffix(m, []byte(`]`))
+
+	_, err = w.Write(m)
+	if err != nil {
+		return 0, err
+	}
+
+	return numDocs, nil
 }
