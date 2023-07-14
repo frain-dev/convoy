00ba7b94d052340033dc1832ef41b341aabd04ab
Daniel Oluojomu <danvixent@gmail.com>
2023-07-14 20:08:22 +0100

    fix: add print

--- database/postgres/export.go
+++ database/postgres/export.go
@@ -1,14 +1,14 @@
 package postgres
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"math"
-	"runtime"
 	"time"
+
+	"github.com/tidwall/gjson"
 
 	"github.com/frain-dev/convoy/database"
 	"github.com/frain-dev/convoy/datastore"
@@ -28,15 +28,14 @@ const (
 		SELECT to_jsonb(ed) - 'id' || jsonb_build_object('uid', ed.id) AS json_output
 		FROM %s AS ed %s
 		ORDER BY id ASC
-		LIMIT $3
-		OFFSET $4;
+		LIMIT $4
 	`
 
 	count = `
 		SELECT COUNT(*) FROM %s %s;
 	`
 
-	where = ` WHERE deleted_at IS NULL AND project_id = $1 AND created_at < $2`
+	where = ` WHERE deleted_at IS NULL AND project_id = $1 AND created_at < $2 AND (id = $3 OR $3 = '')`
 )
 
 // ExportRecords exports the records from the given table and writes them in json format to the passed writer.
@@ -47,7 +46,7 @@ func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID str
 	}{}
 
 	countQuery := fmt.Sprintf(count, tableName, where)
-	err := e.db.QueryRowxContext(ctx, countQuery, projectID, createdAt).StructScan(c)
+	err := e.db.QueryRowxContext(ctx, countQuery, projectID, createdAt, "").StructScan(c)
 	if err != nil {
 		return 0, err
 	}
@@ -57,7 +56,7 @@ func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID str
 	}
 
 	var (
-		batchSize  = 500
+		batchSize  = 5000
 		numDocs    int64
 		numBatches = int(math.Ceil(float64(c.Count) / float64(batchSize)))
 	)
@@ -69,23 +68,22 @@ func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID str
 	fmt.Println("numBatches", numBatches)
 
 	q := fmt.Sprintf(exportRepoQ, tableName, where)
+	var (
+		n      int64
+		lastID string
+	)
+
 	for i := 0; i < numBatches; i++ {
 		fmt.Println("batch ", i)
 		offset := i * batchSize
 
-		n, err := e.querybatch(context.Background(), q, projectID, createdAt, batchSize, offset, w)
+		n, lastID, err = e.querybatch(context.Background(), q, projectID, lastID, createdAt, batchSize, offset, w)
 		fmt.Println("querybatch", err)
-		runtime.GC()
 		if err != nil {
 			return 0, fmt.Errorf("failed to query batch %d: %v", i, err)
 		}
 
 		numDocs += n
-		// err = w.Sync()
-		// fmt.Println("sync err ", err)
-		if err != nil {
-			return 0, err
-		}
 	}
 
 	_, err = w.Write([]byte(`]`))
@@ -96,46 +94,71 @@ func (e *exportRepo) ExportRecords(ctx context.Context, tableName, projectID str
 	return numDocs, nil
 }
 
-func (e *exportRepo) querybatch(ctx context.Context, q, projectID string, createdAt time.Time, batchSize, offset int, w io.Writer) (int64, error) {
+var commaJSON = []byte(`,`)
+
+func (e *exportRepo) querybatch(ctx context.Context, q, projectID, lastID string, createdAt time.Time, batchSize, offset int, w io.Writer) (int64, string, error) {
 	var numDocs int64
 
 	// Calling rows.Close() manually in places before we return is important here to prevent
 	//  a memory leak, we cannot use defer in a loop because this can fill up the function stack quickly
-	rows, err := e.db.QueryxContext(ctx, q, projectID, createdAt, batchSize, offset)
-	fmt.Println("111")
+	c := time.Now()
+
+	rows, err := e.db.QueryxContext(ctx, q, projectID, createdAt, lastID, batchSize)
+	fmt.Println("QueryxContext since", time.Since(c).Seconds())
 	if err != nil {
-		return 0, err
+		return 0, "", err
 	}
 	defer rows.Close()
 
-	records := make([]json.RawMessage, 0, batchSize)
 	var record json.RawMessage
-	fmt.Println("imm befrr next")
-	for rows.Next() {
-		fmt.Println("imm next")
+	records := make([]byte, 0, 20000)
+
+	// scan first record and append it without appending comma
+	if rows.Next() {
 		numDocs++
 		err = rows.Scan(&record)
-		fmt.Println("NEXT", err)
 		if err != nil {
-			return 0, err
+			return 0, "", err
 		}
 
-		records = append(records, record)
+		records = append(records, record...)
 	}
 
-	fmt.Println("222")
-	m, err := json.Marshal(records)
-	if err != nil {
-		return 0, fmt.Errorf("failed to remarshal results: %v", err)
+	i := 0
+	// scan remaining records and prefix a comma before writing it
+	for rows.Next() {
+		numDocs++
+		i++
+		err = rows.Scan(&record)
+		if err != nil {
+			return 0, "", err
+		}
+
+		records = append(records, append(commaJSON, record...)...)
+
+		if i == 3000 {
+			i = 0
+
+			c := time.Now()
+			_, err = w.Write(records)
+			if err != nil {
+				return 0, "", err
+			}
+			fmt.Println("since", time.Since(c).Seconds())
+			records = records[:0]
+		}
 	}
 
-	m = bytes.TrimPrefix(m, []byte(`[`))
-	m = bytes.TrimSuffix(m, []byte(`]`))
-
-	_, err = w.Write(m)
-	if err != nil {
-		return 0, err
+	if len(records) > 0 {
+		c := time.Now()
+		_, err = w.Write(records)
+		if err != nil {
+			return 0, "", err
+		}
+		fmt.Println("since", time.Since(c).Seconds())
 	}
 
-	return numDocs, nil
+	value := gjson.Get(string(record), "uid")
+
+	return numDocs, value.String(), nil
 }
