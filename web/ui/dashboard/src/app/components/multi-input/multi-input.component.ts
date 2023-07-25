72079e60d5c7a6fac7d2e4e3c9f7a7839f707b17
oluwadaminiola <pelumioni25@gmail.com>
2023-07-25 19:26:12 +0100

    feat: create mutli input form

---
+++ web/ui/dashboard/src/app/components/multi-input/multi-input.component.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TooltipComponent } from '../tooltip/tooltip.component';
+
+@Component({
+	selector: 'convoy-multi-input',
+	standalone: true,
+	imports: [CommonModule, TooltipComponent],
+	templateUrl: './multi-input.component.html',
+	styleUrls: ['./multi-input.component.scss']
+})
+export class MultiInputComponent implements OnInit {
+	@Output() inputValues = new EventEmitter<string[]>();
+	@Input('prefilledKeys') prefilledKeys?: string[];
+	@Input('label') label?: string;
+	@Input('action') action!: 'view' | 'create' | 'update';
+
+	keys!: string[];
+
+	constructor() {}
+
+	ngOnInit(): void {
+		if (this.prefilledKeys?.length) this.keys = this.prefilledKeys;
+	}
+
+	addField() {
+		const inputField = document.getElementById('input');
+		const inputValue = document.getElementById('input') as HTMLInputElement;
+		inputField?.addEventListener('keydown', e => {
+			const key = e.keyCode || e.charCode;
+			if (key == 8) {
+				e.stopImmediatePropagation();
+				if (this.keys.length > 0 && !inputValue?.value) this.keys.splice(-1);
+			}
+			if (e.which === 188 || e.key == ' ') {
+				if (this.keys.includes(inputValue?.value)) {
+					inputValue.value = '';
+					this.keys = this.keys.filter(e => String(e).trim());
+				} else {
+					this.keys.push(inputValue?.value);
+					inputValue.value = '';
+					this.keys = this.keys.filter(e => String(e).trim());
+					this.inputValues.emit(this.keys);
+				}
+				e.preventDefault();
+			}
+		});
+	}
+
+	removeValue(key: string) {
+		this.keys = this.keys.filter(e => e !== key);
+		this.inputValues.emit(this.keys);
+	}
+
+	focusInput() {
+		document.getElementById('input')?.focus();
+	}
+}
