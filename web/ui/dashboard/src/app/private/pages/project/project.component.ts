d0d57a37552c56bd016d08cbb279c9df20ab1486
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2024-01-19 07:24:25 +0000

    Enhancement: UI Updates (#1814)
    * update: add retention policy switch on project form
    
    * fix: update subscription config badge
    
    * update tooltip component
    
    * update project form
    
    * hide retention policy if disabled

--- web/ui/dashboard/src/app/private/pages/project/project.component.ts
+++ web/ui/dashboard/src/app/private/pages/project/project.component.ts
@@ -43,7 +43,6 @@ export class ProjectComponent implements OnInit {
 			route: '/meta-events'
 		}
 	];
-	shouldShowFullSideBar = true;
 	projectDetails?: PROJECT;
 	isLoadingProjectDetails: boolean = true;
 	showHelpDropdown = false;
