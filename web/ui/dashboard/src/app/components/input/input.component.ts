34f558b51678275ad14d7652f10f57add27a33a6
Pelumi Muyiwa-Oni <Pelumioni25@gmail.com>
2024-03-15 09:37:49 +0000

    Update dashboard theme (#1874)
    * add radix colors
    
    * update events filter component
    
    * update filter
    
    * update color config and pages
    
    * fix merge conflicts
    
    * fix signature modal (#1939)
    
    * Update radio/dropdown component (#1943)
    
    * fix dropdowns
    
    * update radio component
    
    * feat: update endpoint name and title (#1945)
    
    * fix: add project meta-event to frontend (#1916)
    
    * Re-implement rate limiter using postgres (#1937)
    
    * feat: reimplement rate limiter using postgres
    
    * feat: add bucket size (time duration) for rate limit
    
    * chore: update tests
    
    * chore: revert postgres pkg changes
    
    * chore: go mod tidy
    
    * feat: update rate when endpoint is updated
    
    * feat: set default rate limit value to 0
    
    * chore: update function name, add benchmarks
    
    * chore: update test
    
    * chore: update changes from main, remove dependency on the subscription repo
    
    * fix endpointst page (#1948)
    
    * chore: make rate limiter fail-open (#1950)
    
    * add radix colors
    
    * update color config and pages
    
    * fix merge conflicts
    
    * update package-lock.json
    
    ---------
    
    Co-authored-by: Raymond Tukpe <jirevwe@users.noreply.github.com>

--- web/ui/dashboard/src/app/components/input/input.component.ts
+++ web/ui/dashboard/src/app/components/input/input.component.ts
@@ -7,7 +7,7 @@ import { TooltipComponent } from '../tooltip/tooltip.component';
 	selector: '[convoy-input]',
 	standalone: true,
 	host: {
-		class: 'transition-all duration-[.3s] w-full font-normal text-12 placeholder:text-new.gray-300 text-new.gray-600 border border-new.gray-200 disabled:text-new.gray-400 disabled:border-new.primary-25 hover:border-new.primary-100 focus:border-new.primary-300 outline-none rounded-4px placeholder:text-14 bg-white-100 py-12px px-16px appearance-none',
+		class: 'transition-all duration-[.3s] w-full font-normal text-12 placeholder:text-neutral-6 text-neutral-11 border border-neutral-4 disabled:text-neutral-6 disabled:border-new.primary-25 hover:border-new.primary-100 focus:border-new.primary-300 outline-none rounded-4px placeholder:text-14 bg-white-100 py-12px px-16px appearance-none',
 		'[ngClass]': "{ 'pointer-events-none': readonly, 'appearance-none': type !== 'password' }"
 	}
 })
@@ -56,9 +56,11 @@ export class PasswordInputFieldComponent implements OnInit {
 	standalone: true,
 	imports: [CommonModule],
 	template: `
-		<div class="flex items-center text-12 text-danger-100 mt-8px">
-			<img src="assets/img/input-error-icon.svg" class="mr-6px w-16px" alt="input error icon" />
-			<span><ng-content></ng-content></span>
+		<div class="flex items-center text-12 mt-8px">
+			<svg width="16" height="16" class="mr-6px fill-error-9">
+				<use xlink:href="#error-icon"></use>
+			</svg>
+			<span class="text-error-9"><ng-content></ng-content></span>
 		</div>
 	`
 })
@@ -74,14 +76,14 @@ export class InputErrorComponent implements OnInit {
 	standalone: true,
 	imports: [CommonModule, TooltipComponent],
 	host: {
-		class: 'w-full text-12 text-new.gray-500 mb-8px flex items-center justify-between'
+		class: 'w-full text-12 mb-8px flex items-center justify-between'
 	},
 	template: `
-		<div class="flex items-center">
+		<div class="flex items-center text-neutral-9">
 			<ng-content></ng-content>
 			<convoy-tooltip *ngIf="tooltip" class="ml-4px" size="sm">{{ tooltip }}</convoy-tooltip>
 		</div>
-		<span *ngIf="required === 'true'" class="text-10 text-new.gray-400 px-6px rounded-22px font-medium bg-new.gray-100">required</span>
+		<span *ngIf="required === 'true'" class="text-10 text-gray-11 px-6px rounded-22px font-medium bg-neutral-a3">required</span>
 	`,
 	styleUrls: ['./input.component.scss']
 })
