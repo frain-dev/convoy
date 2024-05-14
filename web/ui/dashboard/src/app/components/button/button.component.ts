24b02f0305e3925b29beda42b1395ba44d588446
Pelumi Honey <pelumioni25@gmail.com>
2024-05-14 16:54:55 +0100

    update button component and uses

--- web/ui/dashboard/src/app/components/button/button.component.ts
+++ web/ui/dashboard/src/app/components/button/button.component.ts
@@ -12,9 +12,9 @@ import { CommonModule } from '@angular/common';
 })
 export class ButtonComponent implements OnInit {
 	@Input('buttonText') buttonText!: string;
-	@Input('fill') fill: 'default' | 'outline' | 'clear' | 'text' | 'link' | 'soft' = 'default';
+	@Input('fill') fill: 'default' | 'outline' | 'clear' | 'text' | 'link' | 'soft' | 'soft-outline' = 'default';
 	@Input('size') size: 'xs' | 'sm' | 'md' | 'lg' = 'md';
-	@Input('color') color: 'primary' | 'success' | 'warning' | 'danger' | 'grey' | 'transparent' = 'primary';
+	@Input('color') color: 'primary' | 'success' | 'warning' | 'danger' | 'grey' | 'transparent' | 'neutral' | 'error' = 'primary';
 	@Input('texture') texture: 'deep' | 'light' = 'deep';
 	@Input('index') tabIndex = 0;
 	buttonSizes = { xs: 'py-4px px-8px text-12', sm: `py-6px px-16px text-12`, md: `py-8px px-18px text-12`, lg: `py-10px px-36px w-full text-14` };
@@ -24,6 +24,81 @@ export class ButtonComponent implements OnInit {
 	ngOnInit(): void {}
 
 	get classes(): string {
+		const colors = {
+			primary: {
+				default: 'bg-new.primary-400 text-white-100',
+				outline: 'border border-new.primary-400 text-new.primary-400',
+				clear: 'border-none text-new.primary-400',
+				text: `border-none text-new.primary-400 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-new.primary-400 underline decoration-new.primary-400 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-new.primary-25 text-new.primary-400',
+				'soft-outline': `border border-new.primary-200 text-new.primary-400`
+			},
+			error: {
+				default: 'bg-error-9 text-white-100',
+				outline: 'border border-error-9 text-error-9',
+				clear: 'border-none text-error-9',
+				text: `border-none text-error-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-error-9 underline decoration-error-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-error-a3 text-error-11',
+				'soft-outline': `border border-error-6 text-error-9`
+			},
+			neutral: {
+				default: 'bg-neutral-9 text-white-100',
+				outline: 'border border-neutral-9 text-neutral-9',
+				clear: 'border-none text-neutral-9',
+				text: `border-none text-neutral-10 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-neutral-10 underline decoration-neutral-10 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-neutral-a3 text-neutral-11',
+				'soft-outline': 'border border-neutral-6 text-neutral-9'
+			},
+			grey: {
+				default: 'bg-neutral-9 text-white-100',
+				outline: 'border border-neutral-9 text-neutral-9',
+				clear: 'border-none text-neutral-9',
+				text: `border-none text-neutral-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-neutral-9 underline decoration-neutral-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-neutral-a3 text-neutral-11',
+				'soft-outline': 'border border-neutral-6 text-neutral-9'
+			},
+			success: {
+				default: 'bg-success-9 text-white-100',
+				outline: 'border border-success-9 text-success-9',
+				clear: 'border-none text-success-9',
+				text: `border-none text-success-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-success-9 underline decoration-success-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-success-a3 text-success-11',
+				'soft-outline': 'border border-success-6 text-success-9'
+			},
+			warning: {
+				default: 'bg-warning-9 text-white-100',
+				outline: 'border border-warning-9 text-warning-9',
+				clear: 'border-none text-warning-9',
+				text: `border-none text-warning-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-warning-9 underline decoration-warning-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-warning-a3 text-warning-11',
+				'soft-outline': 'border border-warning-6 text-warning-9'
+			},
+			danger: {
+				default: 'bg-warning-9 text-white-100',
+				outline: 'border border-warning-9 text-warning-9',
+				clear: 'border-none text-warning-9',
+				text: `border-none text-warning-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				link: `border-none text-warning-9 underline decoration-warning-9 ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
+				soft: 'bg-warning-a3 text-warning-11',
+				'soft-outline': 'border border-warning-6 text-warning-9'
+			},
+			transparent: {
+				default: 'bg-warning-9 text-white-100',
+				outline: 'border border-warning-9 text-warning-9',
+				clear: 'border-none text-warning-9',
+				text: 'border-none text-warning-9',
+				link: 'border-none text-warning-9 underline decoration-warning-9',
+				soft: 'bg-warning-a3 text-warning-11',
+				'soft-outline': 'border border-warning-6 text-warning-9'
+			}
+		};
+
 		const colorLevel = this.texture == 'deep' ? '100' : this.color == 'grey' ? '20' : '500';
 		const buttonTypes = {
 			default: `bg-${this.color}-${colorLevel} text-${this.texture == 'deep' ? 'white' : this.color}-100 border-none rounded-8px hover:shadow-xs transition-all`,
@@ -33,6 +108,7 @@ export class ButtonComponent implements OnInit {
 			text: `border-0 text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''}`,
 			link: `border-none text-${this.color}-${colorLevel} ${this.size == 'sm' || this.size == 'xs' ? 'text-12' : ''} underline decoration-${this.color}-${colorLevel}`
 		};
-		return `${this.fill !== 'text' ? this.buttonSizes[this.size] : ''} ${buttonTypes[this.fill]} flex items-center justify-center disabled:opacity-50`;
+
+		return `${this.fill !== 'text' ? this.buttonSizes[this.size] : ''} ${colors[this.color][this.fill]} flex items-center justify-center disabled:opacity-50 rounded-8px`;
 	}
 }
