fb2d05f534abc43dbbd63077cd57a0967eabe83d
Raymond Tukpe <jirevwe@users.noreply.github.com>
2024-05-29 12:42:42 +0200

    Add api rate limit (#2031)
    * patch: add api rate limit
    
    * patch: add const file
    
    * patch: update refs

--- const.go
+++ const.go
@@ -8,8 +8,15 @@ const (
 )
 
 const (
-	RATE_LIMIT               = 0 // should be deleted
-	RATE_LIMIT_DURATION      = 0 // should be deleted
+	HTTP_RATE_LIMIT         = 50
+	HTTP_RATE_LIMIT_PER_MIN = HTTP_RATE_LIMIT * 60
+
+	INGRESS_RATE_LIMIT         = 100
+	INGRESS_RATE_LIMIT_PER_MIN = HTTP_RATE_LIMIT * 60
+
+	EGRESS_RATE_LIMIT         = 100
+	EGRESS_RATE_LIMIT_PER_MIN = HTTP_RATE_LIMIT * 60
+
 	HTTP_TIMEOUT             = 10
 	HTTP_TIMEOUT_IN_DURATION = time.Duration(HTTP_TIMEOUT) * time.Second
 )
