488c4353be09b3825a55cf715dbdd4cdadbbb683
mekilis <st.nonso@gmail.com>
2024-06-07 18:50:07 +0100

    changed postgres driver to pgxpool

--- database/postgres/configuration.go
+++ database/postgres/configuration.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"github.com/frain-dev/convoy/util"
 
 	"github.com/frain-dev/convoy/database"
 	"github.com/frain-dev/convoy/datastore"
@@ -150,7 +151,7 @@ func (c *configRepo) UpdateConfiguration(ctx context.Context, cfg *datastore.Con
 
 	result, err := c.db.ExecContext(ctx, updateConfiguration,
 		cfg.UID,
-		cfg.IsAnalyticsEnabled,
+		util.BoolToText(cfg.IsAnalyticsEnabled),
 		cfg.IsSignupEnabled,
 		cfg.StoragePolicy.Type,
 		cfg.StoragePolicy.OnPrem.Path,
