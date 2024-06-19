02936d06a89e61827222b11591dbdea8cfca8808
Daniel Oluojomu <danvixent@gmail.com>
2024-06-19 20:39:26 +0100

    fix: add default value for filter_config_filter_is_flattened

--- sql/1718819653.sql
+++ sql/1718819653.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-ALTER TABLE convoy.subscriptions ADD COLUMN filter_config_filter_is_flattened BOOLEAN;
+ALTER TABLE convoy.subscriptions ADD COLUMN filter_config_filter_is_flattened BOOLEAN DEFAULT false;
 
 -- +migrate Down
 ALTER TABLE convoy.subscriptions DROP COLUMN filter_config_filter_is_flattened;
