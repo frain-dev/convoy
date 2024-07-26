b534b4ad6e263f02b12433e3de01c3caa5dc5b4d
Smart Mekiliuwa <st.nonso@gmail.com>
2024-07-26 13:24:33 +0100

    fixed delay duration bug in retry event delivery (#2110)
    * fixed delay duration bug in retry event delivery
    
    * fixed tests

--- worker/task/process_retry_event_delivery_test.go
+++ worker/task/process_retry_event_delivery_test.go
@@ -99,7 +99,7 @@ func TestProcessRetryEventDelivery(t *testing.T) {
 		{
 			name:          "Endpoint does not respond with 2xx",
 			cfgPath:       "./testdata/Config/basic-convoy.json",
-			expectedError: &EndpointError{Err: fmt.Errorf("%s, err: nil", ErrDeliveryAttemptFailed.Error()), delay: defaultEventDelay},
+			expectedError: &EndpointError{Err: fmt.Errorf("%s, err: nil", ErrDeliveryAttemptFailed.Error()), delay: 20000000000},
 			msg: &datastore.EventDelivery{
 				UID: "",
 			},
