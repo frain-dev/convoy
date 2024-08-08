77af637c3fc25454e9e185c6060bba4419864dab
Daniel Oluojomu <danvixent@gmail.com>
2024-08-08 19:56:03 +0100

    fix: remove print

--- testcon/integration_test_helper.go
+++ testcon/integration_test_helper.go
@@ -50,8 +50,6 @@ func setEnv(dbPort int, redisPort int) {
 	_ = os.Setenv("CONVOY_DB_PASSWORD", "convoy")
 	_ = os.Setenv("CONVOY_DB_DATABASE", "convoy")
 	_ = os.Setenv("CONVOY_DB_PORT", strconv.Itoa(dbPort))
-
-	fmt.Println("tresss", os.Getenv("TEST_LICENSE_KEY"))
 	_ = os.Setenv("CONVOY_LICENSE_KEY", os.Getenv("TEST_LICENSE_KEY"))
 }
 
