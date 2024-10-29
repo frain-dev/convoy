7b4adc4a6e38843af5f00568211d5fd363cc03fa
Raymond Tukpe <jirevwe@users.noreply.github.com>
2024-10-29 18:25:25 +0100

    chore: add event types docs (#2183)

--- api/handlers/event_types.go
+++ api/handlers/event_types.go
@@ -158,8 +158,8 @@ func (h *Handler) UpdateEventType(w http.ResponseWriter, r *http.Request) {
 //	@Tags			EventTypes
 //	@Accept			json
 //	@Produce		json
-//	@Param			projectID	path		string					true	"Project ID"
-//	@Param			eventTypeId	path		string					true	"Event Type ID"
+//	@Param			projectID	path		string	true	"Project ID"
+//	@Param			eventTypeId	path		string	true	"Event Type ID"
 //	@Success		201			{object}	util.ServerResponse{data=models.EventTypeResponse}
 //	@Failure		400,401,404	{object}	util.ServerResponse{data=Stub}
 //	@Security		ApiKeyAuth
