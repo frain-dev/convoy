95eac737b22ec58a73b7964fc2821b81c50a6ee6
Orim Dominic <orimdominic@gmail.com>
2025-02-12 19:41:37 +0100

    feat: add sample UI

--- web/ui/dashboard-react/src/App.tsx
+++ web/ui/dashboard-react/src/App.tsx
@@ -1,15 +1,19 @@
 import convoyLogo from './assets/img/svg/convoy-logo-full-new.svg';
+import { Button } from './components/ui/button';
 
 function App() {
 	return (
-		<>
-			<div className="">
-				<a href="/">
-					<img src={convoyLogo} className="logo convoy" alt="Convoy logo" />
-				</a>
-				<h1 className="text-3xl font-bold underline">Convoy</h1>
+		<div className="flex w-full">
+			<div className="bg-white py-24 mx-auto text-center">
+				<img src={convoyLogo} alt="convoy" className="mx-auto py-6" />
+				<h2 className="text-neutral py-4 tracking-tight text-pretty text-16 font-semibold">
+					The complete solution for secure, scalable, and reliable webhook delivery.
+				</h2>
+				<div className="pt-4">
+					<Button size="lg">Learn more</Button>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
