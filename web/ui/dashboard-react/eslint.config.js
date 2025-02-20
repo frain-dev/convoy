d7fc72efeee55afbe6fef668fb05a1c3643b8123
Orim Dominic <orimdominic@gmail.com>
2025-02-20 19:59:02 +0100

    chore: update eslint config

--- web/ui/dashboard-react/eslint.config.js
+++ web/ui/dashboard-react/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 
@@ -9,26 +10,44 @@ export default tseslint.config(
 	{ ignores: ['dist'] },
 	{
 		settings: { react: { version: '18.3' } },
-		extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
+		extends: [
+			js.configs.recommended,
+			eslint.configs.recommended,
+			tseslint.configs.recommended,
+			// ...tseslint.configs.strictTypeChecked
+		],
+		parser: '@typescript-eslint/parser',
 		files: ['**/*.{ts,tsx}'],
 		languageOptions: {
 			ecmaVersion: 'latest',
 			globals: globals.browser,
 			parserOptions: {
 				project: ['./tsconfig.node.json', './tsconfig.app.json'],
-				tsconfigRootDir: import.meta.dirname
-			}
+				tsconfigRootDir: import.meta.dirname,
+			},
 		},
 		plugins: {
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
-			react
+			react,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
 			...react.configs.recommended.rules,
-			...react.configs['jsx-runtime'].rules
-		}
-	}
+			...react.configs['jsx-runtime'].rules,
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true,
+					caughtErrors: 'none',
+				},
+			],
+		},
+	},
 );
