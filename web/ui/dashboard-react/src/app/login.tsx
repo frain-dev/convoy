7c056a72abff9127130204a5599fa75b8df60f71
Orim Dominic <orimdominic@gmail.com>
2025-02-23 10:25:12 +0100

    fix: adjust height of input fields in login

--- web/ui/dashboard-react/src/app/login.tsx
+++ web/ui/dashboard-react/src/app/login.tsx
@@ -47,7 +47,7 @@ function EmailInputField({ form }: EmailInputFieldProps) {
 							autoComplete="email"
 							type="email"
 							className={cn(
-								'mt-0 outline-none focus-visible:ring-0 border-neutral-4 shadow-none w-full h-full transition-all duration-300 bg-white-100 py-3 px-4 text-neutral-11 !text-xs/5 rounded-[4px] placeholder:text-new.gray-300 placeholder:text-sm/5 font-normal disabled:text-neutral-6 disabled:border-new.primary-25',
+								'mt-0 outline-none focus-visible:ring-0 border-neutral-4 shadow-none w-full h-auto transition-all duration-300 bg-white-100 py-3 px-4 text-neutral-11 !text-xs/5 rounded-[4px] placeholder:text-new.gray-300 placeholder:text-sm/5 font-normal disabled:text-neutral-6 disabled:border-new.primary-25',
 								fieldState.error
 									? 'border-new.error-500 focus-visible:ring-0 hover:border-new.error-500'
 									: ' hover:border-new.primary-100 focus:border-new.primary-300',
@@ -91,7 +91,7 @@ function PasswordInputField({ form }: PasswordInputFieldProps) {
 								autoComplete="current-password"
 								type={isPasswordVisible ? 'text' : 'password'}
 								className={cn(
-									'hide-password-toggle mt-0 outline-none focus-visible:ring-0 border-neutral-4 shadow-none w-full h-full transition-all duration-300 bg-white-100 py-3 px-4 text-neutral-11 !text-xs/5 rounded-[4px] placeholder:text-new.gray-300 placeholder:text-sm/5 font-normal disabled:text-neutral-6 disabled:border-new.primary-25',
+									'hide-password-toggle mt-0 outline-none focus-visible:ring-0 border-neutral-4 shadow-none w-full h-auto transition-all duration-300 bg-white-100 py-3 px-4 text-neutral-11 !text-xs/5 rounded-[4px] placeholder:text-new.gray-300 placeholder:text-sm/5 font-normal disabled:text-neutral-6 disabled:border-new.primary-25',
 									fieldState.error
 										? 'border-new.error-500 focus-visible:ring-0 hover:border-new.error-500'
 										: 'hover:border-new.primary-100 focus:border-new.primary-300',
