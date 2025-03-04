d517869d783866d56db1ea231d6c820f3e2f8119
Orim Dominic <orimdominic@gmail.com>
2025-03-04 16:01:47 +0100

    feat: use auth hook

--- web/ui/dashboard-react/src/lib/auth.ts
+++ web/ui/dashboard-react/src/lib/auth.ts
@@ -1,12 +1,6 @@
 import { redirect } from '@tanstack/react-router';
-import { getCachedAuthTokens } from '@/services/auth.service';
 
-export function ensureCanAccessPrivatePages() {
-	const { authState } = getCachedAuthTokens();
-
-	if (authState == false) {
-		redirect({ to: '/login', throw: true });
-	}
-
-	return true;
+export function ensureCanAccessPrivatePages(isLoggedIn?: boolean) {
+	if (isLoggedIn) return true;
+	throw redirect({ to: '/login' });
 }
--- web/ui/dashboard-react/src/lib/router.ts
+++ web/ui/dashboard-react/src/lib/router.ts
@@ -1,7 +1,7 @@
 import { createRouter } from '@tanstack/react-router';
 import { routeTree } from '../routes.gen';
 
-export const router = createRouter({ routeTree });
+export const router = createRouter({ routeTree, context: { auth: null } });
 
 declare module '@tanstack/react-router' {
 	interface Register {
