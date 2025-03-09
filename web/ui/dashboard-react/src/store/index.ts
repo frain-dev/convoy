300196e7712fede17f9480e354cc5e91bcd9f693
Orim Dominic <orimdominic@gmail.com>
2025-03-09 05:28:09 +0100

    feat: set up organisation store with zustand

--- web/ui/dashboard-react/src/store/index.ts
+++ web/ui/dashboard-react/src/store/index.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
-import { CONVOY_LICENSES_KEY } from '@/lib/constants';
+import { CONVOY_LICENSES_KEY, CONVOY_ORG_KEY } from '@/lib/constants';
 
+import type { PaginatedResult } from '@/models/global.model';
 import type { LicenseKey } from '@/services/licenses.service';
+import type { Organisation } from '@/models/organisation.model';
 
 type LicenseStore = {
 	licenses: Array<LicenseKey>;
@@ -14,11 +16,43 @@ export const useLicenseStore = create<LicenseStore>()(
 	persist(
 		set => ({
 			licenses: [],
-			setLicenses: (keys: Array<LicenseKey>) => set({ licenses: keys }),
+			setLicenses: keys => set({ licenses: keys }),
 		}),
 		{
 			name: CONVOY_LICENSES_KEY,
 			storage: createJSONStorage(() => localStorage), // just to be explicit
+		},
+	),
+);
+
+type OrganisationStore = {
+	/** The current organisation in use */
+	org: Organisation | null;
+	/** Set the current organisation in use */
+	setOrg: (org: Organisation | null) => void;
+	paginatedOrgs: PaginatedResult<Organisation>;
+	setPaginatedOrgs: (pgOrgs: PaginatedResult<Organisation>) => void;
+};
+
+export const useOrganisationStore = create<OrganisationStore>()(
+	persist(
+		set => ({
+			org: null,
+			setOrg: org => set({ org }),
+			paginatedOrgs: {
+				content: [],
+				pagination: {
+					per_page: 0,
+					has_next_page: false,
+					has_prev_page: false,
+					prev_page_cursor: '',
+					next_page_cursor: '',
+				},
+			},
+			setPaginatedOrgs: pgOrgs => set({ paginatedOrgs: pgOrgs }),
+		}),
+		{
+			name: CONVOY_ORG_KEY,
 		},
 	),
 );
