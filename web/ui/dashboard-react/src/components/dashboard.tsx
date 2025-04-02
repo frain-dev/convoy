5c470d15bf9defc26691c444d6a32406eaa6966e
Orim Dominic <orimdominic@gmail.com>
2025-04-02 04:40:44 +0100

    feat: update UI components

--- web/ui/dashboard-react/src/components/dashboard.tsx
+++ web/ui/dashboard-react/src/components/dashboard.tsx
@@ -392,7 +392,7 @@ function ProjectsList() {
 											</FormControl>
 										</PopoverTrigger>
 										<PopoverContent className="w-full mt-1 p-0 z-10 rounded-md border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
-											<Command>
+											<Command className="shadow-none">
 												<CommandInput
 													placeholder={'Filter projects'}
 													onInput={e => {
@@ -402,7 +402,7 @@ function ProjectsList() {
 														);
 													}}
 												/>
-												<CommandList>
+												<CommandList className='shadow-none'>
 													<CommandEmpty>
 														{projects.length
 															? `No projects found for '${field.value}'`
