909a149c0fe8954fb34f2e242943893dd36ad423
Smart Mekiliuwa <st.nonso@gmail.com>
2025-06-10 08:57:30 +0100

    Update sql/1746876410.sql
    Co-authored-by: Zubin Henner <9605+zubin@users.noreply.github.com>

--- sql/1746876410.sql
+++ sql/1746876410.sql
@@ -1,5 +1,7 @@
 -- +migrate Up
-ALTER TABLE convoy.subscriptions ADD COLUMN IF NOT EXISTS delivery_mode VARCHAR(255) NOT NULL DEFAULT 'at_least_once';
+CREATE TYPE delivery_mode AS ENUM ('at_least_once', 'at_most_once');
+ALTER TABLE convoy.subscriptions
+  ADD COLUMN IF NOT EXISTS delivery_mode delivery_mode NOT NULL DEFAULT 'at_least_once';
 
 -- Add comment to explain the column
 COMMENT ON COLUMN convoy.subscriptions.delivery_mode IS 'Specifies the delivery mode for the subscription. Can be either at_least_once or at_most_once';
