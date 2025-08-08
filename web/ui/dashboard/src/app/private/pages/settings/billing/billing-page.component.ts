93c6c9835c29b7f7618604ea5dc481509b43c6cc
Smart Mekiliuwa <st.nonso@gmail.com>
2025-08-08 12:37:24 +0100

    removed dead code

--- web/ui/dashboard/src/app/private/pages/settings/billing/billing-page.component.ts
+++ web/ui/dashboard/src/app/private/pages/settings/billing/billing-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-billing-page',
-  templateUrl: './billing-page.component.html',
-  styleUrls: ['./billing-page.component.scss']
+  templateUrl: './billing-page.component.html'
 })
 export class BillingPageComponent {} 
