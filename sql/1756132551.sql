65752e8473b2f32d45ba13fa291e7845f4fe161c
Raymond Tukpe <rtukpe@gmail.com>
2025-08-27 17:53:44 +0200

    update comment

--- sql/1756132551.sql
+++ sql/1756132551.sql
@@ -2,7 +2,7 @@
 create type convoy.portal_auth_types as enum ('none', 'static_token', 'refresh_token');
 alter table convoy.portal_links add column auth_type convoy.portal_auth_types not null default 'static_token';
 
--- convert portal links with endpoint arrays, for each endpoint in that array set the owner_id of the endpoints to the same value
+-- Assigns deterministic owner IDs (16-char SHA256 hash) to endpoints based on their associated portal IDs
 with portals_with_ids as (select id, endpoints from convoy.portal_links where length(endpoints) > 0)
 update convoy.endpoints e
 set owner_id = substr(encode(sha256(p.id::bytea), 'hex'), 1, 16)
