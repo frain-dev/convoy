102c46f706e2119712b1b1661befaa617d9b3b8a
Smart Mekiliuwa <st.nonso@gmail.com>
2026-03-06 06:00:40 +0100

    chore: remove revert-concurrently.sh

--- scripts/revert-concurrently.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Revert CONCURRENTLY removal - run after sqlc generate is done
-# Usage: ./scripts/revert-concurrently.sh
-
-cd "$(dirname "$0")/.."
-git checkout -- sql/
-echo "Reverted CONCURRENTLY in migrations (restored from git)"
