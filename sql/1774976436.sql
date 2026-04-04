1e066da5c4c3358c07154c0fae4689e4784a3e1d
Raymond Tukpe <rtukpe@gmail.com>
2026-04-04 17:48:28 +0200

    Add conditional creation of `convoy_backup` publication to handle existing publications

--- sql/1774976436.sql
+++ sql/1774976436.sql
@@ -1,8 +1,13 @@
 -- +migrate Up
-CREATE PUBLICATION convoy_backup FOR TABLE
-    convoy.events,
-    convoy.event_deliveries,
-    convoy.delivery_attempts;
+-- +migrate StatementBegin
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_publication WHERE pubname = 'convoy_backup') THEN
+        EXECUTE 'CREATE PUBLICATION convoy_backup FOR TABLE convoy.events, convoy.event_deliveries, convoy.delivery_attempts';
+    END IF;
+END;
+$$;
+-- +migrate StatementEnd
 
 -- +migrate Down
 DROP PUBLICATION IF EXISTS convoy_backup;
