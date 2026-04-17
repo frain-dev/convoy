59a88402b2ba84dc18166e6b0269fc27395a950d
Raymond Tukpe <rtukpe@gmail.com>
2026-04-17 11:36:36 +0200

    Add new tools and scripts for OpenAPI spec generation and updates

--- .mise-tasks/gen/docs.sh
+++ .mise-tasks/gen/docs.sh
@@ -7,6 +7,21 @@
 set -e
 
 echo "Generating docs"
+
+#generate custom swag tags
+go run docs/annotate_dtos/main.go
+
+#generate v2 openapi specs
 swag init --generatedTime --parseDependency --parseDependencyLevel 3 --parseInternal -g handlers/main.go -d api/ api/*
 swag fmt -d ./api
-go run v3gen/main.go
+
+# fix openapi2 specs (structural fixes, add proper produce/consume tags, replace x-nullable..)
+bash docs/fix_openapi_spec.sh
+
+#generate v3 specs
+api-spec-converter --from=swagger_2 --to=openapi_3 -s yaml ./docs/swagger.yaml > ./docs/v3/openapi3.yaml
+api-spec-converter --from=swagger_2 --to=openapi_3 ./docs/swagger.json > ./docs/v3/openapi3.json
+
+# add region descriptions and EU server (swag only supports a single host)
+yq -i '.servers[0].description = "US Region" | .servers += [{"url": "https://eu.getconvoy.cloud/api", "description": "EU Region"}]' ./docs/v3/openapi3.yaml
+jq '.servers[0].description = "US Region" | .servers += [{"url": "https://eu.getconvoy.cloud/api", "description": "EU Region"}]' ./docs/v3/openapi3.json > ./docs/v3/openapi3.json.tmp && mv ./docs/v3/openapi3.json.tmp ./docs/v3/openapi3.json
